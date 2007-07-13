41cf8998f5577712be22e74e52bfdd7e6999f95d
rgw_ch <devnull@localhost>
2007-07-13 14:52:27 +0200

    [svn] neue files

---
+++ elexis/src/ch/elexis/views/LabNotSeenView.java
@@ -0,0 +1,24 @@
+package ch.elexis.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class LabNotSeenView extends ViewPart {
+
+	public LabNotSeenView() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
