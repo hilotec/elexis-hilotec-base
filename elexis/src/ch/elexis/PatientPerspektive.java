2830312e9c8af24dd64f5b131fb36b75a0f29f70
rgw_ch <devnull@localhost>
2007-08-03 12:16:42 +0200

    [svn] Standardlayout verbessert

--- elexis/src/ch/elexis/PatientPerspektive.java
+++ elexis/src/ch/elexis/PatientPerspektive.java
@@ -8,7 +8,7 @@
  * Contributors:
  *    G. Weirich - initial implementation
  *    
- *    $Id: PatientPerspektive.java 1778 2007-02-09 21:34:42Z rgw_ch $
+ *    $Id: PatientPerspektive.java 2947 2007-08-03 10:16:42Z rgw_ch $
  *******************************************************************************/
 
 package ch.elexis;
@@ -40,7 +40,7 @@ import ch.elexis.views.codesystems.LeistungenView;
  */
 public class PatientPerspektive implements IPerspectiveFactory {
 	public static final String ID="ch.elexis.PatientPerspective"; //$NON-NLS-1$
-	public void createInitialLayout(IPageLayout layout) {
+	public void createInitialLayout(final IPageLayout layout) {
 		String editorArea = layout.getEditorArea();
 		layout.setEditorAreaVisible(false);
 		layout.setFixed(false);
@@ -50,9 +50,11 @@ public class PatientPerspektive implements IPerspectiveFactory {
         IFolderLayout left=layout.createFolder("Links.folder",IPageLayout.LEFT,0.4f,editorArea); //$NON-NLS-1$
         IFolderLayout main=layout.createFolder("Haupt.Folder",IPageLayout.RIGHT,0.3f,"Links.folder"); //$NON-NLS-1$
 		IFolderLayout leftbottom=layout.createFolder("links.unten", IPageLayout.BOTTOM, 0.7f, "Links.folder");
+		IFolderLayout right=layout.createFolder("Rechts.folder",IPageLayout.RIGHT, 0.7f, "Haupt.Folder");
+		
 		main.addView(PatientDetailView.ID);
 		left.addView(PatientenListeView.ID);
-		left.addView(KonsListe.ID);
+		//left.addView(KonsListe.ID);
 		//left.addView(FallListeView.ID);
 		leftbottom.addView(FaelleView.ID);
 		left.addView(PatHeuteView.ID);
@@ -61,6 +63,8 @@ public class PatientPerspektive implements IPerspectiveFactory {
 		main.addView(LaborView.ID);
 		main.addView(RezepteView.ID);
 		main.addView(AUF2.ID);
+		
+		right.addView(KonsListe.ID);
 		
 		main.addPlaceholder(FallDetailView.ID);
 		main.addPlaceholder(TextView.ID);
@@ -72,7 +76,7 @@ public class PatientPerspektive implements IPerspectiveFactory {
 		layout.addShowViewShortcut(PatientenListeView.ID);
         //layout.addShowViewShortcut(FallListeView.ID);
 		layout.addPerspectiveShortcut(FaelleView.ID);
-		layout.addPerspectiveShortcut(KonsListe.ID);
+		layout.addShowViewShortcut(KonsListe.ID);
         layout.addShowViewShortcut(PatHeuteView.ID);
         layout.addShowViewShortcut(KonsDetailView.ID);
         layout.addShowViewShortcut(RezepteView.ID);
