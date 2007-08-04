211b9c443d531082a2791d48f61d4919ce4163dc
rgw_ch <devnull@localhost>
2007-08-04 11:26:45 +0200

    [svn] Incomplete - task 20: xchange500 Schema adaptieren
    Incomplete - task 19: EMR Printer

--- elexis/src/ch/elexis/exchange/elements/XChangeElement.java
+++ elexis/src/ch/elexis/exchange/elements/XChangeElement.java
@@ -5,16 +5,23 @@ import java.util.List;
 import org.jdom.Element;
 
 import ch.elexis.exchange.XChangeContainer;
+import ch.elexis.util.Log;
+import ch.elexis.util.Result;
 
 public class XChangeElement {
 	Element e;
 	XChangeContainer parent;
-	protected XChangeElement(XChangeContainer p,Element el){
+	
+	public enum FORMAT{PLAIN,XML,HTML};
+	public static final int OK=0;
+	public static final int FORMAT_NOT_SUPPORTED=1;
+	
+	protected XChangeElement(final XChangeContainer p,final Element el){
 		parent=p;
 		e=el;
 	}
 	
-	protected XChangeElement(XChangeContainer p){
+	protected XChangeElement(final XChangeContainer p){
 		parent=p;
 	}
 	public Element getElement(){
@@ -29,17 +36,21 @@ public class XChangeElement {
 	 * @param name name of the atribute
 	 * @return the value which can be an empty String but is never null.
 	 */
-	public String getAttr(String name){
+	public String getAttr(final String name){
 		String ret=e.getAttributeValue(name);
 		return ret==null ? "" : ret;
 	}
 	
-	protected void add(XChangeElement el){
+	protected void add(final XChangeElement el){
 		e.addContent(el.getElement());
 	}
 	
 	@SuppressWarnings("unchecked")
-	protected List<Element> getElements(String name){
+	protected List<Element> getElements(final String name){
 		return e.getChildren(name,XChangeContainer.ns);
+	}
+	
+	public Result<String> toString(final FORMAT format){
+		return new Result<String>(Log.ERRORS,FORMAT_NOT_SUPPORTED,"Format not supported",null,true);
 	}
 }
