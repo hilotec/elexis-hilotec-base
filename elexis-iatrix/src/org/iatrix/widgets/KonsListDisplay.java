34c3e1a89941cec96c7e1763d631081891830738
danlutz <devnull@localhost>
2007-08-20 16:57:50 +0200

    [svn] - New layout and data loading strategy in KonsListDisplay (better performance) - Separate component KonsListComposite

--- elexis-iatrix/src/org/iatrix/widgets/KonsListDisplay.java
+++ elexis-iatrix/src/org/iatrix/widgets/KonsListDisplay.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, G. Weirich, D. Lutz, P. Schönbucher and Elexis
+ * Copyright (c) 2007, G. Weirich, D. Lutz, P. Schönbucher and Elexis
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -22,34 +22,19 @@ import java.util.List;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Text;
-import org.eclipse.ui.forms.events.HyperlinkAdapter;
-import org.eclipse.ui.forms.events.HyperlinkEvent;
 import org.eclipse.ui.forms.widgets.FormToolkit;
-import org.eclipse.ui.forms.widgets.Hyperlink;
 import org.eclipse.ui.forms.widgets.ScrolledForm;
-import org.eclipse.ui.forms.widgets.TableWrapData;
-import org.eclipse.ui.forms.widgets.TableWrapLayout;
 
 import ch.elexis.actions.BackgroundJob;
-import ch.elexis.actions.GlobalEvents;
 import ch.elexis.actions.BackgroundJob.BackgroundJobListener;
 import ch.elexis.data.Fall;
 import ch.elexis.data.Konsultation;
 import ch.elexis.data.Patient;
-import ch.elexis.data.PersistentObject;
-import ch.elexis.data.Verrechnet;
-import ch.elexis.util.Money;
 import ch.elexis.util.SWTHelper;
-import ch.rgw.tools.StringTool;
 import ch.rgw.tools.TimeTool;
 
 /**
@@ -59,23 +44,16 @@ import ch.rgw.tools.TimeTool;
  *
  */
 public class KonsListDisplay extends Composite implements BackgroundJobListener {
-	// TODO DEBUG
-	static long lastTime = System.currentTimeMillis();
-	
-	private static final int COLUMNS = 2;
-	
 	private Patient patient = null;
-	private List<WidgetRow> rows = new ArrayList<WidgetRow>();
 	
 	private FormToolkit toolkit;
 	private ScrolledForm form;
 	private Composite formBody;
-	private Label loadingLabel = null;
 	
-	private LabelProvider verrechnetLabelProvider;
-	
+	private KonsListComposite konsListComposite;
+
 	private KonsLoader dataLoader;
-	
+
 	public KonsListDisplay(Composite parent) {
 		super(parent, SWT.BORDER);
 		
@@ -85,44 +63,28 @@ public class KonsListDisplay extends Composite implements BackgroundJobListener 
 		form = toolkit.createScrolledForm(this);
 		formBody = form.getBody();
 		
-		TableWrapLayout layout = SWTHelper.createTableWrapLayout(COLUMNS, true);
-		layout.leftMargin = 0;
-		layout.rightMargin = 0;
-		layout.topMargin = 0;
-		layout.bottomMargin = 0;
-		layout.horizontalSpacing = 0;
-		layout.verticalSpacing = 0;
-		formBody.setLayout(layout);
+		formBody.setLayout(new GridLayout(1, false));
 		
-		verrechnetLabelProvider = new LabelProvider() {
-        	public String getText(Object element) {
-        		if (!(element instanceof Verrechnet)) {
-        			return "";
-        		}
-        		
-        		Verrechnet verrechnet = (Verrechnet) element;
-        		String name = verrechnet.getText();
-
-        		// TODO replace with verrechnet.getNickname();
-        		String vClass = verrechnet.getVerrechenbar().getClass().getName();
-        		if (vClass.equals("ch.elexis.data.TarmedLeistung")) {
-            		String nick = ((PersistentObject) verrechnet.getVerrechenbar()).get("Nick");
-            		if (!StringTool.isNothing(nick)) {
-            			name = nick;
-            		}
-        		}
-        		
-        		StringBuilder sb = new StringBuilder();
-                int z = verrechnet.getZahl();
-                Money preis=new Money(verrechnet.getEffPreis()).multiply(z);
-                sb.append(z).append(" ").append(name)
-                 .append(" (").append(preis.getAmountAsString()).append(")");
-                return sb.toString();
-            }
-		};
+		konsListComposite = new KonsListComposite(formBody, toolkit);
+		konsListComposite.setLayoutData(SWTHelper.getFillGridData(1, true, 1, false));
 
 		dataLoader = new KonsLoader();
 		dataLoader.addListener(this);
+	}
+	
+	/**
+	 * reload contents
+	 */
+	private void reload(boolean showLoading) {
+		if (patient != null && dataLoader.isValid()) {
+			konsListComposite.setKonsultationen(dataLoader.getKonsultationen());
+		} else {
+			if (showLoading) {
+				konsListComposite.setKonsultationen(null);
+			}
+		}
+
+		refresh();
 	}
 	
 	/*
@@ -130,366 +92,46 @@ public class KonsListDisplay extends Composite implements BackgroundJobListener 
 	 */
 	private void refresh() {
 		form.reflow(true);
-
-		for (WidgetRow row : rows) {
-			row.setVerticalSeparatorVisibility();
-		}
-	}
-	
-	/**
-	 * reload contents
-	 */
-	private void reload() {
-		if (loadingLabel != null) {
-			loadingLabel.dispose();
-		}
-		
-		for (WidgetRow row : rows) {
-			row.dispose();
-		}
-		
-		rows.clear();
-		
-		if (dataLoader.isValid()) {
-			if (patient != null) {
-				List<Konsultation> konsultationen = new ArrayList<Konsultation>();
-
-				for (Fall fall : patient.getFaelle()) {
-					for (Konsultation konsultation: fall.getBehandlungen(false)) {
-						konsultationen.add(konsultation);
-					}
-				}
-
-				Collections.sort(konsultationen, new Comparator<Konsultation>() {
-					TimeTool t1 = new TimeTool();
-					TimeTool t2 = new TimeTool();
-
-					public int compare(final Konsultation o1, final Konsultation o2) {
-						if ((o1 == null) || (o2 == null)) {
-							return 0;
-						}
-						t1.set(o1.getDatum());
-						t2.set(o2.getDatum());
-						if (t1.isBefore(t2)) {
-							return 1;
-						}
-						if (t1.isAfter(t2)) {
-							return -1;
-						}
-						return 0;
-					}
-				});
-
-				for (Konsultation konsultation : konsultationen) {
-					WidgetRow row = new WidgetRow(formBody, konsultation);
-					row.refresh();
-					rows.add(row);
-				}
-			}
-		} else {
-			loadingLabel = toolkit.createLabel(formBody, "Lade Konsultationen...");
-			loadingLabel.setLayoutData(SWTHelper.getFillTableWrapData(COLUMNS, true, 1, false));
-		}
-
-		refresh();
 	}
 	
 	public void setPatient(Patient patient) {
+		boolean patientChanged = patientChanged(patient);
+
 		this.patient = patient;
+
+		dataLoader.cancel();
 		dataLoader.invalidate();
+
+		// cause "loading" label to be displayed
+		if (patientChanged) {
+			reload(true);
+		}
+
 		dataLoader.setPatient(patient);
-		reload();
 		dataLoader.schedule();
 	}
 	
-	private void selectConsultation(Konsultation konsultation) {
-		GlobalEvents.getInstance().fireSelectionEvent(konsultation);
+	private boolean patientChanged(Patient newPatient) {
+		if (this.patient != null || newPatient != null) {
+			if (this.patient == null ||
+					newPatient == null ||
+					!this.patient.getId().equals(newPatient.getId())) {
+				
+				return true;
+			}
+		}
+
+		return false;
 	}
 	
 	public void jobFinished(BackgroundJob j) {
-		reload();
-	}
-	
-	/**
-	 * This class encapsulates the required widgets for a row. It assumes that
-	 * the parent has set a TableWrapLayout with COLUMNS columns.
-	 */
-	class WidgetRow {
-		Label horizontalSeparator;
-		Hyperlink hTitle;
-		Label lFall;
-		EnhancedTextFieldRO etf;
-		Text verrechnung;
-		Label leftVerticalSeparator;
-		Label rightVerticalSeparator;
-
-		Konsultation konsultation;
-		
-		// collect controls for disposal in dispose()
-		List<Control> controls;
-		
-		// TODO DEBUG
-		private void debug(String message) {
-			long time = System.currentTimeMillis();
-			long delta = time - lastTime;
-			lastTime = time;
-			System.err.println(message + "[" + delta + ", " + time + "]");
-		}
-		
-		WidgetRow(Composite parent, Konsultation konsultation) {
-			/*
-			 * Important: Add all created controls to "controls"
-			 *            for later disposal.
-			 */
-			
-			// TODO DEBUG
-			debug("Creating Row");
-
-			TableWrapLayout twLayout;
-			TableWrapData layoutData;
-			
-			controls = new ArrayList<Control>();
-			
-			// header
-			
-			Composite headerArea = toolkit.createComposite(parent);
-			controls.add(headerArea);
-			headerArea.setLayoutData(SWTHelper.getFillTableWrapData(COLUMNS, true, 1, false));
-			GridLayout gridLayout = new GridLayout(2, false);
-			gridLayout.marginLeft = 0;
-			gridLayout.marginRight = 0;
-			gridLayout.marginTop = 0;
-			gridLayout.marginBottom = 0;
-			gridLayout.horizontalSpacing = 0;
-			gridLayout.verticalSpacing = 0;
-			headerArea.setLayout(gridLayout);
-			
-			hTitle = toolkit.createHyperlink(headerArea, "", SWT.NONE);
-			hTitle.addHyperlinkListener(new HyperlinkAdapter() {
-				public void linkActivated(HyperlinkEvent e) {
-					selectConsultation(WidgetRow.this.konsultation);
-				}
-			});
-			controls.add(hTitle);
-			
-			lFall = toolkit.createLabel(headerArea, "");
-			controls.add(lFall);
-			lFall.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_END | GridData.GRAB_HORIZONTAL));
-			
-			// COLUMNS controls for parent
-			
-			// leftArea containing etf and verticalSeparator
-			Composite leftArea = toolkit.createComposite(parent);
-			controls.add(leftArea);
-			layoutData = SWTHelper.getFillTableWrapData(1, true, 1, false);
-			layoutData.valign = TableWrapData.FILL;
-			leftArea.setLayoutData(layoutData);
-			
-			twLayout = SWTHelper.createTableWrapLayout(2, false);
-			twLayout.leftMargin = 0;
-			twLayout.rightMargin = 0;
-			twLayout.topMargin = 0;
-			twLayout.bottomMargin = 0;
-			twLayout.horizontalSpacing = 0;
-			twLayout.verticalSpacing = 0;
-			leftArea.setLayout(twLayout);
-			
-			etf = new EnhancedTextFieldRO(leftArea);
-			controls.add(etf);
-			toolkit.adapt(etf);
-			etf.setLayoutData(SWTHelper.getFillTableWrapData(1, true, 1, false));
-			
-			leftVerticalSeparator = toolkit.createLabel(leftArea, "", SWT.SEPARATOR | SWT.VERTICAL);
-			controls.add(leftVerticalSeparator);
-			layoutData = SWTHelper.getFillTableWrapData(1, false, 1, false);
-			layoutData.valign = TableWrapData.FILL;
-			// work-around: default height of separators is 64
-			layoutData.maxHeight = 0;
-			leftVerticalSeparator.setLayoutData(layoutData);
-
-			// rightArea containing verticalSeparator and verrechnung
-			Composite rightArea = toolkit.createComposite(parent);
-			controls.add(rightArea);
-			layoutData = SWTHelper.getFillTableWrapData(1, true, 1, false);
-			layoutData.valign = TableWrapData.FILL;
-			rightArea.setLayoutData(layoutData);
-			
-			twLayout = SWTHelper.createTableWrapLayout(2, false);
-			twLayout.leftMargin = 0;
-			twLayout.rightMargin = 0;
-			twLayout.topMargin = 0;
-			twLayout.bottomMargin = 0;
-			twLayout.horizontalSpacing = 0;
-			twLayout.verticalSpacing = 0;
-			rightArea.setLayout(twLayout);
-			
-			rightVerticalSeparator = toolkit.createLabel(rightArea, "", SWT.SEPARATOR | SWT.VERTICAL);
-			controls.add(rightVerticalSeparator);
-			layoutData = SWTHelper.getFillTableWrapData(1, false, 1, false);
-			layoutData.valign = TableWrapData.FILL;
-			// work-around: default height of separators is 64, that's too heigh
-			layoutData.maxHeight = 0;
-			rightVerticalSeparator.setLayoutData(layoutData);
-			
-			verrechnung = toolkit.createText(rightArea, "", SWT.WRAP | SWT.READ_ONLY);
-			controls.add(verrechnung);
-			verrechnung.setLayoutData(SWTHelper.getFillTableWrapData(1, true, 1, false));
-
-			// separator
-			
-			horizontalSeparator = toolkit.createLabel(parent, "", SWT.SEPARATOR | SWT.HORIZONTAL);
-			controls.add(horizontalSeparator);
-			horizontalSeparator.setLayoutData(SWTHelper.getFillTableWrapData(COLUMNS, true, 1, false));
-
-			this.konsultation = konsultation;
-		}
-		
-		void refresh() {
-			// TODO DEBUG
-			debug("Refreshing Row");
-
-
-			String lineSeparator = System.getProperty("line.separator");
-			
-			hTitle.setText(konsultation.getLabel());
-			lFall.setText(konsultation.getFall().getLabel());
-
-			String text = konsultation.getEintrag().getHead();
-			if (text == null) {
-				text = "";
-			}
-			etf.setText(text);
-
-			List<Verrechnet> leistungen = konsultation.getLeistungen();
-			List<String> leistungenLabels = replaceBlocks(leistungen);
-			
-			StringBuffer sb = new StringBuffer();
-			boolean isFirst = true;
-			for (String leistungLabel : leistungenLabels) {
-				if (isFirst) {
-					isFirst = false;
-				} else {
-					sb.append(lineSeparator);
-				}
-				sb.append(leistungLabel);
-			}
-
-			verrechnung.setText(sb.toString());
-		}
-		
-		public void setVerticalSeparatorVisibility() {
-			// only show one separator (layout work-around)
-			int height1 = leftVerticalSeparator.getSize().y;
-			int height2 = rightVerticalSeparator.getSize().y;
-			if (height1 >= height2) {
-				leftVerticalSeparator.setVisible(true);
-				rightVerticalSeparator.setVisible(false);
-			} else {
-				leftVerticalSeparator.setVisible(false);
-				rightVerticalSeparator.setVisible(true);
-			}
-			
-			System.err.println("heights: " + height1 + ", " + height2);
-		}
-		
-		private List<String> replaceBlocks(List<Verrechnet> leistungen) {
-			// TODO DEBUG
-			debug("Replacing Blocks");
-
-			List<String> labels = new ArrayList<String>();
-
-			List<Verrechnet> unassigned = new ArrayList<Verrechnet>();
-			unassigned.addAll(leistungen);
-			List<Verrechnet> assigned = new ArrayList<Verrechnet>();
-			
-			// TODO consider number of elements in blocks
-			/*
-			Query<Leistungsblock> query = new Query<Leistungsblock>(Leistungsblock.class);
-			query.orderBy(false, "Name");
-			List<Leistungsblock> blocks = query.execute();
-			if (blocks != null) {
-				for (Leistungsblock block : blocks) {
-					if (containsBlock(unassigned, block)) {
-						removeBlock(unassigned, block);
-						// TODO sum
-						labels.add(block.getName());
-					}
-				}
-			}
-			*/
-
-			// add remaining leistungen
-			for (Verrechnet leistung : unassigned) {
-				labels.add(verrechnetLabelProvider.getText(leistung));
-			}
-			
-			return labels;
-		}
-		
-		/*
-		private boolean compareVerrechenbar(IVerrechenbar v1, IVerrechenbar v2) {
-			final Comparator comparator = new IVerrechenbar.DefaultComparator();
-			
-			int result = comparator.compare(v1, v2); 
-			return (result == 0);
-		}
-
-		private boolean containsBlock(List<Verrechnet> leistungen, Leistungsblock block) {
-			for (ICodeElement element : block.getElements()) {
-				if (element instanceof IVerrechenbar) {
-					IVerrechenbar blockVerrechenbar = (IVerrechenbar) element;
-					
-					for (Verrechnet leistung : leistungen) {
-						IVerrechenbar verrechenbar = leistung.getVerrechenbar();
-						
-						if (!compareVerrechenbar(blockVerrechenbar, verrechenbar)) {
-							return false;
-						}
-					}
-				}
-			}
-			
-			return true;
-		}
-		
-		private void removeBlock(List<Verrechnet> leistungen, Leistungsblock block) {
-			List<Verrechnet> removed = new ArrayList<Verrechnet>();
-			
-			for (ICodeElement element : block.getElements()) {
-				if (element instanceof IVerrechenbar) {
-					IVerrechenbar blockVerrechenbar = (IVerrechenbar) element;
-					
-					for (Verrechnet leistung : leistungen) {
-						IVerrechenbar verrechenbar = leistung.getVerrechenbar();
-						
-						if (compareVerrechenbar(blockVerrechenbar, verrechenbar)) {
-							removed.add(leistung);
-							// continue with next block element
-							break;
-						}
-
-				}
-			}
-		}
-		*/
-		
-		void dispose() {
-			// dispose all used controls
-			for (Control control : controls) {
-				if (control != null) {
-					control.dispose();
-				}
-			}
-			controls.clear();
-			
-			konsultation = null;
-		}
+		reload(false);
 	}
 	
 	class KonsLoader extends BackgroundJob {
 		String name;
 		Patient patient = null;
-		List<Konsultation> konsultationen = new ArrayList<Konsultation>();
+		List<KonsListComposite.KonsData> konsDataList = new ArrayList<KonsListComposite.KonsData>();
 		
 		public KonsLoader() {
 			super("KonsLoader");
@@ -502,34 +144,29 @@ public class KonsListDisplay extends Composite implements BackgroundJobListener 
 		}
 		
 		public IStatus execute(IProgressMonitor monitor) {
-			synchronized (konsultationen) {
-				konsultationen.clear();
-
-				// TODO DEBUG
-				System.err.println("START lade kons");
+			synchronized (konsDataList) {
+				konsDataList.clear();
 				
+				List<Konsultation> konsList = new ArrayList<Konsultation>();
+
 				if (patient != null) {
-					// TODO DEBUG
-					System.err.println("START lade kons " + patient.getLabel());
-					
 					Fall[] faelle = patient.getFaelle();
 					for (Fall fall : faelle) {
-						// TODO DEBUG
-						System.err.println("LADE fall " + fall.getLabel());
-						
 						Konsultation[] kons = fall.getBehandlungen(false);
 						for (Konsultation k : kons) {
-							konsultationen.add(k);
+							konsList.add(k);
+						}
+						
+						if (monitor.isCanceled()) {
+							monitor.done();
+							return Status.CANCEL_STATUS;
 						}
 					}
 				}
 				
 				monitor.worked(1);
 
-				// TODO DEBUG
-				System.err.println("SORT");
-
-				Collections.sort(konsultationen, new Comparator<Konsultation>() {
+				Collections.sort(konsList, new Comparator<Konsultation>() {
 					TimeTool t1=new TimeTool();
 					TimeTool t2=new TimeTool();
 					public int compare(final Konsultation o1, final Konsultation o2) {
@@ -548,13 +185,28 @@ public class KonsListDisplay extends Composite implements BackgroundJobListener 
 					}
 				});
 
-				// TODO DEBUG
-				System.err.println("FINISHED");
+				
+				if (monitor.isCanceled()) {
+					monitor.done();
+					return Status.CANCEL_STATUS;
+				}
+
+				// convert Konsultation objects to KonsData objects
+				
+				for (Konsultation k : konsList) {
+					KonsListComposite.KonsData ks = new KonsListComposite.KonsData(k);
+					konsDataList.add(ks);
+				}
 
 				monitor.worked(1);
 				monitor.done();
-				result = konsultationen;
-				return Status.OK_STATUS;
+
+				if (monitor.isCanceled()) {
+					return Status.CANCEL_STATUS;
+				} else {
+					result = konsDataList;
+					return Status.OK_STATUS;
+				}
 			}
 		}
 		
@@ -571,6 +223,15 @@ public class KonsListDisplay extends Composite implements BackgroundJobListener 
 			
 			// number of work steps in execute()
 			return 2;
+		}
+		
+		public List<KonsListComposite.KonsData> getKonsultationen() {
+			Object data = getData();
+			if (data instanceof List) {
+				return (List<KonsListComposite.KonsData>) data;
+			} else {
+				return null;
+			}
 		}
 	}
 	
