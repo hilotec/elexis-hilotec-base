ed5fc4358ddf097fb1bb1cdea6ba07b5b662e5e6
rgw_ch <devnull@localhost>
2007-09-06 18:27:41 +0200

    [svn] Initial import.

---
+++ elexis-medikamente-bag/src/ch/elexis/medikamente/bag/BAGMediImporter.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Copyright (c) 2007, G. Weirich and Elexis
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    G. Weirich - initial implementation
+ *    
+ *  $Id$
+ *******************************************************************************/
+package ch.elexis.medikamente.bag;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.swt.widgets.Composite;
+
+import ch.elexis.util.ImporterPage;
+
+public class BAGMediImporter extends ImporterPage {
+
+	public BAGMediImporter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Composite createPage(Composite parent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IStatus doImport(IProgressMonitor monitor) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getTitle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
