86a79194d4536b7148dd206936517f823cf3251e
rgw_ch <devnull@localhost>
2007-09-07 07:14:37 +0200

    [svn] Medikamente BAG weiter

--- elexis-medikamente-bag/src/ch/elexis/medikamente/bag/data/BAGMedi.java
+++ elexis-medikamente-bag/src/ch/elexis/medikamente/bag/data/BAGMedi.java
@@ -8,11 +8,21 @@
  * Contributors:
  *    G. Weirich - initial implementation
  *    
- *  $Id: BAGMedi.java 3103 2007-09-06 18:56:55Z rgw_ch $
+ *  $Id: BAGMedi.java 3106 2007-09-07 05:14:37Z rgw_ch $
  *******************************************************************************/
 package ch.elexis.medikamente.bag.data;
 
+import java.io.ByteArrayInputStream;
+import java.io.UnsupportedEncodingException;
+
+import org.eclipse.jface.dialogs.MessageDialog;
+
 import ch.elexis.data.Artikel;
+import ch.elexis.data.Organisation;
+import ch.elexis.data.Query;
+import ch.elexis.data.Xid;
+import ch.elexis.util.SWTHelper;
+import ch.rgw.tools.VersionInfo;
 
 /**
  * This Article is a medicament taken from the BAG (Swiss federal dep. of health)
@@ -20,5 +30,89 @@ import ch.elexis.data.Artikel;
  *
  */
 public class BAGMedi extends Artikel {
+	//static final String EXTTABLE="CH_ElEXIS_MEDIKAMENTE_BAG_EXT";
+	static final String JOINTTABLE="CH_ELEXIS_MEDIKAMENTE_BAG_JOINT";
+	static final String VERSION="0.1.0";
+	/*
+	static final String extDB="CREATE TABLE "+EXTTABLE+" ("
+		+"ID				VARCHAR(25) primary key,"
+		+"deleted			CHAR(1) default '0',"
+		+"generika			CHAR(1),"
+		+"swissmedicliste	CHAR(1),"
+		+"limitation		CHAR(1),"
+		+"hersteller		VARCHAR(25),"
+		+"bagdossier		VARCHAR(10),"
+		+"swissmedicnr		VARCHAR(10),"
+		+"limitationpts		VARCHAR(10)"
+		+");";
+	*/
+	static final String jointDB="CREATE TABLE "+JOINTTABLE+"("
+		+"product			VARCHAR(25),"
+		+"substance         VARCHAR(25)"
+		+");"
+		+"INSERT INTO "+JOINTTABLE+" (product,substance) VALUES('VESRION','"+VERSION+"');";
 	
+	public static final String CODESYSTEMNAME="Medikamente";
+	public static final String DOMAIN_PHARMACODE="www.xid.ch/id/pk";
+	
+	static{
+		addMapping(Artikel.TABLENAME,"");
+		Xid.localRegisterXIDDomainIfNotExists(DOMAIN_PHARMACODE	, Xid.ASSIGNEMENT_REGIONAL);
+		String v=j.queryString("SELECT substance FROM "+JOINTTABLE+" WHERE product='VERSION';");
+		if(v==null){
+			createTable("BAGMedi",jointDB);
+		}else{
+			VersionInfo vi=new VersionInfo(v);
+			if(vi.isOlder(VERSION)){
+				SWTHelper.showError("Datenbank Fehler", "Die Versin von BAG-Medi ist zu alt");
+			}
+		}
+			
+	}
+	
+	/**
+	 * Create a BAGMEdi from a line of the BAG file
+	 * @param row the line
+	 */
+	public BAGMedi(String name, String pharmacode){
+		super(name,CODESYSTEMNAME,pharmacode);
+	}
+	
+	public void update(String[] row){
+		Query<Organisation> qo=new Query<Organisation>(Organisation.class);
+		String id=qo.findSingle("Name","=", row[0]);
+		if(id==null){
+			Organisation o=new Organisation(row[0],"Pharma");
+			id=o.getId();
+		}
+		setExt("HerstellerID", id);
+		setExt("Generika",row[1]);
+		setExt("Pharmacode",row[2]);
+		
+	}
+	@Override
+	protected String getConstraint(){
+		return "Typ=Medikament";
+	}
+	
+	@Override
+	protected void setConstraint(){
+		set("Typ","Medikament");
+	}
+
+	@Override
+	public String getCodeSystemName() {
+		return CODESYSTEMNAME;
+	}
+	
+	
+	
+	public static BAGMedi load(String id){
+		return new BAGMedi(id);
+	}
+	protected BAGMedi(String id){
+		super(id);
+	}
+	protected BAGMedi(){
+	}
 }
--- elexis-medikamente-bag/src/ch/elexis/medikamente/bag/data/BAGMediImporter.java
+++ elexis-medikamente-bag/src/ch/elexis/medikamente/bag/data/BAGMediImporter.java
@@ -8,14 +8,20 @@
  * Contributors:
  *    G. Weirich - initial implementation
  *    
- *  $Id: BAGMediImporter.java 3103 2007-09-06 18:56:55Z rgw_ch $
+ *  $Id: BAGMediImporter.java 3106 2007-09-07 05:14:37Z rgw_ch $
  *******************************************************************************/
 package ch.elexis.medikamente.bag.data;
 
+import java.util.List;
+
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.swt.widgets.Composite;
 
+import ch.elexis.data.Artikel;
+import ch.elexis.data.Query;
+import ch.elexis.importers.ExcelWrapper;
 import ch.elexis.util.ImporterPage;
 
 public class BAGMediImporter extends ImporterPage {
@@ -32,8 +38,40 @@ public class BAGMediImporter extends ImporterPage {
 
 	@Override
 	public IStatus doImport(IProgressMonitor monitor) throws Exception {
-		
-		return null;
+		ExcelWrapper ew=new ExcelWrapper();
+		if(ew.load(results[0], 1)){
+			int f=ew.getFirstRow();
+			int l=ew.getLastRow();
+			monitor.beginTask("Import BAG-Medikamente", l-f);
+			for(int i=f;i<l;i++){
+				List<String> row=ew.getRow(i);
+				importUpdate(row.toArray(new String[0]));
+				monitor.worked(1);
+			}
+			monitor.done();
+			return Status.OK_STATUS;
+		}
+		return Status.CANCEL_STATUS;
+	}
+
+	/**
+	 * Import a medicament from one row of the BAG-Medi file
+	 * @param row
+	 * @return
+	 */
+	public static boolean importUpdate(String[] row){
+		String pharmacode=row[2];
+		Query<Artikel> qbe=new Query<Artikel>(Artikel.class);
+		String id=qbe.findSingle("SubID", "=", pharmacode);
+		BAGMedi imp;
+		if(id==null){
+			imp=new BAGMedi(row[7],pharmacode);
+		}else{
+			imp=BAGMedi.load(id);
+			imp.update(row);
+		}
+			
+		return true;
 	}
 
 	@Override
