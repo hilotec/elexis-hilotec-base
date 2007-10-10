acb4ae606094037443121cb76b344e519d92625e
rgw_ch <devnull@localhost>
2007-10-10 12:49:46 +0200

    [svn] Anpassungen an linux-build

--- BuildElexis/build.sh
+++ BuildElexis/build.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
-# $Id: build.sh 2548 2007-06-21 13:16:43Z rgw_ch $
+# $Id: build.sh 3247 2007-10-10 10:49:46Z rgw_ch $
 cd rsc/build
 #$ANT_HOME/bin/ant "Linux" -Dunplugged=1 
-$ANT_HOME/bin/ant "Linux"
+ant "Linux"
 cd ../..
