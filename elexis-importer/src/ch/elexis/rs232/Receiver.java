b8d1727ae140888dfb167bfdfadac2fa1c8bfd54
rgw_ch <devnull@localhost>
2007-10-12 17:17:51 +0200

    [svn] serial port steuerung generalisiert

---
+++ elexis-importer/src/ch/elexis/rs232/Receiver.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2007, G. Weirich and Elexis
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    G. Weirich - initial implementation
+ *    
+ * $Id$
+ *******************************************************************************/
+
+package ch.elexis.rs232;
+
+public interface Receiver {
+	public void dataReceived(Controller controller, String data);
+	public void breakReceived(Controller controller);
+}
