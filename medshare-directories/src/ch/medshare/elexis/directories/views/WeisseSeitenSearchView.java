d205edbc6055249db7caaafb34e64d7e1e415be0
rgw_ch <devnull@localhost>
2007-10-26 06:37:22 +0200

    [svn] copyright notice und keywords gesetzt

--- medshare-directories/src/ch/medshare/elexis/directories/views/WeisseSeitenSearchView.java
+++ medshare-directories/src/ch/medshare/elexis/directories/views/WeisseSeitenSearchView.java
@@ -1,3 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2007, medshare and Elexis
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    M. Imhof - initial implementation
+ *    
+ * $Id: WeisseSeitenSearchView.java 3286 2007-10-26 04:37:22Z rgw_ch $
+ *******************************************************************************/
+
 package ch.medshare.elexis.directories.views;
 
 import java.util.Iterator;
