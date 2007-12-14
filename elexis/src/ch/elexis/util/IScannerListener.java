08ee8dd91c6ca3657ecb76b3c560969d6def1ea6
michael_imhof <devnull@localhost>
2007-12-14 09:22:53 +0100

    [svn]

---
+++ elexis/src/ch/elexis/util/IScannerListener.java
@@ -0,0 +1,9 @@
+package ch.elexis.util;
+
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Widget;
+
+public interface IScannerListener {
+
+	public void scannerInput(Event e);
+}
