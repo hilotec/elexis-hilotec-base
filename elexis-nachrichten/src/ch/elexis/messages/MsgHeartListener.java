633604fdeafbd682bb1fd3dcae65ac68a7a66fca
rgw_ch <devnull@localhost>
2008-02-16 12:46:42 +0100

    [svn] div Fehlerkorrekturen

--- elexis-nachrichten/src/ch/elexis/messages/MsgHeartListener.java
+++ elexis-nachrichten/src/ch/elexis/messages/MsgHeartListener.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, G. Weirich and Elexis
+ * Copyright (c) 2007-2008, G. Weirich and Elexis
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,7 +8,7 @@
  * Contributors:
  *    G. Weirich - initial implementation
  *    
- * $Id: MsgHeartListener.java 3089 2007-09-03 15:56:23Z rgw_ch $
+ * $Id: MsgHeartListener.java 3684 2008-02-16 11:46:42Z rgw_ch $
  *******************************************************************************/
 
 package ch.elexis.messages;
@@ -25,18 +25,20 @@ public class MsgHeartListener implements HeartListener {
 	
 	public void heartbeat() {
 		if(!bSkip){
-			Query<Message> qbe=new Query<Message>(Message.class);
-			qbe.add("to", "=", Hub.actUser.getId());
-			final List<Message> res=qbe.execute();
-			if(res.size()>0){
-				Desk.theDisplay.asyncExec(new Runnable(){
-					public void run() {
-						bSkip=true;
-						new MsgDetailDialog(Hub.getActiveShell(),res.get(0)).open();
-						bSkip=false;
-					}
-				});
-				
+			if(Hub.actUser!=null){
+				Query<Message> qbe=new Query<Message>(Message.class);
+				qbe.add("to", "=", Hub.actUser.getId());
+				final List<Message> res=qbe.execute();
+				if(res.size()>0){
+					Desk.theDisplay.asyncExec(new Runnable(){
+						public void run() {
+							bSkip=true;
+							new MsgDetailDialog(Hub.getActiveShell(),res.get(0)).open();
+							bSkip=false;
+						}
+					});
+					
+				}
 			}
 		}
 	}
