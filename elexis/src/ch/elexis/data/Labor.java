bf03e24d12699606941a10f14415f96d348f1753
danlutz <devnull@localhost>
2008-02-28 19:05:44 +0100

    [svn] Corrected previous correction

--- elexis/src/ch/elexis/data/Labor.java
+++ elexis/src/ch/elexis/data/Labor.java
@@ -1,14 +1,14 @@
 /*******************************************************************************
  * Copyright (c) 2005-2006, G. Weirich and Elexis
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Egclipse Public License v1.0
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
  *    G. Weirich - initial implementation
  *    
- *  $Id: Labor.java 3713 2008-02-28 18:02:52Z danlutz $
+ *  $Id: Labor.java 3714 2008-02-28 18:05:44Z danlutz $
  *******************************************************************************/
 
 package ch.elexis.data;
