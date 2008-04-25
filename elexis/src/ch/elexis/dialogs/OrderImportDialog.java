9ddca604d69729ad63db776b1afc7269144995f0
danlutz <devnull@localhost>
2008-04-25 14:03:12 +0200

    [svn] Initial import

---
+++ elexis/src/ch/elexis/dialogs/OrderImportDialog.java
@@ -0,0 +1,481 @@
+/*******************************************************************************
+ * Copyright (c) 2005-2007, G. Weirich and Elexis
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    D. Lutz - initial implementation
+ *    
+ * $Id$
+ *******************************************************************************/
+package ch.elexis.dialogs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.jface.dialogs.IInputValidator;
+import org.eclipse.jface.dialogs.InputDialog;
+import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.jface.viewers.CellEditor;
+import org.eclipse.jface.viewers.CheckboxCellEditor;
+import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.eclipse.jface.viewers.ColumnViewerEditor;
+import org.eclipse.jface.viewers.ColumnViewerEditorActivationEvent;
+import org.eclipse.jface.viewers.ColumnViewerEditorActivationStrategy;
+import org.eclipse.jface.viewers.DoubleClickEvent;
+import org.eclipse.jface.viewers.EditingSupport;
+import org.eclipse.jface.viewers.FocusCellOwnerDrawHighlighter;
+import org.eclipse.jface.viewers.IDoubleClickListener;
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.jface.viewers.TableViewerEditor;
+import org.eclipse.jface.viewers.TableViewerFocusCellManager;
+import org.eclipse.jface.viewers.TextCellEditor;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Spinner;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.Text;
+
+import ch.elexis.data.Artikel;
+import ch.elexis.data.Bestellung;
+import ch.elexis.util.SWTHelper;
+ 
+/*
+ * @author Daniel Lutz
+ *
+ */
+public class OrderImportDialog extends TitleAreaDialog {
+	/**
+	 * Order to act on
+	 */
+	private List<OrderElement> orderElements;
+	
+	private Spinner diffSpinner;
+	private Text eanText;
+	
+	private TableViewer viewer;
+	
+	public OrderImportDialog(Shell parentShell, Bestellung bestellung) {
+		super(parentShell);
+		
+		setShellStyle(getShellStyle() | SWT.SHELL_TRIM);
+		
+		orderElements = new ArrayList<OrderElement>();
+		List<Bestellung.Item> items = bestellung.asList();
+		for (Bestellung.Item item : items) {
+			OrderElement orderElement = new OrderElement(item.art, item.num);
+			orderElements.add(orderElement);
+		}
+	}
+
+	@Override
+	protected Control createDialogArea(Composite parent) {
+		Composite mainArea = new Composite(parent, SWT.NONE);
+		mainArea.setLayoutData(SWTHelper.getFillGridData(1, true, 1, true));
+		/*DEBUG*/mainArea.setBackground(mainArea.getDisplay().getSystemColor(SWT.COLOR_RED));
+		mainArea.setLayout(new GridLayout());
+		
+		Composite scannerArea = new Composite(mainArea, SWT.NONE);
+		scannerArea.setLayoutData(SWTHelper.getFillGridData(1, true, 1, false));
+		scannerArea.setLayout(new GridLayout());
+		
+		Group scannerGroup =  new Group(scannerArea, SWT.NONE);
+		scannerGroup.setLayoutData(SWTHelper.getFillGridData(1, true, 1, false));
+		scannerGroup.setLayout(new GridLayout(4, false));
+		scannerGroup.setText("Einlesen mit Barcode-Scanner");
+		
+		diffSpinner = new Spinner(scannerGroup, SWT.NONE);
+		diffSpinner.setMinimum(1);
+		diffSpinner.setSelection(1);
+		
+		Label eanLabel = new Label(scannerGroup, SWT.NONE);
+		eanLabel.setText("EAN:");
+		eanText = new Text(scannerGroup, SWT.NONE);
+		eanText.setLayoutData(SWTHelper.getFillGridData(1, true, 1, false));
+		Button button = new Button(scannerGroup, SWT.PUSH);
+		button.setText("Übernehmen");
+		button.addSelectionListener(new SelectionListener() {
+			public void widgetSelected(SelectionEvent e) {
+				applyScanner();
+			}
+			
+			public void widgetDefaultSelected(SelectionEvent e) {
+				// do nothing
+			}
+		});
+		
+		Composite tableArea = new Composite(mainArea, SWT.NONE);
+		tableArea.setLayoutData(SWTHelper.getFillGridData(1, true, 1, true));
+		tableArea.setLayout(new GridLayout());
+		
+		viewer = new TableViewer(tableArea, SWT.FULL_SELECTION);
+		Table table = viewer.getTable();
+		table.setLayoutData(SWTHelper.getFillGridData(1, true, 1, true));
+		table.setLinesVisible(true);
+		table.setHeaderVisible(true);
+		
+		final TableViewerFocusCellManager mgr = new TableViewerFocusCellManager(viewer,new FocusCellOwnerDrawHighlighter(viewer));
+		ColumnViewerEditorActivationStrategy actSupport = new ColumnViewerEditorActivationStrategy(viewer) {
+			protected boolean isEditorActivationEvent(
+					ColumnViewerEditorActivationEvent event) {
+				return event.eventType == ColumnViewerEditorActivationEvent.TRAVERSAL
+						|| event.eventType == ColumnViewerEditorActivationEvent.MOUSE_DOUBLE_CLICK_SELECTION
+						|| (event.eventType == ColumnViewerEditorActivationEvent.KEY_PRESSED && ( event.keyCode == SWT.CR || event.character == ' ' ))
+						|| event.eventType == ColumnViewerEditorActivationEvent.PROGRAMMATIC;
+			}
+		};
+
+		TableViewerEditor.create(viewer, mgr, actSupport, ColumnViewerEditor.TABBING_HORIZONTAL
+				| ColumnViewerEditor.TABBING_MOVE_TO_ROW_NEIGHBOR
+				| ColumnViewerEditor.TABBING_VERTICAL | ColumnViewerEditor.KEYBOARD_ACTIVATION);
+
+		createViewerColumns();
+
+		viewer.setContentProvider(new ViewerContentProvider());
+		viewer.setInput(this);
+		
+		Button importButton = new Button(mainArea, SWT.PUSH);
+		importButton.setText("Lagerbestände anpassen");
+		importButton.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				doImport();
+			}
+		});
+		
+		return mainArea;
+	}
+	
+	private void createViewerColumns() {
+		TableViewerColumn column;
+		
+		final CheckboxCellEditor checkboxCellEditor = new CheckboxCellEditor(viewer.getTable());
+		final TextCellEditor textCellEditor = new TextCellEditor(viewer.getTable());
+		
+		/* OK (checkbox column) */
+		column = new TableViewerColumn(viewer, SWT.LEFT);
+		column.getColumn().setText("OK");
+		column.getColumn().setWidth(50);
+		column.setLabelProvider(new CheckboxLabelProvider());
+		column.setEditingSupport(new EditingSupport(viewer) {
+			public boolean canEdit(Object element) {
+				return true;
+			}
+			
+			public CellEditor getCellEditor(Object element) {
+				return checkboxCellEditor;
+			}
+			
+			public Object getValue(Object element) {
+				if (element instanceof OrderElement) {
+					OrderElement orderElement = (OrderElement) element;
+					return new Boolean(orderElement.isVerified());
+				} else {
+					return null;
+				}
+			}
+			
+			public void setValue(Object element, Object value) {
+				if (element instanceof OrderElement) {
+					OrderElement orderElement = (OrderElement) element;
+					if (value instanceof Boolean) {
+						Boolean bValue = (Boolean) value;
+						orderElement.setVeified(bValue.booleanValue());
+					}
+					viewer.update(orderElement, null);
+				}
+			}
+		});
+
+		/* Amount */
+		column = new TableViewerColumn(viewer, SWT.LEFT);
+		column.getColumn().setText("Menge");
+		column.getColumn().setWidth(50);
+		column.setLabelProvider(new AmountLabelProvider());
+		column.setEditingSupport(new EditingSupport(viewer) {
+			public boolean canEdit(Object element) {
+				return true;
+			}
+			
+			public CellEditor getCellEditor(Object element) {
+				return textCellEditor;
+			}
+			
+			public Object getValue(Object element) {
+				if (element instanceof OrderElement) {
+					OrderElement orderElement = (OrderElement) element;
+					return orderElement.getAmountAsString();
+				} else {
+					return null;
+				}
+			}
+			
+			public void setValue(Object element, Object value) {
+				if (element instanceof OrderElement) {
+					OrderElement orderElement = (OrderElement) element;
+					if (value instanceof String) {
+						String text = (String) value;
+						try {
+							int amount = Integer.parseInt(text);
+							orderElement.setAmount(amount);
+							orderElement.setVeified(true);
+						} catch (NumberFormatException ex) {
+							// ignore invalid value
+						}
+					}
+					viewer.update(orderElement, null);
+				}
+			}
+		});
+
+		/* Amount */
+		column = new TableViewerColumn(viewer, SWT.LEFT);
+		column.getColumn().setText("Lager");
+		column.getColumn().setWidth(50);
+		column.setLabelProvider(new StockLabelProvider());
+
+		/* Pharamcode */
+		column = new TableViewerColumn(viewer, SWT.LEFT);
+		column.getColumn().setText("Pharmacode");
+		column.getColumn().setWidth(80);
+		column.setLabelProvider(new PharamcodeLabelProvider());
+
+		/* EAN */
+		column = new TableViewerColumn(viewer, SWT.LEFT);
+		column.getColumn().setText("EAN");
+		column.getColumn().setWidth(100);
+		column.setLabelProvider(new EANLabelProvider());
+
+		/* Description */
+		column = new TableViewerColumn(viewer, SWT.LEFT);
+		column.getColumn().setText("Beschreibung");
+		column.getColumn().setWidth(300);
+		column.setLabelProvider(new DescriptionLabelProvider());
+
+	}
+
+	@Override
+	public void create() {
+		super.create();
+		setTitle("Bestellung einlesen");
+		setMessage("Bitte überprüfen Sie alle bestellten Artikel."
+				+ " Überprüfte Artikel werden grün angezeigt."
+				+ " Bei der Anpassung der Bestände werden nur jene Artikel"
+				+ " berücksichtigt, bei denen unter \"OK\" ein Haken gesetzt ist.");
+		//setTitleImage(...));
+		getShell().setText("Bestellung einlesen");
+	}
+
+	@Override
+	protected void okPressed() {
+		super.okPressed();
+	}
+
+	// update the table according to the input from the scanner
+	private void applyScanner() {
+		int diff = diffSpinner.getSelection();
+		String ean = eanText.getText().trim();
+		eanText.setText("");
+
+		OrderElement orderElement = findOrderElementByEAN(ean);
+		if (orderElement == null) {
+			SWTHelper.alert("Artikel nicht bestellt", "Diser Artikel wurde nicht bestellt. Der Bestand kann nicht automatisch angepasst werden.");
+		}
+		
+		int newAmount = orderElement.getAmount() + diff;
+		
+		updateOrderElement(orderElement, newAmount);
+	}
+
+	private OrderElement findOrderElementByEAN(String ean) {
+		if (ean == null) {
+			return null;
+		}
+		
+		for (OrderElement orderElement : orderElements) {
+			if (orderElement.getArtikel().getEAN().equals(ean)) {
+				return orderElement;
+			}
+		}
+		
+		// not found
+		return null;
+	}
+	
+	private void updateOrderElement(OrderElement orderElement, int newAmount) {
+		orderElement.setAmount(newAmount);
+		orderElement.setVeified(true);
+		viewer.update(orderElement, null);
+	}
+	
+	// read in verified order
+	public void doImport() {
+		try {
+			for (OrderElement orderElement : orderElements) {
+				if (orderElement.isVerified()) {
+					Artikel artikel = orderElement.getArtikel();
+					int diff = orderElement.getAmount();
+					int oldAmount = artikel.getIstbestand();
+					int newAmount = oldAmount + diff;
+					artikel.setIstbestand(newAmount);
+
+					// reset amount
+					orderElement.setAmount(0);
+					orderElement.setVeified(false);
+				}
+			}
+		} catch(Exception ex){
+			SWTHelper.showError("Fehler bei Anpassung der Bestände", "Bestände konnten teilweise nicht korrekt angepasst werden.");
+		}
+		
+		viewer.refresh();
+	}
+
+	class ViewerContentProvider implements IStructuredContentProvider {
+		public Object[] getElements(Object inputElement) {
+			return orderElements.toArray();
+		}
+		
+		public void dispose() {
+			// do nothing
+		}
+		
+		public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+			// do nothing
+		}
+	}
+
+	class CheckboxLabelProvider extends ColumnLabelProvider {
+		public String getText(Object element) {
+			String text = "";
+			
+			if (element instanceof OrderElement) {
+				OrderElement orderElement = (OrderElement) element;
+				if (orderElement.isVerified()) {
+					text = "X";
+				}
+			}
+			
+			return text;
+		}
+	}
+
+	class AmountLabelProvider extends ColumnLabelProvider {
+		public String getText(Object element) {
+			String text = "";
+			
+			if (element instanceof OrderElement) {
+				OrderElement orderElement = (OrderElement) element;
+				text = orderElement.getAmountAsString();
+			}
+			
+			return text;
+		}
+	}
+
+	class StockLabelProvider extends ColumnLabelProvider {
+		public String getText(Object element) {
+			String text = "";
+			
+			if (element instanceof OrderElement) {
+				OrderElement orderElement = (OrderElement) element;
+				text = new Integer(orderElement.getArtikel().getIstbestand()).toString();
+			}
+			
+			return text;
+		}
+	}
+
+	class PharamcodeLabelProvider extends ColumnLabelProvider {
+		public String getText(Object element) {
+			String text = "";
+			
+			if (element instanceof OrderElement) {
+				OrderElement orderElement = (OrderElement) element;
+				text = orderElement.artikel.getPharmaCode();
+			}
+			
+			return text;
+		}
+	}
+
+	class EANLabelProvider extends ColumnLabelProvider {
+		public String getText(Object element) {
+			String text = "";
+			
+			if (element instanceof OrderElement) {
+				OrderElement orderElement = (OrderElement) element;
+				text = orderElement.artikel.getEAN();
+			}
+			
+			return text;
+		}
+	}
+
+	class DescriptionLabelProvider extends ColumnLabelProvider {
+		public String getText(Object element) {
+			String text = "";
+			
+			if (element instanceof OrderElement) {
+				OrderElement orderElement = (OrderElement) element;
+				text = orderElement.artikel.getName();
+			}
+			
+			return text;
+		}
+	}
+
+	class OrderElement {
+		private boolean verified = false;
+		
+		private Artikel artikel;
+		private int amount;
+
+		OrderElement(Artikel artikel, int amount) {
+			this.artikel = artikel;
+			this.amount = amount;
+		}
+		
+		int getAmount() {
+			return amount;
+		}
+		
+		/**
+		 * Set new amount. Sets verified to true.
+		 * @param amount the new amount
+		 */
+		void setAmount(int amount) {
+			this.amount = amount;
+		}
+		
+		Artikel getArtikel() {
+			return artikel;
+		}
+		
+		String getAmountAsString() {
+			return new Integer(amount).toString();
+		}
+		
+		boolean isVerified() {
+			return verified;
+		}
+		
+		void setVeified(boolean verified) {
+			this.verified = verified;
+		}
+	}
+}
