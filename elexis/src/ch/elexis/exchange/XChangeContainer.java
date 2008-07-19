91077073fe41653e586c11c112721e79d062891e
rgw_ch <devnull@localhost>
2008-07-19 17:04:37 +0200

    [svn] cleanup

--- elexis/src/ch/elexis/exchange/XChangeContainer.java
+++ elexis/src/ch/elexis/exchange/XChangeContainer.java
@@ -1,10 +1,11 @@
 package ch.elexis.exchange;
 
-import org.jdom.Namespace;
+import java.util.List;
 
-public interface XChangeContainer {
-	public static final Namespace ns=Namespace.getNamespace("SgamXChange","http://informatics.sgam.ch/eXChange");
-	public static final Namespace nsxsi=Namespace.getNamespace("xsi","http://www.w3.org/2001/XML Schema-instance");
-	public static final Namespace nsschema=Namespace.getNamespace("schemaLocation","http://informatics.sgam.ch/eXChange SgamXChange.xsd");
+import org.jdom.Element;
+
+
+public interface XChangeContainer extends IDataSender, IDataReceiver{
+	
 
 }
