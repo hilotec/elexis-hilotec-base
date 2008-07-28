db2bd9c1aa31389bfd530aaa0713f935da115f19
rgw_ch <devnull@localhost>
2008-07-28 19:33:59 +0200

    [svn]

--- elexis-importer/src/ch/elexis/rs232/Connection.java
+++ elexis-importer/src/ch/elexis/rs232/Connection.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, G. Weirich and Elexis
+ * Copyright (c) 2007-2008, G. Weirich and Elexis
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,7 +8,7 @@
  * Contributors:
  *    G. Weirich - initial implementation
  *    
- * $Id: Connection.java 3471 2007-12-20 20:57:02Z rgw_ch $
+ * $Id: Connection.java 4194 2008-07-28 17:33:59Z rgw_ch $
  *******************************************************************************/
 
 package ch.elexis.rs232;
@@ -285,7 +285,7 @@ public class Connection implements SerialPortEventListener{
 					while((newData=is.read()) != -1){
     					if(newData==frameStart){
     						state=AWAIT_END;
-    						sbFrame.append(frameStart);
+    						sbFrame.append((char)frameStart);
     						serialEvent(e);
     					}
     				}
