2499ca36f26fd2cfb61f77b76d4c4fd5aeaaa3ae
rgw_ch <devnull@localhost>
2008-08-13 16:41:33 +0200

    [svn]

--- elexis/src/ch/elexis/data/Labor.java
+++ elexis/src/ch/elexis/data/Labor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005-2006, G. Weirich and Elexis
+ * Copyright (c) 2005-2008, G. Weirich and Elexis
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,42 +8,43 @@
  * Contributors:
  *    G. Weirich - initial implementation
  *    
- *  $Id: Labor.java 3714 2008-02-28 18:05:44Z danlutz $
+ *  $Id: Labor.java 4270 2008-08-13 14:41:33Z rgw_ch $
  *******************************************************************************/
 
 package ch.elexis.data;
 
 public class Labor extends Organisation {
-	static{
-		addMapping("KONTAKT",
-				"Name	=Bezeichnung1",
-				"Zusatz1=Bezeichnung2",
-				"Zusatz2=ExtInfo",
-				"Kuerzel=PatientNr",
-				"Ansprechperson=Bezeichnung3","istOrganisation","istLabor"
-				);
+	static {
+		addMapping("KONTAKT", "Name	=Bezeichnung1", "Zusatz1=Bezeichnung2", "Zusatz2=ExtInfo", "Kuerzel=PatientNr", "Ansprechperson=Bezeichnung3", "istOrganisation", "istLabor");
 	}
+
 	@Override
 	protected String getConstraint() {
 		return "istLabor='1'";
 	}
+
 	@Override
 	protected void setConstraint() {
-		set(new String[]{"istLabor","istOrganisation"},"1","1");
+		set(new String[] { "istLabor", "istOrganisation" }, "1", "1");
 	}
-	public Labor(String Kuerzel,String Name){
-		super(Name,"Labor");
-		set("Kuerzel",Kuerzel);
+
+	public Labor(String Kuerzel, String Name) {
+		super(Name, "Labor");
+		set("Kuerzel", Kuerzel);
 	}
-	public static Labor load(String id){
-		 Labor ret=new Labor(id);
-		 if(ret.exists()){
-	        return ret;
-		 }
-		 return null;
+
+	public static Labor load(String id) {
+		Labor ret = new Labor(id);
+		if (ret.exists()) {
+			return ret;
+		}
+		return null;
 	}
-	protected Labor(String id){
+
+	protected Labor(String id) {
 		super(id);
 	}
-	protected Labor(){}
+
+	protected Labor() {
+	}
 }
