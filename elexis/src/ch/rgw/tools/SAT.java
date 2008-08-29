25bbeeba738b6e86a5dfc138182fda28eff45ce6
rgw_ch <devnull@localhost>
2008-08-29 18:44:34 +0200

    [svn] bridge tools aus elexis zweig

---
+++ elexis/src/ch/rgw/tools/SAT.java
@@ -0,0 +1,131 @@
+/*******************************************************************************
+ * Copyright (c) 2008, G. Weirich and Elexis
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    G. Weirich - initial implementation
+ *    
+ * $Id: SAT.java 4317 2008-08-27 05:19:24Z rgw_ch $
+ *******************************************************************************/
+
+package ch.rgw.tools;
+
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+/**
+ * Secure Authenticated Transmission
+ * The Transmitter stores a hashtable in an encrypted and signed wrapper.
+ * For best compatibility we use SOAP messages encrypted with GnuPG
+ * @author Gerry
+ *
+ */
+public class SAT {
+	public static final String ADM_TIMESTAMP="ADM_timestamp";
+	public static final String ADM_SIGNED_BY="ADM_user";
+	static final Pattern signatureEN=Pattern.compile(".+gpg: good signature from .+<(.+)>.*",Pattern.DOTALL|Pattern.CASE_INSENSITIVE);
+	static final Pattern signatureDE=Pattern.compile(".+gpg: Korrekte Unterschrift von .+<(.+)>.*",Pattern.DOTALL|Pattern.CASE_INSENSITIVE);
+	GnuPG gpg;
+	String userKey;
+	/**
+	 * Create a new SAT actor
+	 * @param gpg a fully configured GnuPG
+	 * @param sender the keyname of the sender.
+	 */
+	public SAT(GnuPG gpg, String userKey){
+		this.gpg=gpg;
+		this.userKey=userKey;
+	}
+
+	
+	public Result<HashMap<String, Object>>unwrap(String encrypted, char[] pwd){
+		if(gpg.decrypt(encrypted, new String(pwd))){
+			String dec=gpg.getResult();
+			String msg=gpg.getErrorString();
+			Matcher matcher=signatureEN.matcher(msg);
+			String user=null;
+			if(matcher.matches()){
+				user=matcher.group(1);
+			}else{
+				matcher=signatureDE.matcher(msg);
+				if(matcher.matches()){
+					user=matcher.group(1).trim();
+				}
+			}
+			try{
+				SoapConverter sc=new SoapConverter();
+				sc.load(dec);
+				HashMap<String, Object> hash=sc.getParameters();
+				Long ts=(Long)hash.get(ADM_TIMESTAMP);
+				if(ts==null || ((System.currentTimeMillis()-ts)>300000)){
+					return new Result<HashMap<String, Object>>(Result.SEVERITY.ERROR,3,"Timeout",null,false);
+				}
+				if(user==null){
+					hash.remove(ADM_SIGNED_BY);
+				}else{
+					hash.put(ADM_SIGNED_BY, user);
+				}
+				return new Result<HashMap<String, Object>>(hash);
+			}catch(Exception ex){
+				ExHandler.handle(ex);
+				return new Result<HashMap<String, Object>>(Result.SEVERITY.ERROR,2,"Deserialize error "+ex.getMessage(),null,true);
+			}
+		}
+		return new Result<HashMap<String, Object>>(Result.SEVERITY.ERROR,1,"Decode error",null,true);
+	}
+	
+	/**
+	 * Transmit a hashtable. 
+	 * @param hash a hashtable containing arbitrary String/Object pairs. All objects must be 
+	 * Serializables. Keynames starting with ADM_ are reserved and must not be used.
+	 * @param dest the receiver. The Object will be encoded with the receiver's public key
+	 * @param pwd Password for the sender's private key
+	 * @return a String containig the ASCII-armored encrypted and signed hashtable or null on error 
+	 */
+	public String wrap(HashMap<String, Object> hash, String dest, char[] senderPwd){
+		try{
+			hash.put(ADM_TIMESTAMP, System.currentTimeMillis());
+			SoapConverter sc=new SoapConverter();
+			sc.create("xidClient","0.0.1","elexis.ch");
+			sc.addHashMap("params", hash);
+			if(gpg.signAndEncrypt(sc.toXML(), userKey, dest, new String(senderPwd))){
+				return gpg.getResult();
+			}
+		}catch(Exception ex){
+			ExHandler.handle(ex);
+		}
+		return null;
+	}
+	
+	public String sendRequest(String hostaddress, String request){
+		String output="";
+		try{
+			// Connect to server
+		    URLConnection conn = (new URL(hostaddress)).openConnection();
+		    
+		    // Get output stream
+		    conn.setDoOutput(true);
+		    OutputStreamWriter out = new OutputStreamWriter(conn.getOutputStream());
+		    
+		    String rx=new BASE64Encoder().encode(request.getBytes("utf-8"));
+		    out.write("request="+rx);
+		    out.close();
+		    byte[] in=new BASE64Decoder().decodeBuffer(conn.getInputStream());
+	    	return new String(in,"utf-8");
+		    
+		}catch(Exception ex){
+			ExHandler.handle(ex);
+		}
+	    return output;
+	}
+}
---
+++ elexis/src/ch/rgw/tools/SoapConverter.java
@@ -0,0 +1,151 @@
+package ch.rgw.tools;
+
+import java.io.CharArrayReader;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+import java.util.Map.Entry;
+
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.Namespace;
+import org.jdom.input.SAXBuilder;
+import org.jdom.output.Format;
+import org.jdom.output.XMLOutputter;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+import ch.rgw.tools.ExHandler;
+
+public class SoapConverter {
+	public static final Namespace ns=Namespace.getNamespace("soap", "http://www.w3.org/2001/12/soap-envelope");
+	public static final String TYPE_STRING="string";
+	public static final String TYPE_INTEGRAL="integral";
+	public static final String TYPE_FLOAT="float";
+	public static final String TYPE_ARRAY="array";
+	public static final String TYPE_HASH="hash";
+	
+	private Document doc;
+	private Element eRoot;
+	private Element eBody;
+	private boolean bValid;
+	
+	public SoapConverter(){
+	}
+	
+	
+	public boolean load(String input) {
+		SAXBuilder builder = new SAXBuilder();
+		try{
+			CharArrayReader car=new CharArrayReader(input.toCharArray());
+			doc = builder.build(car);
+			eRoot=doc.getRootElement();
+			bValid=true;
+		} catch (Exception e) {
+			ExHandler.handle(e);
+			bValid=false;
+		}
+		return bValid;
+		
+	}
+	
+	public HashMap<String, Object> getParameters(){
+		if(bValid){
+			HashMap<String, Object> ret=new HashMap<String, Object>();
+			Element body=eRoot.getChild("Body",ns);
+			List<Element> params=body.getChildren("parameter", ns);
+			for(Element param:params){
+				String type=param.getAttributeValue("type");
+				String s=param.getText();
+				Object res=null;
+				try{
+					if(type.equals(TYPE_STRING)){
+						res=s;
+					}else if(type.equals(TYPE_INTEGRAL)){
+						res=Long.parseLong(s);
+					}else if(type.equals(TYPE_FLOAT)){
+						res=Double.parseDouble(s);
+					}else if(type.equals(TYPE_ARRAY)){
+						res=new BASE64Decoder().decodeBuffer(s);
+					}else{
+						res="** unsupported type **";
+					}
+				}catch(Exception ex){
+					ExHandler.handle(ex);
+					res="** parse error **";
+				}
+				ret.put(param.getAttributeValue("name"), res);
+			}
+			return ret;
+		}
+		return null;
+	}
+	public void create(String creator, String version, String provider){
+		eRoot=new Element("Envelope",ns);
+		Element eHeader=new Element("Header",ns);
+		Element eID=new Element("Creator",ns);
+		eID.setAttribute("name",creator);
+		eID.setAttribute("version",version);
+		eID.setAttribute("provider",provider);
+		eHeader.addContent(eID);
+		eRoot.addContent(eHeader);
+		eBody=new Element("Body",ns);
+		eRoot.addContent(eBody);
+		doc=new Document(eRoot);
+		bValid=true;
+	}
+	
+	public String toXML(){
+		if(doc!=null && eRoot!=null){
+			Format format=Format.getPrettyFormat();
+			format.setEncoding("utf-8");
+			XMLOutputter xmlo=new XMLOutputter(format);
+			return xmlo.outputString(doc);
+		}
+		return null;
+	}
+	
+	private Element createParameter(String name, String type){
+		Element ret=new Element("parameter",ns);
+		ret.setAttribute("type",type);
+		ret.setAttribute("name",name);
+		eBody.addContent(ret);
+		return ret;
+	}
+	public void addString(String name, String s){
+		createParameter(name,TYPE_STRING).setText(s);
+	}
+	
+	public void addIntegral(String name, long x){
+		createParameter(name,TYPE_INTEGRAL).setText(Long.toString(x));
+	}
+	public void addFloat(String name, double x){
+		createParameter(name, TYPE_FLOAT).setText(Double.toString(x));
+	}	
+	
+	public void addArray(String name, byte[] arr){
+		String res=new BASE64Encoder().encode(arr);
+		createParameter(name,TYPE_ARRAY).setText(res);
+	}
+	public void addObject(String name, Object obj){
+		if(obj instanceof String){
+			addString(name,(String)obj);
+		}else if((obj instanceof Double) ||
+				 (obj instanceof Float)){
+			addFloat(name, (Double)obj);
+		}else if((obj instanceof Integer) ||
+				 (obj instanceof Long) ||
+				 (obj instanceof Byte) ){
+			addIntegral(name, (Long)obj);
+		}else if(obj instanceof byte[]){
+			addArray(name, (byte[])obj);
+		}
+	}
+	public void addHashMap(String name, HashMap<String, Object> hash){
+		//Element ret=createParameter(name, TYPE_HASH);
+		Set<Entry<String, Object>> entries=hash.entrySet();
+		for(Entry<String,Object> entry:entries){
+			addObject(entry.getKey(),entry.getValue());
+		}
+	}
+}
