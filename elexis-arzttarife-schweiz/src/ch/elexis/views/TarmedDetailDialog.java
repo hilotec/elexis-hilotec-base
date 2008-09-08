3be14a2bbdc29e84b2e73e39f07815834c3579af
rgw_ch <devnull@localhost>
2008-09-08 12:06:07 +0200

    [svn]

--- elexis-arzttarife-schweiz/src/ch/elexis/views/TarmedDetailDialog.java
+++ elexis-arzttarife-schweiz/src/ch/elexis/views/TarmedDetailDialog.java
@@ -1,8 +1,11 @@
 package ch.elexis.views;
 
 import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 
 import ch.elexis.data.TarmedLeistung;
@@ -18,9 +21,20 @@ public class TarmedDetailDialog extends Dialog {
 	}
 	@Override
 	protected Control createDialogArea(Composite parent) {
-		Composite ret=td.createDisplay(parent, null);
-		td.display(tl);
+		//Composite ret=td.createDisplay(parent, null);
+		//td.display(tl);
+		Composite ret=(Composite)super.createDialogArea(parent);
+		ret.setLayout(new GridLayout(3,true));
+		new Label(ret,SWT.NONE).setText("tpAL: "+Integer.toString(tl.getAL()));
+		new Label(ret,SWT.NONE).setText("TP: ");
 		return ret;
 	}
+	@Override
+	public void create(){
+		super.create();
+		getShell().setText("Tarmed-Details");
+	}
+
 	
 }
+
