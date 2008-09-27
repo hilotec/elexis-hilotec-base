29cdfbee75d0c38c474f59ea1e36d4a58513fbad
rgw_ch <devnull@localhost>
2008-09-27 11:28:08 +0200

    [svn]

--- TestElexis/ch/elexis/tests/TestSAT.java
+++ TestElexis/ch/elexis/tests/TestSAT.java
@@ -5,42 +5,46 @@ import java.util.HashMap;
 
 import junit.framework.TestCase;
 import ch.rgw.IO.FileTool;
-import ch.rgw.tools.GnuPG;
+import ch.rgw.crypt.Cryptologist;
+import ch.rgw.crypt.GnuPG;
+import ch.rgw.crypt.JCECrypter;
+import ch.rgw.crypt.SAT;
 import ch.rgw.tools.Result;
-import ch.rgw.tools.SAT;
 
 public class TestSAT extends TestCase {
 	static String homedir;
-	static GnuPG gpg;
-	static String encrypted;
+	Cryptologist crypt;
+	static byte[] encrypted;
 
 	public TestSAT(){
-		homedir=System.getenv("TEMP")+File.separator+"gpghome";
+		homedir=System.getenv("TEMP")+File.separator+"crypt";
 		File file=new File(homedir);
 		if(file.exists()){
 			FileTool.deltree(file.getAbsolutePath());
 		}
 		file.mkdirs();
+		/*
 		gpg=new GnuPG(false);
 		gpg.setExecutable("d:/apps/gpg/gpg.exe");
 		gpg.setHomedir(homedir);
 		gpg.generateKey("Alice Elexistesterin", "alice@elexis.ch", "aliceelexis".toCharArray(), "alice");
 		gpg.generateKey("Bob Elexistester", "bob@elexis.ch", "bobelexis".toCharArray(), "bob");
-	
+		*/
+		crypt=new JCECrypter();
 	}
 	
 	public void testWrap() throws Exception{
-		SAT sat=new SAT(gpg,"alice@elexis.ch");
+		SAT sat=new SAT(crypt,"alice@elexis.ch");
 		HashMap<String, Object> hash=new HashMap<String, Object>();
 		hash.put("test", "Ein Testtext");
-		String result=sat.wrap(hash, "bob@elexis.ch", "aliceelexis".toCharArray());
+		byte[] result=sat.wrap(hash, "bob@elexis.ch", "aliceelexis".toCharArray());
 		assertNotNull(result);
 		System.out.println(new String(result));
 		encrypted=result;
 	}
 	
 	public void testUnwrap() throws Exception{
-		SAT sat=new SAT(gpg,"bob@elexis.ch");
+		SAT sat=new SAT(crypt,"bob@elexis.ch");
 		Result<HashMap<String,Object>> result=sat.unwrap(encrypted,"bobelexis".toCharArray());
 		assertTrue(result.isOK());
 		HashMap<String, Object> res=result.get();
