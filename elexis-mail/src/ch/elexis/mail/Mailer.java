0af8984e549a1814963ef9ba29dd27e2ea2470cf
rgw_ch <devnull@localhost>
2008-09-27 21:51:35 +0200

    [svn] rgw.tools externalisiert

--- elexis-mail/src/ch/elexis/mail/Mailer.java
+++ elexis-mail/src/ch/elexis/mail/Mailer.java
@@ -9,104 +9,131 @@
  *    G. Weirich - initial implementation
  *    R. Zweifel - SMTP-Authentifizierung
  *    
- *  $Id: Mailer.java 4183 2008-07-27 12:06:16Z rgw_ch $
+ *  $Id: Mailer.java 4458 2008-09-27 19:51:35Z rgw_ch $
  *******************************************************************************/
 
 package ch.elexis.mail;
+
 import java.io.File;
-import java.io.IOException;
 import java.util.Properties;
 
 import javax.activation.DataHandler;
 import javax.activation.DataSource;
-import javax.mail.*;
-import javax.mail.internet.*;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import javax.mail.util.ByteArrayDataSource;
 
 import ch.elexis.Hub;
 import ch.elexis.util.Log;
-import ch.elexis.util.Result;
 import ch.rgw.tools.ExHandler;
+import ch.rgw.tools.Result;
 
 public class Mailer {
 	static Properties props;
-	
-	static{
+
+	static {
 		props = new Properties();
-	    props.put( "mail.smtp.host", Hub.localCfg.get(PreferenceConstants.MAIL_SMTP,Messages.Mailer_1) ); //$NON-NLS-1$
-	    props.put("mail.smtp.auth", Hub.localCfg.get(PreferenceConstants.MAIL_AUTH,Messages.Mailer_1));
-	    props.put("mail.smtp.port", Hub.localCfg.get(PreferenceConstants.MAIL_SMTPPORT,Messages.Mailer_1));
+		props
+				.put(
+						"mail.smtp.host", Hub.localCfg.get(PreferenceConstants.MAIL_SMTP, Messages.Mailer_1)); //$NON-NLS-1$
+		props.put("mail.smtp.auth", Hub.localCfg.get(
+				PreferenceConstants.MAIL_AUTH, Messages.Mailer_1));
+		props.put("mail.smtp.port", Hub.localCfg.get(
+				PreferenceConstants.MAIL_SMTPPORT, Messages.Mailer_1));
 	}
+
 	/**
 	 * Convenience-Methode um einfach schnell eine simple Text-Mail zu versenden
-	 * @param recipient	empfänger-Email
-	 * @param subject Betreff
-	 * @param message Nachrichtentext
-	 * @param from Absender
+	 * 
+	 * @param recipient
+	 *            empfänger-Email
+	 * @param subject
+	 *            Betreff
+	 * @param message
+	 *            Nachrichtentext
+	 * @param from
+	 *            Absender
 	 * @return Result.OK oder eine Feghlermeldung
 	 */
-	public static Result<String> postMail( String recipient, String subject, String message, String from ) {
-		try{
+	public static Result<String> postMail(String recipient, String subject,
+			String message, String from) {
+		try {
 			Session session = null;
 			Authenticator auth = new SMTPAuthenticator();
-		    if(props.getProperty("mail.smtp.auth").equals("true")){
-		    	session = Session.getDefaultInstance( props, auth );
-		    }else{
-		    	session = Session.getDefaultInstance( props);
-		    }
-			Message msg = new MimeMessage( session );
-		    InternetAddress addressFrom = new InternetAddress( from );
-		    msg.setFrom( addressFrom );
-		    InternetAddress addressTo = new InternetAddress( recipient );
-		    msg.setRecipient( Message.RecipientType.TO, addressTo );
-		    msg.setSubject( subject );
-		    msg.setContent( message, "text/plain" ); //$NON-NLS-1$		    
-		    Transport.send( msg );
-		    return new Result<String>("Ok"); //$NON-NLS-1$
-		}catch(Exception ex){
+			if (props.getProperty("mail.smtp.auth").equals("true")) {
+				session = Session.getDefaultInstance(props, auth);
+			} else {
+				session = Session.getDefaultInstance(props);
+			}
+			Message msg = new MimeMessage(session);
+			InternetAddress addressFrom = new InternetAddress(from);
+			msg.setFrom(addressFrom);
+			InternetAddress addressTo = new InternetAddress(recipient);
+			msg.setRecipient(Message.RecipientType.TO, addressTo);
+			msg.setSubject(subject);
+			msg.setContent(message, "text/plain"); //$NON-NLS-1$		    
+			Transport.send(msg);
+			return new Result<String>("Ok"); //$NON-NLS-1$
+		} catch (Exception ex) {
 			ExHandler.handle(ex);
-			return new Result<String>(Log.ERRORS,1,ex.getMessage(),Messages.Mailer_Error,true);
+			return new Result<String>(Result.SEVERITY.ERROR, 1, ex.getMessage(),
+					Messages.Mailer_Error, true);
 		}
 	}
-	
+
 	/**
-	 * Eine vorerst leere Mime/Multipart Nachricht erstellen. Danach kann beliebig oft unt
-	 * gemischt addTextPart(), addBinaryPart() und addFilePart() aufgerufen werden, und am Schluss
-	 * send()
-	 * @param subject Titel
-	 * @param from Absender
+	 * Eine vorerst leere Mime/Multipart Nachricht erstellen. Danach kann
+	 * beliebig oft unt gemischt addTextPart(), addBinaryPart() und
+	 * addFilePart() aufgerufen werden, und am Schluss send()
+	 * 
+	 * @param subject
+	 *            Titel
+	 * @param from
+	 *            Absender
 	 * @return die Message
 	 */
-	public Message createMultipartMessage(String subject, String from){
-		try{
+	public Message createMultipartMessage(String subject, String from) {
+		try {
 			Session session = null;
 			Authenticator auth = new SMTPAuthenticator();
-		    if(props.getProperty("mail.smtp.auth").equals("true")){
-		    	session = Session.getDefaultInstance( props, auth );
-		    }else{
-		    	session = Session.getDefaultInstance( props);
-		    }
-			Message msg=new MimeMessage(session);
-			InternetAddress addressFrom = new InternetAddress( from );
-		    msg.setFrom( addressFrom );
-		    msg.setSubject( subject );
-		    msg.setContent(new MimeMultipart());
-		    return msg;
-		}catch(MessagingException ex){
+			if (props.getProperty("mail.smtp.auth").equals("true")) {
+				session = Session.getDefaultInstance(props, auth);
+			} else {
+				session = Session.getDefaultInstance(props);
+			}
+			Message msg = new MimeMessage(session);
+			InternetAddress addressFrom = new InternetAddress(from);
+			msg.setFrom(addressFrom);
+			msg.setSubject(subject);
+			msg.setContent(new MimeMultipart());
+			return msg;
+		} catch (MessagingException ex) {
 			ExHandler.handle(ex);
 			return null;
 		}
 	}
+
 	/**
-	 * Einen TextPart an eine vorher mit createMultipartMessage erstellte Nachricht anhängen
-	 * @param msg die Nachricht
-	 * @param text der Text für den Part
+	 * Einen TextPart an eine vorher mit createMultipartMessage erstellte
+	 * Nachricht anhängen
+	 * 
+	 * @param msg
+	 *            die Nachricht
+	 * @param text
+	 *            der Text für den Part
 	 * @return true bei Erfolg
 	 */
-	public boolean addTextPart(Message msg, String text){
+	public boolean addTextPart(Message msg, String text) {
 		try {
-			MimeMultipart content=(MimeMultipart)msg.getContent();
-			MimeBodyPart textPart=new MimeBodyPart();
+			MimeMultipart content = (MimeMultipart) msg.getContent();
+			MimeBodyPart textPart = new MimeBodyPart();
 			textPart.setContent(text, "text/plain"); //$NON-NLS-1$
 			content.addBodyPart(textPart);
 			return true;
@@ -115,10 +142,11 @@ public class Mailer {
 		}
 		return false;
 	}
-	public boolean addTextPart(Message msg, String text, String name){
+
+	public boolean addTextPart(Message msg, String text, String name) {
 		try {
-			MimeMultipart content=(MimeMultipart)msg.getContent();
-			MimeBodyPart textPart=new MimeBodyPart();
+			MimeMultipart content = (MimeMultipart) msg.getContent();
+			MimeBodyPart textPart = new MimeBodyPart();
 			textPart.setContent(text, "text/plain"); //$NON-NLS-1$
 			textPart.setFileName(name);
 			content.addBodyPart(textPart);
@@ -128,63 +156,73 @@ public class Mailer {
 		}
 		return false;
 	}
-	
-	public boolean addFilePart(Message msg, File file){
-		if(!file.canRead()){
+
+	public boolean addFilePart(Message msg, File file) {
+		if (!file.canRead()) {
 			return false;
 		}
 		try {
-			MimeMultipart content=(MimeMultipart)msg.getContent();
-			MimeBodyPart filePart=new MimeBodyPart();
+			MimeMultipart content = (MimeMultipart) msg.getContent();
+			MimeBodyPart filePart = new MimeBodyPart();
 			filePart.attachFile(file);
 			filePart.setFileName(file.getName());
 			content.addBodyPart(filePart);
 			return true;
 		} catch (Exception ex) {
 			ExHandler.handle(ex);
-		} 
-		
+		}
+
 		return false;
 	}
-	
-	
+
 	/**
-	 * Eine binäre Part an eine vorher mit createMultipatrtMessage erstellte Nachricht anhängen.
-	 * @param msg die Nachricht
-	 * @param name Name für die Part
-	 * @param part die Daten, die angehängt werden sollen
+	 * Eine binäre Part an eine vorher mit createMultipatrtMessage erstellte
+	 * Nachricht anhängen.
+	 * 
+	 * @param msg
+	 *            die Nachricht
+	 * @param name
+	 *            Name für die Part
+	 * @param part
+	 *            die Daten, die angehängt werden sollen
 	 * @return true bei Erfolg
 	 */
-	public boolean addBinaryPart(Message msg, String name, byte[] part){
-		try{
-			DataSource ds=new ByteArrayDataSource(part,"application/octet-stream"); //$NON-NLS-1$
-			MimeMultipart content=(MimeMultipart)msg.getContent();
-			MimeBodyPart binaryPart=new MimeBodyPart();
+	public boolean addBinaryPart(Message msg, String name, byte[] part) {
+		try {
+			DataSource ds = new ByteArrayDataSource(part,
+					"application/octet-stream"); //$NON-NLS-1$
+			MimeMultipart content = (MimeMultipart) msg.getContent();
+			MimeBodyPart binaryPart = new MimeBodyPart();
 			binaryPart.setDataHandler(new DataHandler(ds));
 			binaryPart.setFileName(name);
 			content.addBodyPart(binaryPart);
 			return true;
-		}catch(Exception ex){
+		} catch (Exception ex) {
 			ExHandler.handle(ex);
 		}
 		return false;
 	}
+
 	/**
 	 * Die vorher mit createMultipartMessage etc. erstellte Nachricht versenden
-	 * @param msg die Nachricht
-	 * @param to Empfänger-Email
+	 * 
+	 * @param msg
+	 *            die Nachricht
+	 * @param to
+	 *            Empfänger-Email
 	 * @return Result.OK oder eine Fehlermeldung
 	 */
-	public Result<String> send(Message msg, String to){
-		try{
-			InternetAddress addressTo = new InternetAddress( to );
-		    msg.setRecipient( Message.RecipientType.TO, addressTo );
-		    Transport.send( msg );
-		    return new Result<String>("Ok"); //$NON-NLS-1$
-		}catch(Exception ex){
+	public Result<String> send(Message msg, String to) {
+		try {
+			InternetAddress addressTo = new InternetAddress(to);
+			msg.setRecipient(Message.RecipientType.TO, addressTo);
+			Transport.send(msg);
+			return new Result<String>("Ok"); //$NON-NLS-1$
+		} catch (Exception ex) {
 			ExHandler.handle(ex);
-			return new Result<String>(Log.ERRORS,1,ex.getMessage(),Messages.Mailer_Error,true);
-		
+			return new Result<String>(Result.SEVERITY.ERROR, 1, ex.getMessage(),
+					Messages.Mailer_Error, true);
+
 		}
 	}
 }
--- elexis-mail/src/ch/elexis/mail/SendQFA.java
+++ elexis-mail/src/ch/elexis/mail/SendQFA.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, G. Weirich and Elexis
+ * Copyright (c) 2006-2008, G. Weirich and Elexis
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,7 +8,7 @@
  * Contributors:
  *    G. Weirich - initial implementation
  *    
- *  $Id: SendQFA.java 2513 2007-06-12 05:11:21Z rgw_ch $
+ *  $Id: SendQFA.java 4458 2008-09-27 19:51:35Z rgw_ch $
  *******************************************************************************/
 
 package ch.elexis.mail;
@@ -29,17 +29,18 @@ import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 
 import ch.elexis.Desk;
 import ch.elexis.Hub;
-import ch.elexis.util.Result;
 import ch.elexis.util.SWTHelper;
-import ch.rgw.IO.FileTool;
+import ch.rgw.io.FileTool;
+import ch.rgw.tools.Result;
 
 /**
  * Qualitiy Feedback Action. Dies ist der Standard-delegate für den Menüpunkt
- * Hilfe-Fehlermeldung senden.
- * Es werden das Elexis log und das Eclipse log gezippt und an eine Mail angehängt, die an die in
- * den Preferences angegebene Adresse gesandt wird.
+ * Hilfe-Fehlermeldung senden. Es werden das Elexis log und das Eclipse log
+ * gezippt und an eine Mail angehängt, die an die in den Preferences angegebene
+ * Adresse gesandt wird.
+ * 
  * @author gerry
- *
+ * 
  */
 public class SendQFA implements IWorkbenchWindowActionDelegate {
 
@@ -49,39 +50,47 @@ public class SendQFA implements IWorkbenchWindowActionDelegate {
 	}
 
 	public void init(IWorkbenchWindow window) {
-		
+
 	}
 
 	public void run(IAction action) {
 
-		QFADialog qfa=new QFADialog(Desk.theDisplay.getActiveShell());
-		if(qfa.open()==Dialog.OK){
-			ByteArrayOutputStream baos=new ByteArrayOutputStream();
-			try{
-				ZipOutputStream zos=new ZipOutputStream(baos);
+		QFADialog qfa = new QFADialog(Desk.theDisplay.getActiveShell());
+		if (qfa.open() == Dialog.OK) {
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			try {
+				ZipOutputStream zos = new ZipOutputStream(baos);
 				zos.putNextEntry(new ZipEntry("elexis.log")); //$NON-NLS-1$
-				FileInputStream fis=new FileInputStream(Hub.localCfg.get(PreferenceConstants.MAIL_ELEXIS_LOG, "elexis.log")); //$NON-NLS-1$
+				FileInputStream fis = new FileInputStream(Hub.localCfg.get(
+						PreferenceConstants.MAIL_ELEXIS_LOG, "elexis.log")); //$NON-NLS-1$
 				FileTool.copyStreams(fis, zos);
 				fis.close();
 				zos.putNextEntry(new ZipEntry("eclipse.log")); //$NON-NLS-1$
-				fis=new FileInputStream(Hub.localCfg.get(PreferenceConstants.MAIL_ECLIPSE_LOG, ".log")); //$NON-NLS-1$
-				FileTool.copyStreams(fis,zos);
+				fis = new FileInputStream(Hub.localCfg.get(
+						PreferenceConstants.MAIL_ECLIPSE_LOG, ".log")); //$NON-NLS-1$
+				FileTool.copyStreams(fis, zos);
 				fis.close();
 				zos.finish();
-			}catch(Exception ex){
-				SWTHelper.showError(Messages.SendQFA_couldtZip, ex.getMessage());
+			} catch (Exception ex) {
+				SWTHelper
+						.showError(Messages.SendQFA_couldtZip, ex.getMessage());
 				return;
 			}
-			Mailer mailer=new Mailer();
-			Message msg=mailer.createMultipartMessage(qfa.subject,
-					Hub.localCfg.get(PreferenceConstants.MAIL_SENDER, "elexisuser")); //$NON-NLS-1$
+			Mailer mailer = new Mailer();
+			Message msg = mailer.createMultipartMessage(qfa.subject,
+					Hub.localCfg.get(PreferenceConstants.MAIL_SENDER,
+							"elexisuser")); //$NON-NLS-1$
 			mailer.addTextPart(msg, qfa.text);
 			mailer.addBinaryPart(msg, "logs.zip", baos.toByteArray()); //$NON-NLS-1$
-			Result res=mailer.send(msg, Hub.localCfg.get(PreferenceConstants.MAIL_QFA_ADDRESS, "errors@elexis.ch")); //$NON-NLS-1$
-			if(res.isOK()){
-				MessageDialog.openInformation(Desk.theDisplay.getActiveShell(), Messages.SendQFA_MessageSent, Messages.SendQFA_YourMessageIsSent);
-			}else{
-				SWTHelper.showError(Messages.SendQFA_ErrorWhileSending,res.toString());
+			Result res = mailer.send(msg, Hub.localCfg.get(
+					PreferenceConstants.MAIL_QFA_ADDRESS, "errors@elexis.ch")); //$NON-NLS-1$
+			if (res.isOK()) {
+				MessageDialog.openInformation(Desk.theDisplay.getActiveShell(),
+						Messages.SendQFA_MessageSent,
+						Messages.SendQFA_YourMessageIsSent);
+			} else {
+				SWTHelper.showError(Messages.SendQFA_ErrorWhileSending, res
+						.toString());
 			}
 		}
 
