fbc8c0e437e39d6362c27efcad9432252c49f1d0
rgw_ch <devnull@localhost>
2008-10-09 12:39:01 +0200

    [svn]

--- TestElexis/ch/elexis/tests/TestSAT.java
+++ TestElexis/ch/elexis/tests/TestSAT.java
@@ -1,6 +1,5 @@
 package ch.elexis.tests;
 
-import java.io.File;
 import java.security.KeyPair;
 import java.util.HashMap;
 
@@ -8,7 +7,6 @@ import junit.framework.TestCase;
 import ch.rgw.crypt.Cryptologist;
 import ch.rgw.crypt.JCECrypter;
 import ch.rgw.crypt.SAT;
-import ch.rgw.io.FileTool;
 import ch.rgw.tools.Result;
 
 public class TestSAT extends TestCase {
@@ -17,24 +15,18 @@ public class TestSAT extends TestCase {
 	static byte[] encrypted;
 	
 	public TestSAT() throws Exception{
-		homedir = System.getenv("TEMP") + File.separator + "crypt";
-	
-		File file = new File(homedir);
-		if (!file.exists()) {
-			file.mkdirs();	
-		}
 		
 		crypt=new JCECrypter(null,null,"admin","adminkey".toCharArray());
-		
+		assertTrue(crypt.hasKeyOf("admin"));
 	}
 	
 	public void testCreateKeys() throws Exception{
 		if(!crypt.hasKeyOf("alice")){
-			KeyPair kp=crypt.generateKeys("alice","alicepwd".toCharArray(),null,null);
+			/*KeyPair kp= */crypt.generateKeys("alice","alicepwd".toCharArray(),null,null);
 			
 		}
 		if(!crypt.hasKeyOf("bob")){
-			KeyPair kp=crypt.generateKeys("bob","bobpwd".toCharArray(),null,null);
+			/*KeyPair kp= */crypt.generateKeys("bob","bobpwd".toCharArray(),null,null);
 		}
 	}
 	public void testWrap() throws Exception{
