2571126dba6642dff69cb65360e3d5a1ab4519a1
rgw_ch <devnull@localhost>
2008-10-10 15:05:27 +0200

    [svn] Anpassungen HL7 / speziellere Datentypen

--- TestElexis/ch/elexis/tests/TestSAT.java
+++ TestElexis/ch/elexis/tests/TestSAT.java
@@ -13,39 +13,46 @@ public class TestSAT extends TestCase {
 	static Cryptologist crypt;
 	static byte[] encrypted;
 	
+	private static final String adminname="admin@elexistest.ch";
+	private static final String alicename="alice@elexistest.ch";
+	private static final String bobname="bob@elexistest.ch";
+	private static final String adminpwd="adminpwd";
+	private static final String alicepwd="alicepwd";
+	private static final String bobpwd="bobpwd";
 		
 	public void testCreate() throws Exception{
-		crypt=new JCECrypter(null,null,"admin","adminkey".toCharArray());
-		assertTrue(crypt.hasKeyOf("admin"));
+		crypt=new JCECrypter(null,null,adminname,adminpwd.toCharArray());
+		assertTrue(crypt.hasKeyOf(adminname));
 	}
 	
 	public void testCreateKeys() throws Exception{
 		if(!crypt.hasKeyOf("alice")){
-			/*KeyPair kp= */crypt.generateKeys("alice","alicepwd".toCharArray(),null,null);
+			/*KeyPair kp= */crypt.generateKeys(alicename,alicepwd.toCharArray(),null,null);
 			
 		}
 		if(!crypt.hasKeyOf("bob")){
-			/*KeyPair kp= */crypt.generateKeys("bob","bobpwd".toCharArray(),null,null);
+			/*KeyPair kp= */crypt.generateKeys(bobname,bobpwd.toCharArray(),null,null);
 		}
 	}
 	public void testWrap() throws Exception{
-		crypt=new JCECrypter(null,null,"alice","alicepwd".toCharArray());
+		crypt=new JCECrypter(null,null,alicename,alicepwd.toCharArray());
 		SAT sat = new SAT(crypt);
 		HashMap<String, Object> hash = new HashMap<String, Object>();
 		hash.put("test", "Ein Testtext");
-		byte[] result = sat.wrap(hash, "bob");
+		byte[] result = sat.wrap(hash, bobname);
 		assertNotNull(result);
 		System.out.println(new String(result));
 		encrypted = result;
 	}
 	
 	public void testUnwrap() throws Exception{
+		crypt=new JCECrypter(null,null,bobname,bobpwd.toCharArray());
 		SAT sat = new SAT(crypt);
-		Result<HashMap<String, Object>> result = sat.unwrap(encrypted, "bobelexis".toCharArray());
+		Result<HashMap<String, Object>> result = sat.unwrap(encrypted);
 		assertTrue(result.isOK());
 		HashMap<String, Object> res = result.get();
 		String val = (String) res.get("test");
 		assertEquals(val, "Ein Testtext");
-		assertEquals("alice@elexis.ch", res.get(SAT.ADM_SIGNED_BY));
+		assertEquals(alicename, res.get(SAT.ADM_SIGNED_BY));
 	}
 }
