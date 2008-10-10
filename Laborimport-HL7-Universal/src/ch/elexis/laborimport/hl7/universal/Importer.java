3fcd7528bcd40717ce614c18196297775cd855fa
rgw_ch <devnull@localhost>
2008-10-10 15:04:55 +0200

    [svn] Anpassungen HL7 / speziellere Datentypen

--- Laborimport-HL7-Universal/src/ch/elexis/laborimport/hl7/universal/Importer.java
+++ Laborimport-HL7-Universal/src/ch/elexis/laborimport/hl7/universal/Importer.java
@@ -16,33 +16,37 @@ import ch.rgw.tools.Result;
 
 public class Importer extends Action implements IAction {
 	public static final String MY_LAB = "Eigenlabor";
-	
+
 	private HL7Parser hlp = new HL7Parser(MY_LAB);
-	
-	public Importer(){
+
+	public Importer() {
 		super("Hl7 Datei", Desk.getImageDescriptor(Desk.IMG_IMPORT));
 	}
-	
+
 	@Override
-	public void run(){
+	public void run() {
 		HL7 hl7 = new HL7(MY_LAB, "Praxis");
-		File dir = new File(Hub.localCfg.get(Preferences.CFG_DIRECTORY, File.separator));
+		File dir = new File(Hub.localCfg.get(Preferences.CFG_DIRECTORY,
+				File.separator));
 		if ((!dir.exists()) || (!dir.isDirectory())) {
-			SWTHelper.showError("bad directory for import", "Konfigurationsfehler",
-				"Das Transferverzeichnis ist nicht korrekt eingestellt");
+			SWTHelper.showError("bad directory for import",
+					"Konfigurationsfehler",
+					"Das Transferverzeichnis ist nicht korrekt eingestellt");
 		} else {
-			Result<String> r = new Result<String>("OK");
 			int err = 0;
+			int files = 0;
+			Result<Object> r = null;
 			for (String fn : dir.list(new FilenameFilter() {
-				
-				public boolean accept(File arg0, String arg1){
+
+				public boolean accept(File arg0, String arg1) {
 					if (arg1.toLowerCase().endsWith(".hl7")) {
 						return true;
 					}
 					return false;
 				}
 			})) {
-				r.add(hl7.load(dir.getAbsolutePath()+File.separator+fn));
+				files++;
+				r = hl7.load(dir.getAbsolutePath() + File.separator + fn);
 				if (r.isOK()) {
 					r = hlp.parse(hl7, false);
 					if (!r.isOK()) {
@@ -53,9 +57,17 @@ public class Importer extends Action implements IAction {
 				}
 			}
 			if (err > 0) {
-				ResultAdapter.displayResult(r, Integer.toString(err) + " Dateien hatten Fehler");
+				ResultAdapter.displayResult(r, Integer.toString(err) + " von "
+						+ Integer.toString(files) + " Dateien hatten Fehler");
+			} else if (files == 0) {
+				SWTHelper.showInfo("Laborimport",
+						"Es waren keine Dateien zum Import vorhanden");
+			} else {
+				SWTHelper.showInfo("Laborimport", Integer.toString(files)
+						+ " Dateien wurden fehlerfrei verarbeitet.");
 			}
 		}
 	}
+
 	
 }
