19a44573c596970c1877ce4a79a2ee60d6bcf8e9
rgw_ch <devnull@localhost>
2008-12-09 19:10:32 +0100

    [svn] javadoc improvements

--- elexis-tools/javadoc/jdoc.cmd
+++ elexis-tools/javadoc/jdoc.cmd
@@ -1,8 +1,8 @@
-@echo off
-rem $Id: jdoc.cmd 4777 2008-12-08 17:01:08Z rgw_ch $
+
+rem $Id: jdoc.cmd 4781 2008-12-09 18:10:32Z rgw_ch $
 
 rem usage jdoc destdir sourcepath packages
 
-mkdir %1
-javadoc -d %1 -charset "utf-8" -docencoding "utf-8" -sourcepath %TOP%\%1\src -subpackages %2
+mkdir %DEST%%1
+%JAVA_HOME%\bin\javadoc -d %DEST%%1 -link file://localhost/%DEST%/elexis -link file://localhost/%DEST%/elexis-utilities -charset "utf-8" -docencoding "utf-8" -sourcepath %TOP%\%1\src -public -encoding "utf-8" -subpackages %2
 
--- elexis-tools/javadoc/mkdoc.cmd
+++ elexis-tools/javadoc/mkdoc.cmd
@@ -1,11 +1,14 @@
 @echo off
-rem $Id: mkdoc.cmd 4777 2008-12-08 17:01:08Z rgw_ch $
+rem $Id: mkdoc.cmd 4781 2008-12-09 18:10:32Z rgw_ch $
 rem =========================
 set TOP=d:\source\elexis_trunk
 rem ==========================
 
 set j=%TOP%\elexis-tools\javadoc\jdoc.cmd
-
+if *%1==* goto stay
+set DEST=%1\
+:stay
+call %j% elexis-utilities ch
 call %j% elexis ch
 call %j% elexis-agenda ch
 call %j% elexis-artikel-schweiz ch
@@ -16,11 +19,18 @@ call %j% elexis-bildanzeige ch
 call %j% elexis-diagnosecodes-schweiz ch
 call %j% elexis-ebanking-schweiz ch
 call %j% elexis-eigendiagnosen ch
+call %j% elexis-externe-dokumente ch
+call %j% elexis-h-net ch
 call %j% elexis-icpc ch
 call %j% elexis-importer ch
+call %j% elexis-importer-aerztekasse ch
+call %j% elexis-mail ch
+call %j% elexis-medikamente-bag ch
 call %j% elexis-nachrichten ch
 call %j% elexis-notes ch
 call %j% elexis-omnivore ch
 call %j% elexis-privatnotizen ch
-call %j% elexis-utilities ch
+call %j% iatrix-help-wiki org
+call %j% Laborimport-Viollier ch
+call %j% OOWrapper ch
 call %j% SGAM-xChange ch
