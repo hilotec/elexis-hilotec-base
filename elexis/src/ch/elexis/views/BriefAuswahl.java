fbc9cb950a97d6ef9f13b278e7a3f6c2d0e342fe
psiska <devnull@localhost>
2008-12-11 11:20:15 +0100

    [svn] Remodeled LetterChoiceView. NOTE: There's still a white border around the entire page from the cttab :/ Had no luck removing it.

--- elexis/src/ch/elexis/views/BriefAuswahl.java
+++ elexis/src/ch/elexis/views/BriefAuswahl.java
@@ -8,7 +8,7 @@
  * Contributors:
  *    G. Weirich - initial implementation
  *    
- *    $Id: BriefAuswahl.java 4109 2008-07-06 19:35:50Z rgw_ch $
+ *    $Id: BriefAuswahl.java 4804 2008-12-11 10:20:15Z psiska $
  *******************************************************************************/
 
 package ch.elexis.views;
@@ -55,235 +55,271 @@ import ch.elexis.util.ViewMenus;
 import ch.elexis.util.ViewerConfigurer;
 import ch.rgw.tools.ExHandler;
 
-public class BriefAuswahl extends ViewPart implements SelectionListener, ActivationListener, ISaveablePart2{
-	public final static String ID="ch.elexis.BriefAuswahlView";
+public class BriefAuswahl extends ViewPart implements SelectionListener,
+		ActivationListener, ISaveablePart2 {
+
+	public final static String ID = "ch.elexis.BriefAuswahlView";
 	private final FormToolkit tk;
 	private Form form;
-	private Action briefNeuAction,briefLadenAction, editNameAction;
+	private Action briefNeuAction, briefLadenAction, editNameAction;
 	private Action deleteAction;
 	private ViewMenus menus;
 	CTabFolder ctab;
-	//private ViewMenus menu;
-	//private IAction delBriefAction;
+
+	// private ViewMenus menu;
+	// private IAction delBriefAction;
 	public BriefAuswahl() {
-		tk=Desk.getToolkit();
+		tk = Desk.getToolkit();
 	}
 
 	@Override
-	public void createPartControl(final Composite parent){
-		StringBuilder sb=new StringBuilder();
-		sb.append("Alle,").append(Brief.UNKNOWN).append(",").append(Brief.AUZ).append(",")
-			.append(Brief.RP).append(",").append(Brief.LABOR);
-		String cats=Hub.globalCfg.get(PreferenceConstants.DOC_CATEGORY,sb.toString());
+	public void createPartControl(final Composite parent) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Alle,").append(Brief.UNKNOWN).append(",").append(Brief.AUZ)
+				.append(",").append(Brief.RP).append(",").append(Brief.LABOR);
+		String cats = Hub.globalCfg.get(PreferenceConstants.DOC_CATEGORY, sb
+				.toString());
 		parent.setLayout(new GridLayout());
-		form=tk.createForm(parent);
-		form.setLayoutData(SWTHelper.getFillGridData(1,true,1,true));
-		Composite body=form.getBody();
-		body.setLayout(new GridLayout());
-	
-		ctab=new CTabFolder(body,SWT.BOTTOM);
-		ctab.setLayoutData(SWTHelper.getFillGridData(1,true,1,true));
-		makeActions();
-		menus=new ViewMenus(getViewSite());
 
-		for(String cat:cats.split(",")){
-			CTabItem ct=new CTabItem(ctab,SWT.NONE);
+		form = tk.createForm(parent);
+		form.setLayoutData(SWTHelper.getFillGridData(1, true, 1, true));
+		form.setBackground(parent.getBackground());
+
+		// Grid layout with zero margins
+		GridLayout slimLayout = new GridLayout();
+		slimLayout.marginHeight = 0;
+		slimLayout.marginWidth = 0;
+
+		Composite body = form.getBody();
+		body.setLayout(slimLayout);
+		body.setBackground(parent.getBackground());
+
+		ctab = new CTabFolder(body, SWT.BOTTOM);
+		ctab.setLayout(slimLayout);
+		ctab.setLayoutData(SWTHelper.getFillGridData(1, true, 1, true));
+		ctab.setBackground(parent.getBackground());
+		makeActions();
+		menus = new ViewMenus(getViewSite());
+
+		for (String cat : cats.split(",")) {
+			CTabItem ct = new CTabItem(ctab, SWT.NONE);
 			ct.setText(cat);
-			sPage page=new sPage(ctab,cat);
-			menus.createViewerContextMenu(page.cv.getViewerWidget(),editNameAction,deleteAction);
+			sPage page = new sPage(ctab, cat);
+			menus.createViewerContextMenu(page.cv.getViewerWidget(),
+					editNameAction, deleteAction);
 			ct.setData(page.cv);
 			ct.setControl(page);
 		}
-		ctab.addSelectionListener(new SelectionAdapter(){
-
+		
+		ctab.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(final SelectionEvent e) {
 				relabel();
 			}
-			
+
 		});
-		GlobalEvents.getInstance().addActivationListener(this,this);
-		menus.createMenu(briefNeuAction,briefLadenAction,editNameAction,deleteAction);
-		menus.createToolbar(briefNeuAction,briefLadenAction,deleteAction);
+		
+		GlobalEvents.getInstance().addActivationListener(this, this);
+		menus.createMenu(briefNeuAction, briefLadenAction, editNameAction,
+				deleteAction);
+		menus.createToolbar(briefNeuAction, briefLadenAction, deleteAction);
 		ctab.setSelection(0);
 		relabel();
 	}
-	
+
 	@Override
-	public void dispose(){
+	public void dispose() {
 		GlobalEvents.getInstance().removeSelectionListener(this);
-		GlobalEvents.getInstance().removeActivationListener(this,this);
-		for(CTabItem it:ctab.getItems()){
-			CommonViewer cv=(CommonViewer)it.getData();
-			if(!cv.getViewerWidget().getControl().isDisposed()){
+		GlobalEvents.getInstance().removeActivationListener(this, this);
+		for (CTabItem it : ctab.getItems()) {
+			CommonViewer cv = (CommonViewer) it.getData();
+			if (!cv.getViewerWidget().getControl().isDisposed()) {
 				cv.getConfigurer().getContentProvider().stopListening();
 			}
 		}
 	}
+
 	@Override
 	public void setFocus() {
-		
+
 	}
-	public void relabel(){
-		Patient pat=GlobalEvents.getSelectedPatient();
-		if(pat==null){
+
+	public void relabel() {
+		Patient pat = GlobalEvents.getSelectedPatient();
+		if (pat == null) {
 			form.setText("Kein Patient ausgewählt");
-		}else{
+		} else {
 			form.setText(pat.getLabel());
-			CTabItem sel=ctab.getSelection();
-			if(sel!=null){
-				CommonViewer cv=(CommonViewer)sel.getData();
+			CTabItem sel = ctab.getSelection();
+			if (sel != null) {
+				CommonViewer cv = (CommonViewer) sel.getData();
 				cv.notify(CommonViewer.Message.update);
 			}
 		}
-		
+
 	}
+
 	public void selectionEvent(final PersistentObject obj) {
-		if(obj instanceof Patient){
+		if (obj instanceof Patient) {
 			relabel();
 		}
-		
+
 	}
-	
-	
-	class sPage extends Composite{
+
+	class sPage extends Composite {
 		private final CommonViewer cv;
 		private final ViewerConfigurer vc;
-		
-		sPage(final Composite parent,final String cat){
-			super(parent,SWT.NONE);
+
+		sPage(final Composite parent, final String cat) {
+			super(parent, SWT.NONE);
 			setLayout(new GridLayout());
-			cv=new CommonViewer();
-			vc=new ViewerConfigurer(
-				new DefaultContentProvider(cv,Brief.class){
+			cv = new CommonViewer();
+			vc = new ViewerConfigurer(new DefaultContentProvider(cv,
+					Brief.class) {
 
-					@Override
-					public Object[] getElements(final Object inputElement) {
-						Patient actPat=GlobalEvents.getSelectedPatient();
-						if(actPat!=null){
-							Query<Brief> qbe=new Query<Brief>(Brief.class);
-							qbe.add("PatientID","=",actPat.getId());
-							if(cat.equals("Alle")){
-								qbe.add("Typ","<>",Brief.TEMPLATE);
-							}else{
-								qbe.add("Typ","=",cat);
-							}
-							cv.getConfigurer().getControlFieldProvider().setQuery(qbe);
-							List list=qbe.execute();
-							return list.toArray();
-						}else{
-							return new Brief[0];
+				@Override
+				public Object[] getElements(final Object inputElement) {
+					Patient actPat = GlobalEvents.getSelectedPatient();
+					if (actPat != null) {
+						Query<Brief> qbe = new Query<Brief>(Brief.class);
+						qbe.add("PatientID", "=", actPat.getId());
+						if (cat.equals("Alle")) {
+							qbe.add("Typ", "<>", Brief.TEMPLATE);
+						} else {
+							qbe.add("Typ", "=", cat);
 						}
+						cv.getConfigurer().getControlFieldProvider().setQuery(
+								qbe);
+						List list = qbe.execute();
+						return list.toArray();
+					} else {
+						return new Brief[0];
 					}
+				}
 
-				},
-				new DefaultLabelProvider(),
-				new DefaultControlFieldProvider(cv,new String[]{"Betreff=Titel"}),
-				new ViewerConfigurer.DefaultButtonProvider(),
-				new SimpleWidgetProvider(SimpleWidgetProvider.TYPE_LIST,SWT.V_SCROLL,cv)
-			);
-			cv.create(vc,this,SWT.NONE,getViewSite());
+			}, new DefaultLabelProvider(), new DefaultControlFieldProvider(cv,
+					new String[] { "Betreff=Titel" }),
+					new ViewerConfigurer.DefaultButtonProvider(),
+					new SimpleWidgetProvider(SimpleWidgetProvider.TYPE_LIST,
+							SWT.V_SCROLL, cv));
+			cv.create(vc, this, SWT.NONE, getViewSite());
 			vc.getContentProvider().startListening();
-			Button bLoad=tk.createButton(this,"Laden",SWT.PUSH);
-			bLoad.addSelectionListener(new SelectionAdapter(){
+			Button bLoad = tk.createButton(this, "Laden", SWT.PUSH);
+			bLoad.addSelectionListener(new SelectionAdapter() {
 				@Override
 				public void widgetSelected(final SelectionEvent e) {
-					try{
-						TextView tv=(TextView)getViewSite().getPage().showView(TextView.ID);
-						Object[] o=cv.getSelection();
-						if((o!=null) && (o.length>0)){
-							Brief brief=(Brief)o[0];
-							if(tv.openDocument(brief)==false){
-								SWTHelper.alert("Fehler","Konnte Text nicht laden");
+					try {
+						TextView tv = (TextView) getViewSite().getPage()
+								.showView(TextView.ID);
+						Object[] o = cv.getSelection();
+						if ((o != null) && (o.length > 0)) {
+							Brief brief = (Brief) o[0];
+							if (tv.openDocument(brief) == false) {
+								SWTHelper.alert("Fehler",
+										"Konnte Text nicht laden");
 							}
-						}else{
-							tv.createDocument(null,null);
+						} else {
+							tv.createDocument(null, null);
 						}
-					}catch(Throwable ex){
+					} catch (Throwable ex) {
 						ExHandler.handle(ex);
 					}
 				}
-			
+
 			});
-			bLoad.setLayoutData(SWTHelper.getFillGridData(1,true,1,false));
+			bLoad.setLayoutData(SWTHelper.getFillGridData(1, true, 1, false));
 
 		}
 	}
-	private void makeActions(){
-		briefNeuAction=new Action("Neu..."){
+
+	private void makeActions() {
+		briefNeuAction = new Action("Neu...") {
 			@Override
-			public void run(){
-				TextView tv=null;
-				try{
-					tv=(TextView)getSite().getPage().showView(TextView.ID /*,StringTool.unique("textView"),IWorkbenchPage.VIEW_ACTIVATE */);
-					DocumentSelectDialog bs=new DocumentSelectDialog(getViewSite().getShell(),Hub.actMandant,DocumentSelectDialog.TYPE_CREATE_DOC_WITH_TEMPLATE);
-					if(bs.open()==Dialog.OK){
-						tv.createDocument(bs.getSelectedDocument(),bs.getBetreff());
+			public void run() {
+				TextView tv = null;
+				try {
+					tv = (TextView) getSite().getPage()
+							.showView(TextView.ID /*
+												 * ,StringTool.unique("textView")
+												 * ,IWorkbenchPage.VIEW_ACTIVATE
+												 */);
+					DocumentSelectDialog bs = new DocumentSelectDialog(
+							getViewSite().getShell(), Hub.actMandant,
+							DocumentSelectDialog.TYPE_CREATE_DOC_WITH_TEMPLATE);
+					if (bs.open() == Dialog.OK) {
+						tv.createDocument(bs.getSelectedDocument(), bs
+								.getBetreff());
 						tv.setName();
-						CTabItem sel=ctab.getSelection();
-						if(sel!=null){
-							CommonViewer cv=(CommonViewer)sel.getData();
+						CTabItem sel = ctab.getSelection();
+						if (sel != null) {
+							CommonViewer cv = (CommonViewer) sel.getData();
 							cv.notify(CommonViewer.Message.update_keeplabels);
 						}
 
-					}	
-				}catch(Exception ex){
+					}
+				} catch (Exception ex) {
 					ExHandler.handle(ex);
 				}
 			}
 		};
-		briefLadenAction=new Action("Öffnen"){
+		briefLadenAction = new Action("Öffnen") {
 			@Override
-			public void run(){
+			public void run() {
 				try {
-					TextView tv=(TextView)getViewSite().getPage().showView(TextView.ID);
-					CTabItem sel=ctab.getSelection();
-					if(sel!=null){
-						CommonViewer cv=(CommonViewer)sel.getData();
-						Object[] o=cv.getSelection();
-						if((o!=null) && (o.length>0)){
-							Brief brief=(Brief)o[0];
-							if(tv.openDocument(brief)==false){
-								SWTHelper.alert("Fehler","Konnte Text nicht laden");
+					TextView tv = (TextView) getViewSite().getPage().showView(
+							TextView.ID);
+					CTabItem sel = ctab.getSelection();
+					if (sel != null) {
+						CommonViewer cv = (CommonViewer) sel.getData();
+						Object[] o = cv.getSelection();
+						if ((o != null) && (o.length > 0)) {
+							Brief brief = (Brief) o[0];
+							if (tv.openDocument(brief) == false) {
+								SWTHelper.alert("Fehler",
+										"Konnte Text nicht laden");
 							}
-						}else{
-							tv.createDocument(null,null);
+						} else {
+							tv.createDocument(null, null);
 						}
 						cv.notify(CommonViewer.Message.update);
 					}
 				} catch (PartInitException e) {
 					ExHandler.handle(e);
 				}
-	
+
 			}
 		};
-		deleteAction=new Action("Löschen"){
+		deleteAction = new Action("Löschen") {
 			@Override
-			public void run(){
-				CTabItem sel=ctab.getSelection();
-				if(sel!=null){
-					CommonViewer cv=(CommonViewer)sel.getData();
-					Object[] o=cv.getSelection();
-					if((o!=null) && (o.length>0)){
-						Brief brief=(Brief)o[0];
+			public void run() {
+				CTabItem sel = ctab.getSelection();
+				if (sel != null) {
+					CommonViewer cv = (CommonViewer) sel.getData();
+					Object[] o = cv.getSelection();
+					if ((o != null) && (o.length > 0)) {
+						Brief brief = (Brief) o[0];
 						brief.delete();
 					}
 					cv.notify(CommonViewer.Message.update);
 				}
-			
+
 			}
 		};
-		editNameAction=new Action("Umbenennen..."){
+		editNameAction = new Action("Umbenennen...") {
 			@Override
-			public void run(){
-				CTabItem sel=ctab.getSelection();
-				if(sel!=null){
-					CommonViewer cv=(CommonViewer)sel.getData();
-					Object[] o=cv.getSelection();
-					if((o!=null) && (o.length>0)){
-						Brief brief=(Brief)o[0];
-						InputDialog id=new InputDialog(getViewSite().getShell(),"Neuer Betreff","Geben Sie bitte den neuen Betreff für das Dokument ein",brief.getBetreff(),null);
-						if(id.open()==Dialog.OK){
+			public void run() {
+				CTabItem sel = ctab.getSelection();
+				if (sel != null) {
+					CommonViewer cv = (CommonViewer) sel.getData();
+					Object[] o = cv.getSelection();
+					if ((o != null) && (o.length > 0)) {
+						Brief brief = (Brief) o[0];
+						InputDialog id = new InputDialog(
+								getViewSite().getShell(),
+								"Neuer Betreff",
+								"Geben Sie bitte den neuen Betreff für das Dokument ein",
+								brief.getBetreff(), null);
+						if (id.open() == Dialog.OK) {
 							brief.setBetreff(id.getValue());
 						}
 					}
@@ -292,58 +328,69 @@ public class BriefAuswahl extends ViewPart implements SelectionListener, Activat
 			}
 		};
 		/*
-		importAction=new Action("Importieren..."){
-			public void run(){
-				
-			}
-		};
-		*/
-        briefLadenAction.setImageDescriptor(Hub.getImageDescriptor("rsc/file.gif"));
-        briefLadenAction.setToolTipText("Dokument zum Bearbeiten öffnen");
-        briefNeuAction.setImageDescriptor(Hub.getImageDescriptor("rsc/fileneu.gif"));
-        briefNeuAction.setToolTipText("Einen neues Dokument erstellen");
-        editNameAction.setImageDescriptor(Hub.getImageDescriptor("rsc/rename.gif"));
-        editNameAction.setToolTipText("Dokument umbenennen");
-        deleteAction.setImageDescriptor(Hub.getImageDescriptor("rsc/filedel.gif"));
-        deleteAction.setToolTipText("Dokument löschen");
+		 * importAction=new Action("Importieren..."){ public void run(){
+		 * 
+		 * } };
+		 */
+		briefLadenAction.setImageDescriptor(Hub
+				.getImageDescriptor("rsc/file.gif"));
+		briefLadenAction.setToolTipText("Dokument zum Bearbeiten öffnen");
+		briefNeuAction.setImageDescriptor(Hub
+				.getImageDescriptor("rsc/fileneu.gif"));
+		briefNeuAction.setToolTipText("Einen neues Dokument erstellen");
+		editNameAction.setImageDescriptor(Hub
+				.getImageDescriptor("rsc/rename.gif"));
+		editNameAction.setToolTipText("Dokument umbenennen");
+		deleteAction.setImageDescriptor(Hub
+				.getImageDescriptor("rsc/filedel.gif"));
+		deleteAction.setToolTipText("Dokument löschen");
 	}
 
 	public void activation(final boolean mode) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void visible(final boolean mode) {
-		if(mode==true){
-			selectionEvent(GlobalEvents.getInstance().getSelectedObject(Patient.class));
+		if (mode == true) {
+			selectionEvent(GlobalEvents.getInstance().getSelectedObject(
+					Patient.class));
 			GlobalEvents.getInstance().addSelectionListener(this);
-		}else{
+		} else {
 			GlobalEvents.getInstance().removeSelectionListener(this);
 		}
-		
+
 	}
 
 	public void clearEvent(final Class template) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	/* ******
-	 * Die folgenden 6 Methoden implementieren das Interface ISaveablePart2
-	 * Wir benötigen das Interface nur, um das Schliessen einer View zu verhindern,
-	 * wenn die Perspektive fixiert ist.
-	 * Gibt es da keine einfachere Methode?
-	 */ 
+
+	/*
+	 * Die folgenden 6 Methoden implementieren das Interface ISaveablePart2 Wir
+	 * benötigen das Interface nur, um das Schliessen einer View zu verhindern,
+	 * wenn die Perspektive fixiert ist. Gibt es da keine einfachere Methode?
+	 */
 	public int promptToSaveOnClose() {
-		return GlobalActions.fixLayoutAction.isChecked() ? ISaveablePart2.CANCEL : ISaveablePart2.NO;
+		return GlobalActions.fixLayoutAction.isChecked() ? ISaveablePart2.CANCEL
+				: ISaveablePart2.NO;
 	}
-	public void doSave(final IProgressMonitor monitor) { /* leer */ }
-	public void doSaveAs() { /* leer */}
+
+	public void doSave(final IProgressMonitor monitor) { /* leer */
+	}
+
+	public void doSaveAs() { /* leer */
+	}
+
 	public boolean isDirty() {
 		return true;
 	}
+
 	public boolean isSaveAsAllowed() {
 		return false;
 	}
+
 	public boolean isSaveOnCloseNeeded() {
 		return true;
 	}
