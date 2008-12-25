550f253dcab42128f6f07216fa9814a35edc0642
rgw_ch <devnull@localhost>
2008-12-25 21:13:41 +0100

    [svn] do not overweite prices if official price is 0.00

--- elexis-artikel-schweiz/src/ch/elexis/artikel_ch/data/MedikamentImporter.java
+++ elexis-artikel-schweiz/src/ch/elexis/artikel_ch/data/MedikamentImporter.java
@@ -9,7 +9,7 @@
  *    G. Weirich - initial implementation
  *    G. Weirich 1/08 - major redesign to implement IGM updates etc. 
  *    
- *  $Id: MedikamentImporter.java 4463 2008-09-27 19:52:37Z rgw_ch $
+ *  $Id: MedikamentImporter.java 4857 2008-12-25 20:13:41Z rgw_ch $
  *******************************************************************************/
 
 package ch.elexis.artikel_ch.data;
@@ -35,6 +35,12 @@ import ch.elexis.util.Log;
 import ch.elexis.util.SWTHelper;
 import ch.rgw.tools.ExHandler;
 
+/**
+ * Importing articles from an IGM-10 or IGM-11 file.
+ * 
+ * @author Gerry
+ * 
+ */
 public class MedikamentImporter extends ImporterPage {
 	static final String EQUALS = "=";
 	private static final String MWST_TYP = "MWSt-Typ";
@@ -43,36 +49,33 @@ public class MedikamentImporter extends ImporterPage {
 	private static final String LAGERART = "Lagerart";
 	private static final String KASSENTYP = "Kassentyp";
 	private static final String VK_PREIS = "VK_Preis";
-
+	
 	// Button bClear;
 	// boolean bDelete;
-	public MedikamentImporter() {
-	}
-
+	public MedikamentImporter(){}
+	
 	final static String SUBID = "SubID";
 	final static String NAME = "Name";
 	final static String MEDIKAMENT = "Medikament";
 	final static String MEDICAL = "Medical";
 	final static String EK_PREIS = "EK_Preis";
-
+	
 	@SuppressWarnings("unchecked")
 	@Override
-	public IStatus doImport(final IProgressMonitor monitor) throws Exception {
+	public IStatus doImport(final IProgressMonitor monitor) throws Exception{
 		File file = new File(results[0]);
 		long l = file.length();
-		InputStreamReader ir = new InputStreamReader(new FileInputStream(file),
-				"iso-8859-1");
+		InputStreamReader ir = new InputStreamReader(new FileInputStream(file), "iso-8859-1");
 		BufferedReader br = new BufferedReader(ir);
 		int cachetime = PersistentObject.getDefaultCacheLifetime();
 		PersistentObject.setDefaultCacheLifetime(2);
 		String in;
 		String mode = " (Modus: Daten ergänzen/update)";
 		/*
-		 * if(bDelete==true){ if(SWTHelper.askYesNo("Wirklich Daten löschen",
-		 * "Achtung: Wenn die alten Daten gelöscht werden, kann es\nsein, dass
-		 * bestehende Bezüge ungültig werden.")){
-		 * PersistentObject.getConnection().exec("DELETE FROM ARTIKEL WHERE
-		 * TYP='Medikament'"); mode=" (Modus: Alles neu erstellen)"; } }
+		 * if(bDelete==true){ if(SWTHelper.askYesNo("Wirklich Daten löschen", "Achtung: Wenn die
+		 * alten Daten gelöscht werden, kann es\nsein, dass bestehende Bezüge ungültig werden.")){
+		 * PersistentObject.getConnection().exec("DELETE FROM ARTIKEL WHERE TYP='Medikament'");
+		 * mode=" (Modus: Alles neu erstellen)"; } }
 		 */
 		monitor.beginTask("Medikamenten Import" + mode, (int) (l / 100));
 		Query<Artikel> qbe = new Query<Artikel>(Artikel.class);
@@ -90,7 +93,7 @@ public class MedikamentImporter extends ImporterPage {
 				ExHandler.handle(ex);
 				log.log("Falscher Pharmacode: pkraw", Log.ERRORS);
 			}
-
+			
 			// String id=qbe.findSingle(SUBID, EQUALS, pk);
 			qbe.clear();
 			qbe.add(SUBID, EQUALS, pk);
@@ -108,24 +111,24 @@ public class MedikamentImporter extends ImporterPage {
 						hasValid = true;
 					}
 				}
-
+				
 			}
 			Artikel a = lArt.size() > 0 ? lArt.get(0) : null;
 			if ((a == null) || (!a.exists())) {
 				if (cmut.equals("3") || (!reca.equals("11"))) { // ausser handel
-																// oder kein
-																// Stammsatz
+					// oder kein
+					// Stammsatz
 					continue; // Dann Artikel nicht neu erstellen, falls er
-								// nicht existiert
+					// nicht existiert
 				}
 			} else {
 				if (cmut.equals("3")) { // Wen n er existiert, muss er gelöscht
-										// werden
+					// werden
 					a.delete();
 					continue;
 				}
 			}
-
+			
 			if (reca.equals("11")) { // Stammsatz
 				titel = new String(in.substring(10, 60)).trim(); // Text
 				ek = new String(in.substring(60, 66)).trim(); // EK-Preis
@@ -134,10 +137,10 @@ public class MedikamentImporter extends ImporterPage {
 				String lager = new String(in.substring(73, 75)); // Lagerart
 				String hix = new String(in.substring(75, 76)); // iks-listencode
 				String ithe = new String(in.substring(76, 83)); // index
-																// therapeuticus
+				// therapeuticus
 				String ean = new String(in.substring(83, 96)); // EAN
 				mwst = new String(in.substring(96, 97)); // MWSt-Typ
-
+				
 				if (a == null) {
 					if (mwst.equals("1")) {
 						a = new Artikel(titel, MEDICAL, pk);
@@ -145,8 +148,16 @@ public class MedikamentImporter extends ImporterPage {
 						a = new Artikel(titel, MEDIKAMENT, pk);
 					}
 				}
-				String[] fields = { EK_PREIS, VK_PREIS, EAN };
-				a.set(fields, ek, vk, ean);
+				if (vk.matches("0+")) {
+					a.set(EK_PREIS, ek);
+					a.set(EAN, ean);
+				} else {
+					String[] fields = {
+						EK_PREIS, VK_PREIS, EAN
+					};
+					a.set(fields, ek, vk, ean);
+				}
+				
 				Hashtable ext = a.getHashtable(Artikel.EXT_INFO);
 				ext.put(Artikel.PHARMACODE, pk);
 				ext.put(KASSENTYP, kasse);
@@ -160,16 +171,21 @@ public class MedikamentImporter extends ImporterPage {
 				vk = new String(in.substring(16, 22));
 				kasse = new String(in.substring(22, 23));
 				mwst = new String(in.substring(23, 24));
-				String[] fields = { EK_PREIS, VK_PREIS };
-				a.set(fields, ek, vk);
-
+				if (vk.matches("0+")) {
+					a.set(EK_PREIS, ek);
+				} else {
+					String[] fields = {
+						EK_PREIS, VK_PREIS
+					};
+					a.set(fields, ek, vk);
+				}
+				
 			} else {
-				SWTHelper
-						.showError("Falsches Dateiformat",
-								"Es können nur IGM-10 und IGM-11 Dateien importiert werden");
+				SWTHelper.showError("Falsches Dateiformat",
+					"Es können nur IGM-10 und IGM-11 Dateien importiert werden");
 				return Status.CANCEL_STATUS;
 			}
-
+			
 			monitor.worked(1);
 			if (monitor.isCanceled()) {
 				monitor.done();
@@ -184,25 +200,25 @@ public class MedikamentImporter extends ImporterPage {
 				Thread.sleep(100);
 				counter = 0;
 			}
-
+			
 		}
 		monitor.done();
 		PersistentObject.setDefaultCacheLifetime(cachetime);
 		return Status.OK_STATUS;
 	}
-
+	
 	@Override
-	public String getTitle() {
+	public String getTitle(){
 		return "Medikamente";
 	}
-
+	
 	@Override
-	public String getDescription() {
+	public String getDescription(){
 		return "Bitte wählen Sie die Datei (IGM10 oder IGM11 -Format) aus, aus der die Artikel importiert werden sollen";
 	}
-
+	
 	@Override
-	public Composite createPage(final Composite parent) {
+	public Composite createPage(final Composite parent){
 		Composite ret = new ImporterPage.FileBasedImporter(parent, this);
 		ret.setLayoutData(SWTHelper.getFillGridData(1, true, 1, true));
 		// bClear=new Button(parent,SWT.CHECK|SWT.WRAP);
@@ -211,9 +227,8 @@ public class MedikamentImporter extends ImporterPage {
 		// bClear.setLayoutData(SWTHelper.getFillGridData(1,true,1,false));
 		return ret;
 	}
-
+	
 	/*
-	 * @Override public void collect() { bDelete=bClear.getSelection();
-	 * super.collect(); }
+	 * @Override public void collect() { bDelete=bClear.getSelection(); super.collect(); }
 	 */
 }
--- elexis/src/ch/elexis/preferences/inputs/PrefAccessDenied.java
+++ elexis/src/ch/elexis/preferences/inputs/PrefAccessDenied.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, G. Weirich and Elexis
+ * Copyright (c) 2007-2008, G. Weirich and Elexis
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,7 +8,7 @@
  * Contributors:
  *    G. Weirich - initial implementation
  *    
- *  $Id: PrefAccessDenied.java 1849 2007-02-19 07:52:43Z rgw_ch $
+ *  $Id: PrefAccessDenied.java 4857 2008-12-25 20:13:41Z rgw_ch $
  *******************************************************************************/
 package ch.elexis.preferences.inputs;
 
@@ -19,11 +19,16 @@ import org.eclipse.swt.widgets.Label;
 
 import ch.elexis.util.SWTHelper;
 
+/**
+ * A preference just saying "access denied"
+ * @author Gerry
+ *
+ */
 public class PrefAccessDenied extends Composite {
 	public PrefAccessDenied(Composite parent){
-		super(parent,SWT.NONE);
+		super(parent, SWT.NONE);
 		setLayoutData(SWTHelper.getFillGridData(1, true, 1, true));
 		setLayout(new FillLayout());
-		new Label(this,SWT.WRAP).setText("Diese Einstellungsseite ist gesperrt");
+		new Label(this, SWT.WRAP).setText("Diese Einstellungsseite ist gesperrt");
 	}
 }
