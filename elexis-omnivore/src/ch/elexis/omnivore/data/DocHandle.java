d56d1b4c3f9106590a42d65c575439c67bbe2e6c
rgw_ch <devnull@localhost>
2009-01-13 18:46:15 +0100

    [svn] db-änderungen 2.0

--- elexis-omnivore/src/ch/elexis/omnivore/data/DocHandle.java
+++ elexis-omnivore/src/ch/elexis/omnivore/data/DocHandle.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006-2008, G. Weirich and Elexis
+ * Copyright (c) 2006-2009, G. Weirich and Elexis
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,7 +8,7 @@
  * Contributors:
  *    G. Weirich - initial implementation
  *    
- *  $Id: DocHandle.java 3932 2008-05-16 17:25:44Z rgw_ch $
+ *  $Id: DocHandle.java 4933 2009-01-13 17:46:15Z rgw_ch $
  *******************************************************************************/
 
 package ch.elexis.omnivore.data;
@@ -35,11 +35,12 @@ import ch.rgw.tools.TimeTool;
 import ch.rgw.tools.VersionInfo;
 
 public class DocHandle extends PersistentObject {
-	public static final String TABLENAME="CH_ELEXIS_OMNIVORE_DATA";
-	public static final String DBVERSION="1.2.0";
+	public static final String TABLENAME = "CH_ELEXIS_OMNIVORE_DATA";
+	public static final String DBVERSION = "1.2.1";
 	public static final String createDB=
 		"CREATE TABLE "+TABLENAME+" ("+
 		"ID				VARCHAR(25) primary key,"+
+		"lastupdate     BIGINT,"+
 		"deleted        CHAR(1) default '0',"+
 		"PatID			VARCHAR(25),"+
 		"Datum			CHAR(8),"+
@@ -57,153 +58,168 @@ public class DocHandle extends PersistentObject {
 		"ALTER TABLE "+TABLENAME+" MODIFY Keywords VARCHAR(255);"+
 		"ALTER TABLE "+TABLENAME+" Modify Path VARCHAR(255);";
 	
-	static{
-		addMapping(
-			TABLENAME,"PatID","Datum=S:D:Datum","Titel=Title","Keywords","Path","Doc","Mimetype"
-				);
-		DocHandle start=load("1");
-		if(start==null){
+	private static final String upd121=
+		"ALTER TABLE "+TABLENAME+" ADD lastupdate BIGINT;";
+	
+	static {
+		addMapping(TABLENAME, "PatID", "Datum=S:D:Datum", "Titel=Title", "Keywords", "Path", "Doc",
+			"Mimetype");
+		DocHandle start = load("1");
+		if (start == null) {
 			init();
-		}else{
-			VersionInfo vi=new VersionInfo(start.get("Titel"));
-			if(vi.isOlder(DBVERSION)){
-				if(vi.isOlder("1.1.0")){
-					getConnection().exec("ALTER TABLE "+TABLENAME+" ADD deleted CHAR(1) default '0';");
+		} else {
+			VersionInfo vi = new VersionInfo(start.get("Titel"));
+			if (vi.isOlder(DBVERSION)) {
+				if (vi.isOlder("1.1.0")) {
+					getConnection().exec(
+						"ALTER TABLE " + TABLENAME + " ADD deleted CHAR(1) default '0';");
 					start.set("Titel", DBVERSION);
-				}else if (vi.isOlder("1.2.0")){
+				} 
+				if (vi.isOlder("1.2.0")) {
 					createTable(TABLENAME, upd120);
 					start.set("Titel", DBVERSION);
+				} 
+				if(vi.isOlder("1.2.1")){
+					createTable(TABLENAME, upd121);
+					start.set("Titel", DBVERSION);
 				}
-				else{
-					MessageDialog.openError(Desk.getTopShell(), "Versionskonsflikt", 
-						"Die Datentabelle für Dokumentspeicherung (Omnivore) hat eine zu alte Versionsnummer. Dies kann zu Fehlern führen");
-				}
-			
+				
 			}
 		}
 	}
 	
 	public DocHandle(byte[] doc, Patient pat, String title, String mime, String keyw){
-		if((doc==null) || (doc.length==0)){
-			SWTHelper.showError("Fehler mit Dokument", "Das Dokument konnte nicht korrekt gelesen werden");
+		if ((doc == null) || (doc.length == 0)) {
+			SWTHelper.showError("Fehler mit Dokument",
+				"Das Dokument konnte nicht korrekt gelesen werden");
 			return;
 		}
 		create(null);
-		if(setBinary("Doc", doc)==1){
-			set(new String[]{"PatID","Datum","Titel","Keywords","Mimetype"},
-				pat.getId(),new TimeTool().toString(TimeTool.DATE_GER),title,keyw,mime);
-		}else{
-			log.log("Der Datensatz wurde nicht geschrieben",Log.ERRORS);
+		if (setBinary("Doc", doc) == 1) {
+			set(new String[] {
+				"PatID", "Datum", "Titel", "Keywords", "Mimetype"
+			}, pat.getId(), new TimeTool().toString(TimeTool.DATE_GER), title, keyw, mime);
+		} else {
+			log.log("Der Datensatz wurde nicht geschrieben", Log.ERRORS);
 		}
 	}
+	
 	/**
-	   * Tabelle neu erstellen
-	   */
-	  public static void init(){
-		  createTable(TABLENAME,createDB);
-	  }
+	 * Tabelle neu erstellen
+	 */
+	public static void init(){
+		createTable(TABLENAME, createDB);
+	}
+	
 	public static DocHandle load(String id){
-		DocHandle ret= new DocHandle(id);
-		if(ret.exists()){
+		DocHandle ret = new DocHandle(id);
+		if (ret.exists()) {
 			return ret;
 		}
 		return null;
 	}
 	
 	@Override
-	public String getLabel() {
-		StringBuilder sb=new StringBuilder();
+	public String getLabel(){
+		StringBuilder sb = new StringBuilder();
 		sb.append(get("Datum")).append(" ").append(get("Titel"));
 		return sb.toString();
 	}
-
+	
 	@Override
-	public boolean delete() {
+	public boolean delete(){
 		return super.delete();
 	}
 	
 	public void execute(){
-		try{
-			String ext="";
-			String typname=get("Mimetype");
-			int r=typname.lastIndexOf('.');
-			if(r==-1){
-				typname=get("Titel");
-				r=typname.lastIndexOf('.');
+		try {
+			String ext = "";
+			String typname = get("Mimetype");
+			int r = typname.lastIndexOf('.');
+			if (r == -1) {
+				typname = get("Titel");
+				r = typname.lastIndexOf('.');
 			}
 			
-			if(r!=-1){
-				ext=typname.substring(r+1);
+			if (r != -1) {
+				ext = typname.substring(r + 1);
 			}
-			File temp=File.createTempFile("omni_", "_vore."+ext);
-			byte[] b=getBinary("Doc");
-			if(b==null){
-				SWTHelper.showError("Fehler beim lesen", "Konnte das Dokument nicht aus der Datenbank laden");
+			File temp = File.createTempFile("omni_", "_vore." + ext);
+			byte[] b = getBinary("Doc");
+			if (b == null) {
+				SWTHelper.showError("Fehler beim lesen",
+					"Konnte das Dokument nicht aus der Datenbank laden");
 				return;
 			}
-			FileOutputStream fos=new FileOutputStream(temp);
+			FileOutputStream fos = new FileOutputStream(temp);
 			fos.write(b);
 			fos.close();
-			Program proggie=Program.findProgram(ext);
-			if(proggie!=null){
+			Program proggie = Program.findProgram(ext);
+			if (proggie != null) {
 				proggie.execute(temp.getAbsolutePath());
-			}else{
-				if(Program.launch(temp.getAbsolutePath())==false){
-					Runtime.getRuntime().exec(temp.getAbsolutePath());	
+			} else {
+				if (Program.launch(temp.getAbsolutePath()) == false) {
+					Runtime.getRuntime().exec(temp.getAbsolutePath());
 				}
-								
+				
 			}
-
-		}catch(Exception ex){
+			
+		} catch (Exception ex) {
 			ExHandler.handle(ex);
 			SWTHelper.showError("Konnte Datei nicht starten", ex.getMessage());
 		}
 	}
+	
 	@Override
-	protected String getTableName() {
+	protected String getTableName(){
 		return TABLENAME;
 	}
 	
 	protected DocHandle(String id){
 		super(id);
 	}
+	
 	protected DocHandle(){}
 	
-	public static void assimilate(String f) {
-		Patient act=GlobalEvents.getSelectedPatient();
-		if(act==null){
-			SWTHelper.showError("Kein Patient ausgewählt", "Bitte wählen Sie zuerst einen Patienten, dem dieses Doukentn zugeordnet werden soll");
+	public static void assimilate(String f){
+		Patient act = GlobalEvents.getSelectedPatient();
+		if (act == null) {
+			SWTHelper
+				.showError("Kein Patient ausgewählt",
+					"Bitte wählen Sie zuerst einen Patienten, dem dieses Doukentn zugeordnet werden soll");
 			return;
 		}
-		File file=new File(f);
-		if(!file.canRead()){
-			SWTHelper.showError("Kann Datei nicht lesen", "Die Datei "+f+" kann nicht gelesen werden");
+		File file = new File(f);
+		if (!file.canRead()) {
+			SWTHelper.showError("Kann Datei nicht lesen", "Die Datei " + f
+				+ " kann nicht gelesen werden");
 			return;
 		}
-		FileImportDialog fid=new FileImportDialog(file.getName());
-		if(fid.open()==Dialog.OK){
-			try{
-				BufferedInputStream bis=new BufferedInputStream(new FileInputStream(file));
-				ByteArrayOutputStream baos=new ByteArrayOutputStream();
+		FileImportDialog fid = new FileImportDialog(file.getName());
+		if (fid.open() == Dialog.OK) {
+			try {
+				BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+				ByteArrayOutputStream baos = new ByteArrayOutputStream();
 				int in;
-				while((in=bis.read())!=-1){
+				while ((in = bis.read()) != -1) {
 					baos.write(in);
 				}
 				bis.close();
 				baos.close();
-				String nam=file.getName();
-				if(nam.length()>255){
-					SWTHelper.showError("Fehler beim Einlesen", "Der Dateiname ist zu lang (max. 255 Zeichen");
+				String nam = file.getName();
+				if (nam.length() > 255) {
+					SWTHelper.showError("Fehler beim Einlesen",
+						"Der Dateiname ist zu lang (max. 255 Zeichen");
 					return;
 				}
-				new DocHandle(baos.toByteArray(),act,fid.title,file.getName(),fid.keywords);
-			}catch(Exception ex){
+				new DocHandle(baos.toByteArray(), act, fid.title, file.getName(), fid.keywords);
+			} catch (Exception ex) {
 				ExHandler.handle(ex);
-				SWTHelper.showError("Fehler beim Einlesen", "Es ist ein Fehler beim Einlesen passiert. Bitte log prüfen");
+				SWTHelper.showError("Fehler beim Einlesen",
+					"Es ist ein Fehler beim Einlesen passiert. Bitte log prüfen");
 			}
 		}
 		
 	}
 	
-
 }
