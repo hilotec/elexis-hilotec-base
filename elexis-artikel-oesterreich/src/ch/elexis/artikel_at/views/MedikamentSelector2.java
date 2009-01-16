49886d34c570c62c3bd28b4d8f17b9d76363c2ea
rgw_ch <devnull@localhost>
2009-01-17 00:38:05 +0100

    [svn]

--- elexis-artikel-oesterreich/src/ch/elexis/artikel_at/views/MedikamentSelector2.java
+++ elexis-artikel-oesterreich/src/ch/elexis/artikel_at/views/MedikamentSelector2.java
@@ -26,24 +26,24 @@ import ch.elexis.views.codesystems.CodeSelectorFactory;
 
 public class MedikamentSelector2 extends CodeSelectorFactory {
 	SelectorPanelProvider slp;
-	public static final String SELECT_NAME="Name";
-	public static final String SELECT_SUBSTANCE="Substanz";
-	public static final String SELECT_NOTE="Notiz";
-	String[] fields=new String[]{SELECT_NAME,SELECT_SUBSTANCE,SELECT_NOTE};
+	public static final String SELECT_NAME = "Name";
+	public static final String SELECT_SUBSTANCE = "Substanz";
+	public static final String SELECT_NOTE = "Notiz";
+	String[] fields = new String[] {
+		SELECT_NAME, SELECT_SUBSTANCE, SELECT_NOTE
+	};
 	private CommonViewer cv;
 	
 	@Override
 	public ViewerConfigurer createViewerConfigurer(CommonViewer cv){
-		this.cv=cv;
-		slp=new SelectorPanelProvider(fields);
-		ContentProvider cp=new ContentProvider(this);
-		ViewerConfigurer vc=new ViewerConfigurer(
-			cp,
-			new VidalLabelProvider(),
-			new SimpleWidgetProvider(SimpleWidgetProvider.TYPE_TABLE,SWT.NONE,cv)
-			);
+		this.cv = cv;
+		slp = new SelectorPanelProvider(fields, true);
+		ContentProvider cp = new ContentProvider(this);
+		ViewerConfigurer vc =
+			new ViewerConfigurer(cp, new VidalLabelProvider(), new SimpleWidgetProvider(
+				SimpleWidgetProvider.TYPE_TABLE, SWT.NONE, cv));
 		vc.setControlFieldProvider(slp);
-
+		
 		return vc;
 	}
 	
@@ -54,6 +54,7 @@ public class MedikamentSelector2 extends CodeSelectorFactory {
 	public ViewerConfigurer getConfigurer(){
 		return cv.getConfigurer();
 	}
+	
 	@Override
 	public void dispose(){
 	// TODO Auto-generated method stub
