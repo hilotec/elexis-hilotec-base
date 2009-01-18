76f95a964477ad6e1943da93ee20b19eaea386ff
rgw_ch <devnull@localhost>
2009-01-18 17:54:18 +0100

    [svn] introduced new ACL/ACE system

--- elexis-befunde/src/ch/elexis/befunde/ACLContributor.java
+++ elexis-befunde/src/ch/elexis/befunde/ACLContributor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, G. Weirich and Elexis
+ * Copyright (c) 2007-2009, G. Weirich and Elexis
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,26 +8,28 @@
  * Contributors:
  *    G. Weirich - initial implementation
  *    
- *  $Id: ACLContributor.java 2516 2007-06-12 15:56:07Z rgw_ch $
+ *  $Id: ACLContributor.java 4972 2009-01-18 16:54:18Z rgw_ch $
  *******************************************************************************/
 package ch.elexis.befunde;
 
+import ch.elexis.admin.ACE;
 import ch.elexis.admin.AccessControlDefaults;
 import ch.elexis.admin.IACLContributor;
 
 public class ACLContributor implements IACLContributor {
-	public static final String DELETE_PARAM=AccessControlDefaults.DELETE+"/Messwertrubrik";
-	public static final String ADD_PARAM= "Messwert/Befund zufügen";
-			
-	public String[] getACL() {
-		return new String[]{
-				DELETE_PARAM,
-				ADD_PARAM
+	public static final ACE ACE_BEFUNDE = new ACE(ACE.ACE_ROOT, "Messwert", "Messwert");
+	public static final ACE DELETE_PARAM =
+		new ACE(AccessControlDefaults.DELETE, "Messwertrubrik", "Messwertrubrik");
+	public static final ACE ADD_PARAM = new ACE(ACE_BEFUNDE, "Befund zufügen", "Befund zufügen");
+	
+	public ACE[] getACL(){
+		return new ACE[] {
+			DELETE_PARAM, ADD_PARAM
 		};
 	}
-
-	public String[] reject(String[] acl) {
+	
+	public ACE[] reject(ACE[] acl){
 		return null;
 	}
-
+	
 }
