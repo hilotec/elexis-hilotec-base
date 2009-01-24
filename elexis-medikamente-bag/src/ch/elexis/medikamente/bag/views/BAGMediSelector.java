880dec7ad7669afc2259f1a9f453d679c1807cd5
rgw_ch <devnull@localhost>
2009-01-24 17:36:00 +0100

    [svn]

--- elexis-medikamente-bag/src/ch/elexis/medikamente/bag/views/BAGMediSelector.java
+++ elexis-medikamente-bag/src/ch/elexis/medikamente/bag/views/BAGMediSelector.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, G. Weirich and Elexis
+ * Copyright (c) 2007-2009, G. Weirich and Elexis
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,7 +8,7 @@
  * Contributors:
  *    G. Weirich - initial implementation
  *    
- *  $Id: BAGMediSelector.java 5016 2009-01-23 16:32:22Z rgw_ch $
+ *  $Id: BAGMediSelector.java 5030 2009-01-24 16:36:00Z rgw_ch $
  *******************************************************************************/
 package ch.elexis.medikamente.bag.views;
 
@@ -18,6 +18,7 @@ import org.eclipse.swt.SWT;
 
 import ch.elexis.medikamente.bag.data.BAGMedi;
 import ch.elexis.medikamente.bag.data.BAGMediFactory;
+import ch.elexis.selectors.SelectorPanelProvider;
 import ch.elexis.util.viewers.CommonViewer;
 import ch.elexis.util.viewers.SimpleWidgetProvider;
 import ch.elexis.util.viewers.ViewerConfigurer;
@@ -27,58 +28,57 @@ import ch.elexis.views.codesystems.CodeSelectorFactory;
 public class BAGMediSelector extends CodeSelectorFactory {
 	private IAction sameOfGroupAction;
 	CommonViewer cv;
-
-	public BAGMediSelector() {
+	SelectorPanelProvider slp;
+	
+	public BAGMediSelector(){
 		makeActions();
 	}
+	
 	@Override
-	public ViewerConfigurer createViewerConfigurer(final CommonViewer cv) {
-		ArtikelContextMenu menu=new ArtikelContextMenu((BAGMedi)new BAGMediFactory().createTemplate(BAGMedi.class),cv);
+	public ViewerConfigurer createViewerConfigurer(final CommonViewer cv){
+		ArtikelContextMenu menu =
+			new ArtikelContextMenu((BAGMedi) new BAGMediFactory().createTemplate(BAGMedi.class), cv);
 		menu.addAction(sameOfGroupAction);
 		//
-		this.cv=cv;
-		return new ViewerConfigurer(
-				new ContentProvider(cv),
-				new BAGMediLabelProvider(),
-				new ControlFieldProvider(cv),
-				new ViewerConfigurer.DefaultButtonProvider(),
-				new SimpleWidgetProvider(SimpleWidgetProvider.TYPE_LAZYLIST, SWT.NONE,null)
-		);
+		this.cv = cv;
+		return new ViewerConfigurer(new ContentProvider(cv), new BAGMediLabelProvider(),
+			new ControlFieldProvider(cv), new ViewerConfigurer.DefaultButtonProvider(),
+			new SimpleWidgetProvider(SimpleWidgetProvider.TYPE_LAZYLIST, SWT.NONE, null));
 	}
-
-
+	
 	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
-		
+	public void dispose(){
+	// TODO Auto-generated method stub
+	
 	}
-
+	
 	@Override
-	public String getCodeSystemName() {
+	public String getCodeSystemName(){
 		return BAGMedi.CODESYSTEMNAME;
 	}
-
+	
 	@SuppressWarnings("unchecked")
 	@Override
-	public Class getElementClass() {
+	public Class getElementClass(){
 		return BAGMedi.class;
 	}
 	
 	private void makeActions(){
-		sameOfGroupAction=new Action("Selbe therap. Gruppe"){
+		sameOfGroupAction = new Action("Selbe therap. Gruppe") {
 			{
 				setToolTipText("Zeige alle Medikamente derselben therapeutischen Gruppe");
 			}
-
+			
 			@Override
-			public void run() {
-				ContentProvider cp=(ContentProvider) cv.getConfigurer().getContentProvider();
-				BAGMedi selected=(BAGMedi)cv.getSelection()[0];
-				cp.group=selected.get("Gruppe");
-				ControlFieldProvider cfp=(ControlFieldProvider)cv.getConfigurer().getControlFieldProvider();
-				if(cfp.isEmpty()){
-					cv.notify(CommonViewer.Message.update_keeplabels);	
-				}else{
+			public void run(){
+				ContentProvider cp = (ContentProvider) cv.getConfigurer().getContentProvider();
+				BAGMedi selected = (BAGMedi) cv.getSelection()[0];
+				cp.group = selected.get("Gruppe");
+				ControlFieldProvider cfp =
+					(ControlFieldProvider) cv.getConfigurer().getControlFieldProvider();
+				if (cfp.isEmpty()) {
+					cv.notify(CommonViewer.Message.update_keeplabels);
+				} else {
 					cfp.clearValues();
 				}
 				
@@ -86,5 +86,5 @@ public class BAGMediSelector extends CodeSelectorFactory {
 			
 		};
 	}
-
+	
 }
