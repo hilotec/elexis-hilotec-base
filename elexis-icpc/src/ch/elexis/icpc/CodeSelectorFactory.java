3592709b49c06f652335f8cf721426edc82a296e
rgw_ch <devnull@localhost>
2009-01-25 20:48:02 +0100

    [svn] signature Codeselectorfactory changed

--- elexis-icpc/src/ch/elexis/icpc/CodeSelectorFactory.java
+++ elexis-icpc/src/ch/elexis/icpc/CodeSelectorFactory.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006-2008, G. Weirich and Elexis
+ * Copyright (c) 2006-2009, G. Weirich and Elexis
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,10 +8,12 @@
  * Contributors:
  *    G. Weirich - initial implementation
  *    
- *    $Id: CodeSelectorFactory.java 5015 2009-01-23 16:31:47Z rgw_ch $
+ *    $Id: CodeSelectorFactory.java 5037 2009-01-25 19:48:02Z rgw_ch $
  *******************************************************************************/
 
 package ch.elexis.icpc;
+
+import java.util.HashMap;
 
 import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.LabelProvider;
@@ -26,97 +28,94 @@ import ch.elexis.util.viewers.ViewerConfigurer.CommonContentProvider;
 import ch.rgw.tools.Tree;
 
 public class CodeSelectorFactory extends ch.elexis.views.codesystems.CodeSelectorFactory {
-
 	
-	public CodeSelectorFactory() {
-		// TODO Auto-generated constructor stub
+	public CodeSelectorFactory(){
+	// TODO Auto-generated constructor stub
 	}
-
+	
 	@Override
-	public ViewerConfigurer createViewerConfigurer(CommonViewer cv) {
-		return new ViewerConfigurer(
-				new IcpcCodeContentProvider(),
-				new IcpcCodeLabelProvider(),
-				new DefaultControlFieldProvider(cv, new String[]{"Text"}),
-				new ViewerConfigurer.DefaultButtonProvider(),
-				new SimpleWidgetProvider(SimpleWidgetProvider.TYPE_TREE, SWT.NONE, null)
-				);
+	public ViewerConfigurer createViewerConfigurer(CommonViewer cv){
+		return new ViewerConfigurer(new IcpcCodeContentProvider(), new IcpcCodeLabelProvider(),
+			new DefaultControlFieldProvider(cv, new String[] {
+				"Text"
+			}), new ViewerConfigurer.DefaultButtonProvider(), new SimpleWidgetProvider(
+				SimpleWidgetProvider.TYPE_TREE, SWT.NONE, null));
 	}
-
+	
 	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
-
+	public void dispose(){
+	// TODO Auto-generated method stub
+	
 	}
-
+	
 	@Override
-	public String getCodeSystemName() {
+	public String getCodeSystemName(){
 		return "ICPC";
 	}
-
+	
 	@Override
-	public Class getElementClass() {
+	public Class getElementClass(){
 		return IcpcCode.class;
 	}
+	
 	public class IcpcCodeContentProvider implements CommonContentProvider, ITreeContentProvider {
-
-		public void startListening() {}
-
-		public void stopListening() {}
-
-		public Object[] getElements(Object inputElement) {
+		
+		public void startListening(){}
+		
+		public void stopListening(){}
+		
+		public Object[] getElements(Object inputElement){
 			return IcpcCode.getRoot().getChildren().toArray();
 		}
-
-		public void dispose() {}
-
-		public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {}
-
-		public void changed(String[] fields, String[] values) {
+		
+		public void dispose(){}
+		
+		public void inputChanged(Viewer viewer, Object oldInput, Object newInput){}
+		
+		public void changed(HashMap<String, String> values){}
+		
+		public void reorder(String field){}
+		
+		public void selected(){
+		// nothing to do
 		}
-
-		public void reorder(String field) {
-		}
-
-		public void selected() {
-			// nothing to do
-		}
-
-		public Object[] getChildren(Object parentElement) {
-			if(parentElement instanceof Tree){
-				Tree t=(Tree)parentElement;
+		
+		public Object[] getChildren(Object parentElement){
+			if (parentElement instanceof Tree) {
+				Tree t = (Tree) parentElement;
 				return t.getChildren().toArray();
 			}
 			return null;
 		}
-
-		public Object getParent(Object element) {
-			if(element instanceof Tree){
-				Tree t=(Tree)element;
+		
+		public Object getParent(Object element){
+			if (element instanceof Tree) {
+				Tree t = (Tree) element;
 				return t.getParent();
 			}
 			return null;
 		}
-
-		public boolean hasChildren(Object element) {
-			if(element instanceof Tree){
-				Tree t=(Tree)element;
+		
+		public boolean hasChildren(Object element){
+			if (element instanceof Tree) {
+				Tree t = (Tree) element;
 				return t.hasChildren();
 			}
 			return false;
 		}
-
+		
 	}
+	
 	public class IcpcCodeLabelProvider extends LabelProvider {
-
+		
 		@Override
-		public String getText(Object element) {
-			if(element instanceof Tree){
-				Tree t=(Tree)element;
-				if(t.contents instanceof String){
-					return (String)t.contents;
-				}else if(t.contents instanceof IcpcCode){
-					IcpcCode c=(IcpcCode)t.contents;
+		public String getText(Object element){
+			if (element instanceof Tree) {
+				Tree t = (Tree) element;
+				if (t.contents instanceof String) {
+					return (String) t.contents;
+				} else if (t.contents instanceof IcpcCode) {
+					IcpcCode c = (IcpcCode) t.contents;
 					return c.getLabel();
 				}
 			}
@@ -124,5 +123,5 @@ public class CodeSelectorFactory extends ch.elexis.views.codesystems.CodeSelecto
 		}
 		
 	}
-
+	
 }
