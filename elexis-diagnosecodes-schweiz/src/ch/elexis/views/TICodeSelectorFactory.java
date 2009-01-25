e0aa791c7a1c4d82ebbd6a642827f59929cd3c97
rgw_ch <devnull@localhost>
2009-01-25 20:50:03 +0100

    [svn] signature Codeselectorfactory changed

--- elexis-diagnosecodes-schweiz/src/ch/elexis/views/TICodeSelectorFactory.java
+++ elexis-diagnosecodes-schweiz/src/ch/elexis/views/TICodeSelectorFactory.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, G. Weirich and Elexis
+ * Copyright (c) 2006-2009, G. Weirich and Elexis
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,10 +8,12 @@
  * Contributors:
  *    G. Weirich - initial implementation
  *    
- *    $Id: TICodeSelectorFactory.java 5018 2009-01-23 16:33:17Z rgw_ch $
+ *    $Id: TICodeSelectorFactory.java 5041 2009-01-25 19:50:03Z rgw_ch $
  *******************************************************************************/
 
 package ch.elexis.views;
+
+import java.util.HashMap;
 
 import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.LabelProvider;
@@ -27,74 +29,82 @@ import ch.elexis.util.viewers.ViewerConfigurer;
 import ch.elexis.util.viewers.ViewerConfigurer.CommonContentProvider;
 import ch.elexis.views.codesystems.CodeSelectorFactory;
 
-public class TICodeSelectorFactory extends CodeSelectorFactory{
-
+public class TICodeSelectorFactory extends CodeSelectorFactory {
+	
 	public TICodeSelectorFactory(){
 		System.out.println("hier"); //$NON-NLS-1$
 	}
+	
 	@Override
-	public ViewerConfigurer createViewerConfigurer(CommonViewer cv) {
-		return new ViewerConfigurer(
-				new TICodeContentProvider(),
-				new TICodeLabelProvider(),
-				new DefaultControlFieldProvider(cv, new String[]{"Text"}), //$NON-NLS-1$
-				new ViewerConfigurer.DefaultButtonProvider(),
-				new SimpleWidgetProvider(SimpleWidgetProvider.TYPE_TREE, SWT.NONE, null)
-				);
+	public ViewerConfigurer createViewerConfigurer(CommonViewer cv){
+		return new ViewerConfigurer(new TICodeContentProvider(), new TICodeLabelProvider(),
+			new DefaultControlFieldProvider(cv, new String[] {
+				"Text"}), //$NON-NLS-1$
+			new ViewerConfigurer.DefaultButtonProvider(), new SimpleWidgetProvider(
+				SimpleWidgetProvider.TYPE_TREE, SWT.NONE, null));
 	}
 	
-	static class TICodeContentProvider implements ITreeContentProvider, CommonContentProvider{
-		public Object[] getChildren(Object parentElement) {
-			TICode c=(TICode)parentElement;
+	static class TICodeContentProvider implements ITreeContentProvider, CommonContentProvider {
+		public Object[] getChildren(Object parentElement){
+			TICode c = (TICode) parentElement;
 			return c.getChildren();
 		}
-
-		public Object getParent(Object element) {
-			TICode c=(TICode)element;
+		
+		public Object getParent(Object element){
+			TICode c = (TICode) element;
 			return c.getParent();
 		}
-
-		public boolean hasChildren(Object element) {
-			TICode c=(TICode)element;
+		
+		public boolean hasChildren(Object element){
+			TICode c = (TICode) element;
 			return c.hasChildren();
 		}
-
-		public Object[] getElements(Object inputElement) {
+		
+		public Object[] getElements(Object inputElement){
 			return TICode.getRootNodes();
 		}
-
-		public void dispose() {
-			// TODO Auto-generated method stub
-			
+		
+		public void dispose(){
+		// TODO Auto-generated method stub
+		
 		}
-
-		public void inputChanged(Viewer v, Object oldInput, Object newInput) {}
-		public void startListening() {}
-		public void stopListening() {}
-		public void changed(String[] fields, String[] values) {}
-		public void reorder(String field) {}
-		public void selected() {}
+		
+		public void inputChanged(Viewer v, Object oldInput, Object newInput){}
+		
+		public void startListening(){}
+		
+		public void stopListening(){}
+		
+		public void changed(HashMap<String, String> values){}
+		
+		public void reorder(String field){}
+		
+		public void selected(){}
 	}
-	static class TICodeLabelProvider extends LabelProvider{
-		public String getText(Object element) {
-			TICode c=(TICode)element;
-			return c.getCode()+" "+c.getText(); //$NON-NLS-1$
+	
+	static class TICodeLabelProvider extends LabelProvider {
+		public String getText(Object element){
+			TICode c = (TICode) element;
+			return c.getCode() + " " + c.getText(); //$NON-NLS-1$
 		}
-
+		
 		@Override
-		public Image getImage(Object element) {
+		public Image getImage(Object element){
 			return null;
 		}
 		
 	}
+	
 	@Override
-	public Class getElementClass() {
+	public Class getElementClass(){
 		return TICode.class;
 	}
+	
 	@Override
-	public void dispose() {}
+	public void dispose(){}
+	
 	@Override
-	public String getCodeSystemName() {
+	public String getCodeSystemName(){
 		return "TI-Code"; //$NON-NLS-1$
 	}
 }
