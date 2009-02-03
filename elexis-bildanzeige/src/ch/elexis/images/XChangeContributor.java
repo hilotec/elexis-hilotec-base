feb6f0b95888294dc9c5e3b02c90c46775b1bd57
rgw_ch <devnull@localhost>
2009-02-03 19:28:09 +0100

    [svn] xchange auf decorator pattern umgebaut

--- elexis-bildanzeige/src/ch/elexis/images/XChangeContributor.java
+++ elexis-bildanzeige/src/ch/elexis/images/XChangeContributor.java
@@ -8,11 +8,10 @@
  * Contributors:
  *    G. Weirich - initial implementation
  *    
- *  $Id: XChangeContributor.java 4413 2008-09-14 20:06:45Z rgw_ch $
+ *  $Id: XChangeContributor.java 5079 2009-02-03 18:28:09Z rgw_ch $
  *******************************************************************************/
 package ch.elexis.images;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.core.runtime.CoreException;
@@ -29,21 +28,21 @@ import ch.elexis.exchange.XChangeContainer;
 import ch.elexis.exchange.elements.DocumentElement;
 import ch.elexis.exchange.elements.MedicalElement;
 import ch.elexis.exchange.elements.RecordElement;
-import ch.elexis.util.XMLTool;
+import ch.rgw.tools.XMLTool;
 
 public class XChangeContributor implements IExchangeContributor {
-	public static final String PLUGIN_ID="ch.elexis.bildanzeige";
-
-	public void exportHook(MedicalElement me) {
+	public static final String PLUGIN_ID = "ch.elexis.bildanzeige";
+	
+	public void exportHook(MedicalElement me){
 		
-		Patient p=(Patient)me.getContainer().getMapping(me);
-		Query<Bild> qbe=new Query<Bild>(Bild.class);
+		Patient p = (Patient) me.getContainer().getMapping(me);
+		Query<Bild> qbe = new Query<Bild>(Bild.class);
 		qbe.add("PatID", "=", p.getId());
-		List<Bild> images=qbe.execute();
-		for(Bild img:images){
-			byte[] data=img.getData();
-			if(data!=null && data.length>0){
-				DocumentElement de=new DocumentElement(me.getContainer());
+		List<Bild> images = qbe.execute();
+		for (Bild img : images) {
+			byte[] data = img.getData();
+			if (data != null && data.length > 0) {
+				DocumentElement de = new DocumentElement(me.getContainer(), (Element) null);
 				de.setMimetype("image/jpeg");
 				de.setDate(XMLTool.dateToXmlDate(img.getDate()));
 				de.setTitle(img.getTitle());
@@ -57,79 +56,63 @@ public class XChangeContributor implements IExchangeContributor {
 		}
 		
 	}
-
+	
 	@SuppressWarnings("unchecked")
-	public void importHook(XChangeContainer container, PersistentObject context) {
-		String rootpath=container.getProperty("ROOTPATH");
-		List<RecordElement> records=(List<RecordElement>) container.getElements(rootpath+"/records/record");
-		for(Element re:records){
+	public void importHook(XChangeContainer container, PersistentObject context){
+		String rootpath = container.getProperty("ROOTPATH");
+		List<RecordElement> records =
+			(List<RecordElement>) container.getElements(rootpath + "/records/record");
+		for (RecordElement re : records) {
 			/*
-			List<Element> xrefs=re.getChildren("xref", container.getNamespace());
-			Samdas smd=new Samdas(k.getEintrag().getHead());
-			if(xrefs!=null){
-				for(Element e:xrefs){
-					String type=e.getAttributeValue("type");
-					if(type.equalsIgnoreCase("image/jpeg") || type.equalsIgnoreCase("image/png")){
-						String content=e.getAttributeValue("content");
-						String id=e.getAttributeValue("id");
-						byte[] data=null;
-						if(content.equals("inline")){
-							BASE64Decoder b64=new BASE64Decoder();
-							try {
-								data=b64.decodeBuffer(e.getText());
-							} catch (IOException e1) {
-								ExHandler.handle(e1);
-								continue;
-							}
-						}else{
-							data=container.getBinary(id);
-						}
-						Patient pat=k.getFall().getPatient();
-						Bild bild=new Bild(pat,"type",data);
-						String spos=e.getAttributeValue("pos");
-						int pos=(spos==null) ? 0 : Integer.parseInt(spos);
-						String slen=e.getAttributeValue("len");
-						int len=(slen==null) ? 0 : Integer.parseInt(slen);
-						Samdas.Record rec=smd.getRecord();
-						Samdas.XRef xr=new Samdas.XRef("bildanzeige",bild.getId(),pos,len);
-						rec.add(xr);
-						k.updateEintrag(smd.toString(), true);
-					}
-				}
-			}
-*/
+			 * List<Element> xrefs=re.getChildren("xref", container.getNamespace()); Samdas smd=new
+			 * Samdas(k.getEintrag().getHead()); if(xrefs!=null){ for(Element e:xrefs){ String
+			 * type=e.getAttributeValue("type"); if(type.equalsIgnoreCase("image/jpeg") ||
+			 * type.equalsIgnoreCase("image/png")){ String content=e.getAttributeValue("content");
+			 * String id=e.getAttributeValue("id"); byte[] data=null; if(content.equals("inline")){
+			 * BASE64Decoder b64=new BASE64Decoder(); try { data=b64.decodeBuffer(e.getText()); }
+			 * catch (IOException e1) { ExHandler.handle(e1); continue; } }else{
+			 * data=container.getBinary(id); } Patient pat=k.getFall().getPatient(); Bild bild=new
+			 * Bild(pat,"type",data); String spos=e.getAttributeValue("pos"); int pos=(spos==null) ?
+			 * 0 : Integer.parseInt(spos); String slen=e.getAttributeValue("len"); int
+			 * len=(slen==null) ? 0 : Integer.parseInt(slen); Samdas.Record rec=smd.getRecord();
+			 * Samdas.XRef xr=new Samdas.XRef("bildanzeige",bild.getId(),pos,len); rec.add(xr);
+			 * k.updateEintrag(smd.toString(), true); } } }
+			 */
 		}
 		
 	}
-
-	public void setInitializationData(IConfigurationElement config, String propertyName, Object data) throws CoreException {
-		// Nothing
-		
+	
+	public void setInitializationData(IConfigurationElement config, String propertyName, Object data)
+		throws CoreException{
+	// Nothing
+	
 	}
+	
+	public boolean init(MedicalElement me, boolean bExport){
+		if (bExport) {
 
-	public boolean init(MedicalElement me,boolean bExport){
-		if(bExport){
-			
 		}
 		return true;
 	}
-
+	
 	public IExchangeDataHandler[] getImportHandlers(){
-		return new IExchangeDataHandler[]{new ImportHandler()};
+		return new IExchangeDataHandler[] {
+			new ImportHandler()
+		};
 	}
-
-	static class ImportHandler implements IExchangeDataHandler{
-
+	
+	static class ImportHandler implements IExchangeDataHandler {
+		
 		public String getDatatype(){
 			return DocumentElement.XMLNAME;
 		}
-
+		
 		public String[] getRestrictions(){
-			String[] ret=new String[1];
-			ret[0]="@mimetype=image/jpeg";
+			String[] ret = new String[1];
+			ret[0] = "@mimetype=image/jpeg";
 			return ret;
 		}
-
+		
 		public int getValue(){
 			return 1;
 		}
