26bba9590b2f5fa2f8f0bf496082fc7c827338e4
rgw_ch <devnull@localhost>
2009-02-10 18:42:50 +0100

    [svn]

---
+++ elexis-fire/src/ch/elexis/icpc/fire/handlers/Analyzer.java
@@ -0,0 +1,12 @@
+package ch.elexis.icpc.fire.handlers;
+
+import ch.elexis.data.Konsultation;
+
+public class Analyzer {
+	private Konsultation mine;
+	
+	public Analyzer(Konsultation k){
+		mine = k;
+		
+	}
+}
