bf75d46385e6c517f9ab801ae38f6a1d40b841fa
rgw_ch <devnull@localhost>
2009-02-10 18:42:43 +0100

    [svn]

--- elexis-icpc/src/ch/elexis/icpc/Encounter.java
+++ elexis-icpc/src/ch/elexis/icpc/Encounter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007-2008, G. Weirich and Elexis
+ * Copyright (c) 2007-2009, G. Weirich and Elexis
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,7 +8,7 @@
  * Contributors:
  *    G. Weirich - initial implementation
  *    
- *  $Id: Encounter.java 3875 2008-05-05 16:59:47Z rgw_ch $
+ *  $Id: Encounter.java 5119 2009-02-10 17:42:43Z rgw_ch $
  *******************************************************************************/
 package ch.elexis.icpc;
 
@@ -19,32 +19,31 @@ import ch.rgw.tools.StringTool;
 import ch.rgw.tools.VersionInfo;
 
 public class Encounter extends PersistentObject {
-	private static final String VERSION="0.2.0";
-	private static final String TABLENAME="CH_ELEXIS_ICPC_ENCOUNTER";
+	private static final String VERSION = "0.2.1";
+	private static final String TABLENAME = "CH_ELEXIS_ICPC_ENCOUNTER";
 	
-	private static final String createDB="CREATE TABLE "+TABLENAME+" ("+
-	"ID				VARCHAR(25),"+
-	"deleted		CHAR(1) default '0',"+
-	"KONS			VARCHAR(25),"+
-	"EPISODE		VARCHAR(25),"+
-	"RFE			CHAR(4),"+
-	"DIAG			CHAR(4),"+
-	"PROC			CHAR(4),"+
-	"ExtInfo		BLOB);"+
-	"CREATE INDEX "+TABLENAME+"1 ON "+TABLENAME+" (KONS);"+
-	"CREATE INDEX "+TABLENAME+"2 ON "+TABLENAME+" (EPISODE);"+
-	"INSERT INTO "+TABLENAME+" (ID,KONS) VALUES ('1',"+JdbcLink.wrap(VERSION)+");";
+	private static final String createDB =
+		"CREATE TABLE " + TABLENAME + " (" + "ID				VARCHAR(25)," + "lastupdate 	BIGINT,"
+			+ "deleted		CHAR(1) default '0'," + "KONS			VARCHAR(25)," + "EPISODE		VARCHAR(25),"
+			+ "RFE			CHAR(4)," + "DIAG			CHAR(4)," + "PROC			CHAR(4)," + "ExtInfo		BLOB);"
+			+ "CREATE INDEX " + TABLENAME + "1 ON " + TABLENAME + " (KONS);" + "CREATE INDEX "
+			+ TABLENAME + "2 ON " + TABLENAME + " (EPISODE);" + "INSERT INTO " + TABLENAME
+			+ " (ID,KONS) VALUES ('1'," + JdbcLink.wrap(VERSION) + ");";
 	
-	static{
-		addMapping(TABLENAME,"KonsID=KONS","EpisodeID=EPISODE","RFE","Diag","Proc","ExtInfo");
-		Encounter version=load("1");
-		if(!version.exists()){
+	static {
+		addMapping(TABLENAME, "KonsID=KONS", "EpisodeID=EPISODE", "RFE", "Diag", "Proc", "ExtInfo");
+		Encounter version = load("1");
+		if (!version.exists()) {
 			createTable(TABLENAME, createDB);
-		}else{
-			VersionInfo vi=new VersionInfo(version.get("KonsID"));
-			if(vi.isOlder(VERSION)){
-				if(vi.isOlder("0.2.0")){
-					getConnection().exec("ALTER TABLE "+TABLENAME+" ADD deleted CHAR(1) default '0';");
+		} else {
+			VersionInfo vi = new VersionInfo(version.get("KonsID"));
+			if (vi.isOlder(VERSION)) {
+				if (vi.isOlder("0.2.0")) {
+					getConnection().exec(
+						"ALTER TABLE " + TABLENAME + " ADD deleted CHAR(1) default '0';");
+					version.set("KonsID", VERSION);
+				} else if (vi.isOlder("0.2.1")) {
+					createTable(TABLENAME, "ALTER TABLE " + TABLENAME + " ADD lastupdate BIGINT;");
 					version.set("KonsID", VERSION);
 				}
 			}
@@ -53,27 +52,30 @@ public class Encounter extends PersistentObject {
 	
 	public Encounter(Konsultation kons, Episode ep){
 		create(null);
-		set(new String[]{"KonsID","EpisodeID"},kons.getId(),ep.getId());
+		set(new String[] {
+			"KonsID", "EpisodeID"
+		}, kons.getId(), ep.getId());
 	}
+	
 	@Override
-	public String getLabel() {
-		StringBuilder sb=new StringBuilder();
+	public String getLabel(){
+		StringBuilder sb = new StringBuilder();
 		
-		sb.append(getKons().getDatum()).append(": ")
-			.append(get("RFE")).append(", ")
-			.append(get("Diag")).append(", ")
-			.append(get("Proc"));
+		sb.append(getKons().getDatum()).append(": ").append(get("RFE")).append(", ").append(
+			get("Diag")).append(", ").append(get("Proc"));
 		return sb.toString();
 	}
-
+	
 	public Konsultation getKons(){
 		return Konsultation.load(get("KonsID"));
 	}
+	
 	public Episode getEpisode(){
 		return Episode.load(get("EpisodeID"));
 	}
+	
 	@Override
-	protected String getTableName() {
+	protected String getTableName(){
 		return TABLENAME;
 	}
 	
@@ -86,34 +88,32 @@ public class Encounter extends PersistentObject {
 	}
 	
 	protected Encounter(){}
-
-	public void setRFE(IcpcCode code) {
-		set("RFE",code.getId());
+	
+	public void setRFE(IcpcCode code){
+		set("RFE", code.getId());
 		
 	}
+	
 	public void setDiag(IcpcCode code){
-		set("Diag",code.getId());
+		set("Diag", code.getId());
 	}
+	
 	public void setProc(IcpcCode code){
-		set("Proc",code.getId());
+		set("Proc", code.getId());
 	}
 	
 	public IcpcCode getRFE(){
-		String rfeID=get("RFE");
-		return StringTool.isNothing(rfeID)?
-				null :
-				IcpcCode.load(rfeID);
+		String rfeID = get("RFE");
+		return StringTool.isNothing(rfeID) ? null : IcpcCode.load(rfeID);
 	}
+	
 	public IcpcCode getDiag(){
-		String diagID=get("Diag");
-		return StringTool.isNothing(diagID) ?
-				null :
-				IcpcCode.load(diagID);
+		String diagID = get("Diag");
+		return StringTool.isNothing(diagID) ? null : IcpcCode.load(diagID);
 	}
+	
 	public IcpcCode getProc(){
-		String procID=get("Proc");
-		return StringTool.isNothing(procID) ?
-				null :
-		        IcpcCode.load(procID);
+		String procID = get("Proc");
+		return StringTool.isNothing(procID) ? null : IcpcCode.load(procID);
 	}
 }
