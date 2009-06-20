397d0e82ea329bb381fa8ea545a6821a75579242
rgw_ch <devnull@localhost>
2009-06-20 15:59:45 +0200

    [svn] please use jdk1.5 compatibility

--- elexis-importer/src/ch/elexis/rs232/AuslesenDialog.java
+++ elexis-importer/src/ch/elexis/rs232/AuslesenDialog.java
@@ -13,80 +13,71 @@ import org.eclipse.swt.widgets.Shell;
 import ch.elexis.util.SWTHelper;
 
 /**
- * Dialog startet Connection und liest RS232 Input bis
- * Benutzer dialog schliesst. Wird von LogConnection verwendet.
+ * Dialog startet Connection und liest RS232 Input bis Benutzer dialog schliesst. Wird von
+ * LogConnection verwendet.
+ * 
  * @author immi
- *
+ * 
  */
 public class AuslesenDialog extends Dialog implements Connection.ComPortListener {
 	Connection conn;
 	Label label;
 	final String schnittstelle;
-
-	public AuslesenDialog(Shell parentShell, final String text) {
+	
+	public AuslesenDialog(Shell parentShell, final String text){
 		super(parentShell);
 		this.schnittstelle = text;
 	}
 	
-	public void setConnection(Connection connection) {
+	public void setConnection(Connection connection){
 		this.conn = connection;
 	}
 	
 	@Override
-	protected Control createDialogArea(Composite parent) {
-		Composite ret=(Composite)super.createDialogArea(parent);
-		label = new Label(ret,SWT.NONE);
+	protected Control createDialogArea(Composite parent){
+		Composite ret = (Composite) super.createDialogArea(parent);
+		label = new Label(ret, SWT.NONE);
 		label.setText("Daten vom Gerät werden fortlaufend ins Log geschrieben..");
 		return ret;
 	}
-
+	
 	@Override
-	protected void configureShell(Shell newShell) {
+	protected void configureShell(Shell newShell){
 		newShell.setText(schnittstelle);
 		super.configureShell(newShell);
 	}
-
+	
 	@Override
-	protected void createButtonsForButtonBar(Composite parent) {
-		createButton(parent, IDialogConstants.CANCEL_ID,
-				IDialogConstants.CANCEL_LABEL, false);
+	protected void createButtonsForButtonBar(Composite parent){
+		createButton(parent, IDialogConstants.CANCEL_ID, IDialogConstants.CANCEL_LABEL, false);
 	}
 	
-	
-
 	@Override
-	public int open() {
-		if(conn.connect()){
+	public int open(){
+		if (conn.connect()) {
 			conn.awaitFrame(1, 4, 0, 3600000);
 			return super.open();
-		} else{
+		} else {
 			String title = MessageFormat.format("{0} Schnittstelle auslesen", schnittstelle);
 			SWTHelper.showError(title, conn.getErrorMessage());
 			close();
 		}
 		return CANCEL;
 	}
-
+	
 	@Override
-	protected void cancelPressed() {
+	protected void cancelPressed(){
 		if (conn.isOpen()) {
 			conn.close();
 			conn = null;
 		}
 		super.cancelPressed();
 	}
-
-	@Override
-	public void gotBreak(Connection conn) {
-	}
-
-	@Override
-	public void gotChunk(Connection conn, String chunk) {
-	}
-
-	@Override
-	public void timeout() {
-	}
 	
+	public void gotBreak(Connection conn){}
+	
+	public void gotChunk(Connection conn, String chunk){}
+	
+	public void timeout(){}
 	
 }
