c0909192d15c82471c0fbf5a6b703e35971b8fed
rgw_ch <devnull@localhost>
2009-06-25 14:41:34 +0200

    [svn] Terminliste

---
+++ elexis-agenda/src/ch/elexis/agenda/ui/TerminListeView.java
@@ -0,0 +1,24 @@
+package ch.elexis.agenda.ui;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class TerminListeView extends ViewPart {
+
+	public TerminListeView() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
