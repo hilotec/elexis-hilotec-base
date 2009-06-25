f76a48a5a357737052de9ff7cf3ab80f45a9bc40
rgw_ch <devnull@localhost>
2009-06-25 15:20:18 +0200

    [svn] Terminliste

--- elexis-agenda/src/ch/elexis/agenda/ui/TerminListeView.java
+++ elexis-agenda/src/ch/elexis/agenda/ui/TerminListeView.java
@@ -1,5 +1,6 @@
 package ch.elexis.agenda.ui;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.forms.widgets.ScrolledForm;
@@ -9,14 +10,21 @@ import ch.elexis.Desk;
 import ch.elexis.actions.FlatDataLoader;
 import ch.elexis.actions.GlobalActions;
 import ch.elexis.actions.GlobalEvents;
+import ch.elexis.actions.GlobalEvents.ActivationListener;
+import ch.elexis.actions.GlobalEvents.SelectionListener;
 import ch.elexis.actions.PersistentObjectLoader.QueryFilter;
 import ch.elexis.agenda.data.Termin;
+import ch.elexis.data.Patient;
 import ch.elexis.data.PersistentObject;
 import ch.elexis.data.Query;
 import ch.elexis.util.SWTHelper;
 import ch.elexis.util.viewers.CommonViewer;
+import ch.elexis.util.viewers.DefaultLabelProvider;
+import ch.elexis.util.viewers.SimpleWidgetProvider;
+import ch.elexis.util.viewers.ViewerConfigurer;
+import ch.elexis.util.viewers.CommonViewer.Message;
 
-public class TerminListeView extends ViewPart {
+public class TerminListeView extends ViewPart implements ActivationListener, SelectionListener {
 	ScrolledForm form;
 	CommonViewer cv;
 	
@@ -34,8 +42,19 @@ public class TerminListeView extends ViewPart {
 		fdl.addQueryFilter(new QueryFilter(){
 
 			public void apply(Query<? extends PersistentObject> qbe) {
-				qbe.add(Termin.FLD_PATIENT, Query.EQUALS, GlobalEvents.getSelectedPatient().getId());
+				Patient p=GlobalEvents.getSelectedPatient();
+				if(p==null){
+					qbe.add(Termin.FLD_PATIENT, Query.EQUALS, "--");
+				}else{
+					qbe.add(Termin.FLD_PATIENT, Query.EQUALS, p.getId());
+				}
 			}});
+		cv=new CommonViewer();
+		ViewerConfigurer vc=new ViewerConfigurer(fdl,new DefaultLabelProvider(),
+				new SimpleWidgetProvider(SimpleWidgetProvider.TYPE_TABLE,SWT.NONE,cv));
+		GlobalEvents.getInstance().addActivationListener(this, this);
+		cv.create(vc, body, SWT.NONE, this);
+		
 	}
 
 	@Override
@@ -43,5 +62,39 @@ public class TerminListeView extends ViewPart {
 		// TODO Auto-generated method stub
 
 	}
+
+	public void activation(boolean mode) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		GlobalEvents.getInstance().removeActivationListener(this, this);
+		super.dispose();
+	}
+
+	public void visible(boolean mode) {
+		if(mode){
+			GlobalEvents.getInstance().addSelectionListener(this);
+		}else{
+			GlobalEvents.getInstance().removeSelectionListener(this);
+		}
+	}
+
+	public void clearEvent(Class<? extends PersistentObject> template) {
+		if(template.equals(Patient.class)){
+			form.setText("No Patient selected");
+		}
+	}
+
+	public void selectionEvent(PersistentObject obj) {
+		if(obj instanceof Patient){
+			form.setText(((Patient)obj).getLabel());
+			cv.notify(Message.update);
+		}
+	}
+	
+	
 
 }
