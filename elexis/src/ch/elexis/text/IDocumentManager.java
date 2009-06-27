c47a0aa8f523a383e4d0eedb1d13ae75f9e9cff6
rgw_ch <devnull@localhost>
2009-06-27 19:16:50 +0200

    [svn]

--- elexis/src/ch/elexis/text/IDocumentManager.java
+++ elexis/src/ch/elexis/text/IDocumentManager.java
@@ -2,19 +2,23 @@ package ch.elexis.text;
 
 import java.io.File;
 
+import ch.elexis.data.Patient;
+
 /**
  * A Service acting as DocumentManagement must implement this Interfache
+ * 
  * @author gerry
- *
+ * 
  */
 public interface IDocumentManager {
 	public static final String NAME = "DocumentManagement";
 
 	/** List Categories for Documents */
 	public String[] getCategories();
-	
+
 	/** Add a Categorie */
 	public boolean addCategorie(String categorie);
-	
-	public boolean addDocument(String name, String catecory, String keywords, File file);
+
+	public boolean addDocument(Patient pat, String name, String catecory,
+			String keywords, File file, String date);
 }
