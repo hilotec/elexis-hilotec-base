b1cdaca9fc2c9e2983fe6bcb9e564192dc509cc9
michael_imhof <devnull@localhost>
2009-07-02 13:36:46 +0200

    [svn] Watchdog in Background moeglich

--- elexis-connect-afinion/src/ch/elexis/connect/afinion/AfinionAS100Action.java
+++ elexis-connect-afinion/src/ch/elexis/connect/afinion/AfinionAS100Action.java
@@ -26,7 +26,7 @@ import ch.elexis.rs232.AbstractConnection.ComPortListener;
 import ch.elexis.util.SWTHelper;
 
 public class AfinionAS100Action extends Action implements ComPortListener {
-	
+
 	AfinionConnectionOld _ctrl;
 	Labor _myLab;
 	Thread msgDialogThread;
@@ -34,67 +34,63 @@ public class AfinionAS100Action extends Action implements ComPortListener {
 	Patient selectedPatient;
 	Logger _log;
 	Record lastRecord = null;
-	
-	public AfinionAS100Action(){
+	boolean background;
+
+	public AfinionAS100Action() {
 		super(Messages.getString("AfinionAS100Action.ButtonName"), AS_CHECK_BOX); //$NON-NLS-1$
 		setToolTipText(Messages.getString("AfinionAS100Action.ToolTip")); //$NON-NLS-1$
 		setImageDescriptor(AbstractUIPlugin.imageDescriptorFromPlugin("ch.elexis.connect.afinion", //$NON-NLS-1$
-			"icons/afinion.png")); //$NON-NLS-1$
+		"icons/afinion.png")); //$NON-NLS-1$
 	}
-	
-	private void initConnection(){
+
+	private void initConnection() {
 		if (_ctrl != null && _ctrl.isOpen()) {
 			_ctrl.close();
 		}
-		_ctrl =
-			new AfinionConnectionOld(Messages.getString("AfinionAS100Action.ConnectionName"), //$NON-NLS-1$
-				Hub.localCfg.get(Preferences.PORT, Messages
-					.getString("AfinionAS100Action.DefaultPort")), Hub.localCfg.get( //$NON-NLS-1$
-					Preferences.PARAMS, Messages.getString("AfinionAS100Action.DefaultParams")), //$NON-NLS-1$
-				this);
-		
-		
+		_ctrl = new AfinionConnectionOld(Messages.getString("AfinionAS100Action.ConnectionName"), //$NON-NLS-1$
+		Hub.localCfg.get(Preferences.PORT, Messages.getString("AfinionAS100Action.DefaultPort")), Hub.localCfg.get( //$NON-NLS-1$
+		Preferences.PARAMS, Messages.getString("AfinionAS100Action.DefaultParams")), //$NON-NLS-1$
+		this);
+
 		Calendar cal = new GregorianCalendar();
 		cal.add(Calendar.HOUR, -1);
 		_ctrl.setCurrentDate(cal);
-		
+
 		if (Hub.localCfg.get(Preferences.LOG, "n").equalsIgnoreCase("y")) { //$NON-NLS-1$ //$NON-NLS-2$
 			try {
 				_log = new Logger(System.getProperty("user.home") + File.separator + "elexis" //$NON-NLS-1$ //$NON-NLS-2$
-					+ File.separator + "afinion.log"); //$NON-NLS-1$
+						+ File.separator + "afinion.log"); //$NON-NLS-1$
 			} catch (FileNotFoundException e) {
 				SWTHelper.showError(Messages.getString("AfinionAS100Action.LogError.Title"), //$NON-NLS-1$
-					Messages.getString("AfinionAS100Action.LogError.Text")); //$NON-NLS-1$
+				Messages.getString("AfinionAS100Action.LogError.Text")); //$NON-NLS-1$
 				_log = new Logger();
 			}
 		} else {
 			_log = new Logger(false);
 		}
+
+		background = Hub.localCfg.get(Preferences.BACKGROUND, "n").equalsIgnoreCase("y");
 	}
-	
+
 	@Override
-	public void run(){
+	public void run() {
 		if (isChecked()) {
 			initConnection();
 			_log.logStart();
 			String msg = _ctrl.connect();
 			if (msg == null) {
-				String timeoutStr =
-					Hub.localCfg.get(Preferences.TIMEOUT, Messages
-						.getString("AfinionAS100Action.DefaultTimeout")); //$NON-NLS-1$
+				String timeoutStr = Hub.localCfg.get(Preferences.TIMEOUT, Messages.getString("AfinionAS100Action.DefaultTimeout")); //$NON-NLS-1$
 				int timeout = 20;
 				try {
 					timeout = Integer.parseInt(timeoutStr);
 				} catch (NumberFormatException e) {
 					// Do nothing. Use default value
 				}
-				_ctrl.awaitFrame(Desk.getTopShell(), Messages
-					.getString("AfinionAS100Action.WaitMsg"), 1, 4, 0, timeout); //$NON-NLS-1$
+				_ctrl.awaitFrame(Desk.getTopShell(), Messages.getString("AfinionAS100Action.WaitMsg"), 1, 4, 0, timeout, background); //$NON-NLS-1$
 				return;
 			} else {
 				_log.log("Error"); //$NON-NLS-1$
-				SWTHelper
-					.showError(Messages.getString("AfinionAS100Action.RS232.Error.Title"), msg); //$NON-NLS-1$
+				SWTHelper.showError(Messages.getString("AfinionAS100Action.RS232.Error.Title"), msg); //$NON-NLS-1$
 			}
 		} else {
 			if (_ctrl.isOpen()) {
@@ -105,42 +101,41 @@ public class AfinionAS100Action extends Action implements ComPortListener {
 		setChecked(false);
 		_log.logEnd();
 	}
-	
-	public void gotBreak(final AbstractConnection connection){
+
+	public void gotBreak(final AbstractConnection connection) {
 		connection.close();
 		setChecked(false);
 		_log.log("Break"); //$NON-NLS-1$
 		_log.logEnd();
 		SWTHelper.showError(Messages.getString("AfinionAS100Action.RS232.Break.Title"), Messages //$NON-NLS-1$
-			.getString("AfinionAS100Action.RS232.Break.Text")); //$NON-NLS-1$
+		.getString("AfinionAS100Action.RS232.Break.Text")); //$NON-NLS-1$
 	}
-	
+
 	/**
 	 * Liest Bytes aus einem Bytearray
 	 */
-	private byte[] subBytes(final byte[] bytes, final int pos, final int length){
+	private byte[] subBytes(final byte[] bytes, final int pos, final int length) {
 		byte[] retVal = new byte[length];
 		for (int i = 0; i < length; i++) {
 			retVal[i] = bytes[pos + i];
 		}
 		return retVal;
 	}
-	
+
 	/**
 	 * Einzelner Messwert wird verarbeitet
 	 * 
 	 * @param probe
 	 */
-	private void processRecord(final Record record){
+	private void processRecord(final Record record) {
 		Desk.getDisplay().syncExec(new Runnable() {
-			
-			public void run(){
+
+			public void run() {
 				selectedPatient = GlobalEvents.getSelectedPatient();
 				Patient probePat = null;
 				String vorname = null;
 				String name = null;
-				String patientStr =
-					Messages.getString("AfinionAS100Action.UnknownPatientHeaderString") + record.getId() + ")"; //$NON-NLS-1$ //$NON-NLS-2$
+				String patientStr = Messages.getString("AfinionAS100Action.UnknownPatientHeaderString") + record.getId() + ")"; //$NON-NLS-1$ //$NON-NLS-2$
 				
 				if (record.getId() != null) {
 					String patIdStr = record.getId();
@@ -150,7 +145,7 @@ public class AfinionAS100Action extends Action implements ComPortListener {
 					} catch (NumberFormatException e) {
 						// Do nothing
 					}
-					
+
 					// Patient-ID oder Name?
 					Query<Patient> patQuery = new Query<Patient>(Patient.class);
 					if (patId != null) {
@@ -160,39 +155,33 @@ public class AfinionAS100Action extends Action implements ComPortListener {
 						if (parts.length > 1) {
 							vorname = parts[1].trim().toUpperCase();
 							if (parts[1].length() > 1) {
-								vorname =
-									parts[1].substring(0, 1).toUpperCase() + parts[1].substring(1);
+								vorname = parts[1].substring(0, 1).toUpperCase()
+										+ parts[1].substring(1);
 							}
 							patQuery.add(Patient.FIRSTNAME, "like", vorname + "%"); //$NON-NLS-1$ //$NON-NLS-2$
 						}
 						if (parts.length > 0) {
 							name = parts[0].trim().toUpperCase();
 							if (parts[0].length() > 1) {
-								name =
-									parts[0].substring(0, 1).toUpperCase() + parts[0].substring(1);
+								name = parts[0].substring(0, 1).toUpperCase()
+										+ parts[0].substring(1);
 							}
 							patQuery.add(Patient.NAME, "like", name + "%"); //$NON-NLS-1$ //$NON-NLS-2$
 						}
 					}
 					List<Patient> patientList = patQuery.execute();
-					
+
 					if (patientList.size() == 1) {
 						probePat = patientList.get(0);
-						patientStr =
-							Messages.getString("AfinionAS100Action.PatientHeaderString") + probePat.getName() + ", " + probePat.getVorname() + " (" //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+						patientStr = Messages.getString("AfinionAS100Action.PatientHeaderString") + probePat.getName() + ", " + probePat.getVorname() + " (" //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 								+ record.getId() + ")"; //$NON-NLS-1$
 					}
 				}
-				
-				String text =
-					MessageFormat
-						.format(
-							Messages.getString("AfinionAS100Action.ValueInfoMsg"), patientStr, record.getRunNr(), record //$NON-NLS-1$
-								.getText());
-				
-				boolean ok =
-					MessageDialog.openConfirm(Desk.getTopShell(), Messages
-						.getString("AfinionAS100Action.DeviceName"), text); //$NON-NLS-1$
+
+				String text = MessageFormat.format(Messages.getString("AfinionAS100Action.ValueInfoMsg"), patientStr, record.getRunNr(), record //$NON-NLS-1$
+				.getText());
+
+				boolean ok = MessageDialog.openConfirm(Desk.getTopShell(), Messages.getString("AfinionAS100Action.DeviceName"), text); //$NON-NLS-1$
 				if (ok) {
 					boolean showSelectionDialog = false;
 					if (probePat != null) {
@@ -200,24 +189,21 @@ public class AfinionAS100Action extends Action implements ComPortListener {
 					} else {
 						showSelectionDialog = true;
 					}
-					
+
 					if (showSelectionDialog) {
 						Desk.getDisplay().syncExec(new Runnable() {
-							public void run(){
+							public void run() {
 								// TODO: Filter vorname/name in KontaktSelektor einbauen
-								KontaktSelektor ksl =
-									new KontaktSelektor(Hub.getActiveShell(), Patient.class,
-										Messages.getString("AfinionAS100Action.Patient.Title"), //$NON-NLS-1$
-										Messages.getString("AfinionAS100Action.Patient.Text")); //$NON-NLS-1$
+								KontaktSelektor ksl = new KontaktSelektor(Hub.getActiveShell(), Patient.class, Messages.getString("AfinionAS100Action.Patient.Title"), //$NON-NLS-1$
+								Messages.getString("AfinionAS100Action.Patient.Text")); //$NON-NLS-1$
 								ksl.create();
-								ksl.getShell().setText(
-									Messages.getString("AfinionAS100Action.Patient.Title")); //$NON-NLS-1$
+								ksl.getShell().setText(Messages.getString("AfinionAS100Action.Patient.Title")); //$NON-NLS-1$
 								if (ksl.open() == org.eclipse.jface.dialogs.Dialog.OK) {
 									selectedPatient = (Patient) ksl.getSelection();
 								} else {
 									selectedPatient = null;
 								}
-								
+
 							}
 						});
 					}
@@ -225,14 +211,12 @@ public class AfinionAS100Action extends Action implements ComPortListener {
 						try {
 							record.write(selectedPatient);
 						} catch (PackageException e) {
-							SWTHelper.showError(Messages
-								.getString("AfinionAS100Action.ProbeError.Title"), e //$NON-NLS-1$
-								.getMessage());
+							SWTHelper.showError(Messages.getString("AfinionAS100Action.ProbeError.Title"), e //$NON-NLS-1$
+							.getMessage());
 						}
 					} else {
-						SWTHelper.showError(Messages
-							.getString("AfinionAS100Action.Patient.Title"), //$NON-NLS-1$
-							Messages.getString("AfinionAS100Action.NoPatientSelectedMsg")); //$NON-NLS-1$
+						SWTHelper.showError(Messages.getString("AfinionAS100Action.Patient.Title"), //$NON-NLS-1$
+						Messages.getString("AfinionAS100Action.NoPatientSelectedMsg")); //$NON-NLS-1$
 					}
 					_log.log("Saved"); //$NON-NLS-1$
 					GlobalEvents.getInstance().fireUpdateEvent(LabItem.class);
@@ -240,11 +224,11 @@ public class AfinionAS100Action extends Action implements ComPortListener {
 			}
 		});
 	}
-	
+
 	/**
 	 * Messagedaten von Afinion wurden gelesen
 	 */
-	public void gotData(final AbstractConnection connection, final byte[] data){
+	public void gotData(final AbstractConnection connection, final byte[] data) {
 		_log.logRaw("--> \"");
 		_log.logRaw("<DLE>");
 		_log.logRaw("<STX>");
@@ -255,7 +239,7 @@ public class AfinionAS100Action extends Action implements ComPortListener {
 		_log.logRaw("<DLE>");
 		_log.logRaw("<ETX>");
 		_log.logRaw(" \"");
-		
+
 		// Record lesen
 		int pos = 0;
 		int i = 0;
@@ -271,46 +255,44 @@ public class AfinionAS100Action extends Action implements ComPortListener {
 			pos += 256;
 			i++;
 		}
-		
+
 		if (validRecords == 10) { // Read next 10 records
 			Calendar cal = lastRecord.getCalendar();
 			cal.add(Calendar.SECOND, 1);
 			_ctrl.setCurrentDate(cal);
 		} else {
 			_ctrl.close();
-			
+
 			if (lastRecord != null) {
 				processRecord(lastRecord);
 			} else {
-				SWTHelper
-					.showInfo(
-						Messages.getString("AfinionAS100Action.DeviceName"), Messages.getString("AfinionAS100Action.NoValuesMsg")); //$NON-NLS-2$
+				SWTHelper.showInfo(Messages.getString("AfinionAS100Action.DeviceName"), Messages.getString("AfinionAS100Action.NoValuesMsg")); //$NON-NLS-2$
 			}
-			
+
 			_log.log("Saved"); //$NON-NLS-1$
 			GlobalEvents.getInstance().fireUpdateEvent(LabItem.class);
 		}
 	}
-	
-	public void closed(){
+
+	public void closed() {
 		_ctrl.close();
 		_log.log("Closed"); //$NON-NLS-1$
 		setChecked(false);
 		_log.logEnd();
 	}
-	
-	public void cancelled(){
+
+	public void cancelled() {
 		_ctrl.close();
 		_log.log("Cancelled"); //$NON-NLS-1$
 		setChecked(false);
 		_log.logEnd();
 	}
-	
-	public void timeout(){
+
+	public void timeout() {
 		_ctrl.close();
 		_log.log("Timeout"); //$NON-NLS-1$
 		SWTHelper.showError(Messages.getString("AfinionAS100Action.RS232.Timeout.Title"), Messages //$NON-NLS-1$
-			.getString("AfinionAS100Action.RS232.Timeout.Text")); //$NON-NLS-1$
+		.getString("AfinionAS100Action.RS232.Timeout.Text")); //$NON-NLS-1$
 		setChecked(false);
 		_log.logEnd();
 	}
