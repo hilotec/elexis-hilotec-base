d9a320c53d5b3382436db5d72dc3925cebdb0e9f
rgw_ch <devnull@localhost>
2009-09-15 18:12:20 +0200

    [svn]

--- hilotec-messwerte/src/com/hilotec/elexis/messwerte/data/MesswertTypCalc.java
+++ hilotec-messwerte/src/com/hilotec/elexis/messwerte/data/MesswertTypCalc.java
@@ -6,9 +6,9 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    A. Kaufmann - initial implementation 
- *    
- * $Id: MesswertTypCalc.java 5640 2009-08-18 08:28:08Z rgw_ch $
+ *    A. Kaufmann - initial implementation
+ * 
+ * $Id: MesswertTypCalc.java 5731 2009-09-15 16:12:20Z rgw_ch $
  *******************************************************************************/
 
 package com.hilotec.elexis.messwerte.data;
@@ -17,12 +17,11 @@ import java.util.ArrayList;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Widget;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.Widget;
 
 import bsh.EvalError;
 import bsh.Interpreter;
-
 import ch.elexis.util.SWTHelper;
 import ch.rgw.tools.Log;
 
@@ -30,22 +29,22 @@ import ch.rgw.tools.Log;
  * @author Antoine Kaufmann
  */
 public class MesswertTypCalc extends MesswertBase implements IMesswertTyp {
-	private Log log = Log.get("Messwerte");
-
+	private final Log log = Log.get("Messwerte");
+	
 	/**
 	 * Eigentlicher Code der Formel
 	 */
 	private String formula;
 	
 	/**
-	 * Interpreter, der benutzt werden soll, um die 
+	 * Interpreter, der benutzt werden soll, um die
 	 */
 	private String interpreter;
-
+	
 	/**
 	 * Liste mit den Variablen die fuer die Formel gesetzt werden sollen
 	 */
-	private ArrayList<CalcVar> variables = new ArrayList<CalcVar>(); 
+	private final ArrayList<CalcVar> variables = new ArrayList<CalcVar>();
 	
 	
 	public MesswertTypCalc(String n, String t, String u) {
@@ -64,14 +63,14 @@ public class MesswertTypCalc extends MesswertBase implements IMesswertTyp {
 	 */
 	private void interpreterSetzeKontext(Interpreter interpreter,
 		Messung messung) throws EvalError
-	{
+		{
 		for (CalcVar cv: variables) {
 			Object wert = holeVariable(messung, cv.getName(), cv.getSource());
 			if (wert != null) {
 				interpreter.set(cv.getName(), wert);
 			}
 		}
-	}
+		}
 	
 	/**
 	 * Wert einer Variable fuer die Formel bestimmen
@@ -110,7 +109,7 @@ public class MesswertTypCalc extends MesswertBase implements IMesswertTyp {
 				return Integer.parseInt(messwert.getWert());
 			} else if (typ instanceof MesswertTypData) {
 				log.log("Fehler beim Auswerten einer Variable(" + name +"): " +
-						"wertet auf ein Data-Feld aus.", Log.ERRORS);
+					"wertet auf ein Data-Feld aus.", Log.ERRORS);
 				return null;
 			}
 		}
@@ -136,12 +135,12 @@ public class MesswertTypCalc extends MesswertBase implements IMesswertTyp {
 		/**
 		 * Name der Variable
 		 */
-		private String name;
+		private final String name;
 		
 		/**
-		 * Quelle der Variable(meist Feldname in der Messung) 
+		 * Quelle der Variable(meist Feldname in der Messung)
 		 */
-		private String source;
+		private final String source;
 		
 		CalcVar(String n, String s) {
 			name = n;
@@ -160,7 +159,7 @@ public class MesswertTypCalc extends MesswertBase implements IMesswertTyp {
 	/**
 	 * Neue Variable hinzufuegen
 	 * 
-	 * @param name   Name der Variable  
+	 * @param name   Name der Variable
 	 * @param source Quelle fuer den Variableninhalt
 	 */
 	public void addVariable(String name, String source) {
@@ -177,7 +176,7 @@ public class MesswertTypCalc extends MesswertBase implements IMesswertTyp {
 		formula = f;
 		interpreter = i;
 	}
-
+	
 	public String erstelleDarstellungswert(Messwert messwert) {
 		if (!interpreter.equals("beanshell")) {
 			log.log("Unbekannter Interpreter: " + interpreter, Log.ERRORS);
@@ -185,11 +184,11 @@ public class MesswertTypCalc extends MesswertBase implements IMesswertTyp {
 		}
 		
 		Interpreter interpreter = new Interpreter();
-
+		
 		try {
 			interpreterSetzeKontext(interpreter, messwert.getMessung());
 			Object wert = interpreter.eval(formula);
-			return ((Double) wert).toString();
+			return wert.toString();
 		} catch (EvalError e) {
 			e.printStackTrace();
 			log.log("Fehler beim Berechnen eines Wertes: " + e.getMessage(), Log.ERRORS);
@@ -198,11 +197,11 @@ public class MesswertTypCalc extends MesswertBase implements IMesswertTyp {
 		}
 		return "";
 	}
-
+	
 	public String getDefault() {
 		return "";
 	}
-
+	
 	public void setDefault(String str) {
 	}
 	
