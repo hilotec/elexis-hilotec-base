f43a92c4b59945b589fd925fa2c2bceff55c439a
rgw_ch <devnull@localhost>
2009-09-28 17:03:31 +0200

    [svn]

--- elexis-utilities/src/ch/rgw/crypt/SAT.java
+++ elexis-utilities/src/ch/rgw/crypt/SAT.java
@@ -7,7 +7,7 @@
  *
  * Contributors:
  *    G. Weirich - initial implementation
- *    
+ * 
  * $Id: SAT.java 4317 2008-08-27 05:19:24Z rgw_ch $
  *******************************************************************************/
 
@@ -46,7 +46,7 @@ public class SAT {
 	public static final String KEY_RESULT="return";
 	/** A key to store error conditions */
 	public static final String KEY_ERROR="error";
-
+	
 	public static final String USER_UNKNOWN = "User unknown";
 	public static final String RESULT_BAD_SIGNATURE = "Bad signature";
 	public static final String ADM_TIMESTAMP = "ADM_timestamp";
@@ -60,7 +60,7 @@ public class SAT {
 	private String prov="elexis.ch";
 	
 	Cryptologist crypt;
-
+	
 	/**
 	 * Create a new SAT actor
 	 * 
@@ -70,7 +70,7 @@ public class SAT {
 	public SAT(Cryptologist c) {
 		crypt = c;
 	}
-
+	
 	public SAT(String creator, String provider, Cryptologist c){
 		this(c);
 		ident=creator;
@@ -87,16 +87,16 @@ public class SAT {
 	public Map<String,Serializable> unwrap(byte[] encrypted, boolean bCheckSignature) throws CryptologistException {
 		if (encrypted == null) {
 			throw new CryptologistException("Null packet from server",
-					CryptologistException.ERR_BAD_PARAMETER);
+				CryptologistException.ERR_BAD_PARAMETER);
 		}
-		if (encrypted.length < 25) { // is probably an error message
+		if (encrypted.length < 35) { // is probably an error message
 			throw new CryptologistException(new String(encrypted),
-					CryptologistException.ERR_SHORT_BLOCK);
+				CryptologistException.ERR_SHORT_BLOCK);
 		}
 		Result<byte[]> dec = crypt.decrypt(encrypted);
 		if ((dec == null) || (!dec.isOK())) {
-			throw new CryptologistException("Decryption failed",
-					CryptologistException.ERR_DECRYPTION_FAILURE);
+			throw new CryptologistException("Decryption failed: "+dec.toString(),
+				CryptologistException.ERR_DECRYPTION_FAILURE);
 		}
 		byte[] decrypted = dec.get();
 		SoapConverter sc = new SoapConverter();
@@ -107,14 +107,14 @@ public class SAT {
 			byte[] signature = (byte[]) fields.get(ADM_SIGNATURE);
 			if ((StringTool.isNothing(user)) || (signature == null)) {
 				throw new CryptologistException("Bad protocol",
-						CryptologistException.ERR_BAD_PROTOCOL);
+					CryptologistException.ERR_BAD_PROTOCOL);
 			}
 			if (ts == null || ((System.currentTimeMillis() - ts) > 300000)) {
 				throw new CryptologistException("timeout",
-						CryptologistException.ERR_TIMEOUT);
+					CryptologistException.ERR_TIMEOUT);
 			}
 			Map<String, Serializable> ret = (Map<String, Serializable>) fields
-					.get(ADM_PAYLOAD);
+			.get(ADM_PAYLOAD);
 			if(!bCheckSignature){
 				return ret;
 			}
@@ -124,16 +124,16 @@ public class SAT {
 				return ret;
 			} else {
 				throw new CryptologistException(USER_UNKNOWN+": "+user,
-						CryptologistException.ERR_USER_UNKNOWN);
+					CryptologistException.ERR_USER_UNKNOWN);
 			}
 		} else {
 			HashMap<String, Serializable> result=new HashMap<String, Serializable>();
 			result.put(KEY_ERROR, "Invalid Message");
 			return result;
 		}
-
+		
 	}
-
+	
 	/**
 	 * Sign and encrypt a HashMap. We sign the unencrypted data and encrypt
 	 * later. This imposes more load on verifying (since it must be decrypted
@@ -152,14 +152,14 @@ public class SAT {
 	 * @throws Exception
 	 */
 	public byte[] wrap(Map<String, Serializable> var, String dest) throws CryptologistException {
-
+		
 		SoapConverter sc = new SoapConverter();
 		sc.create(ident, VERSION, prov);
 		try {
 			sc.addMap(null, ADM_PAYLOAD, var);
 		} catch (Exception ex) {
 			throw new CryptologistException("Internal Cryptologist error: "+ex.getMessage(),
-					CryptologistException.ERR_INTERNAL);
+				CryptologistException.ERR_INTERNAL);
 		}
 		sc.addIntegral(ADM_TIMESTAMP, System.currentTimeMillis());
 		sc.addString(ADM_SIGNED_BY, crypt.getUser());
@@ -170,19 +170,19 @@ public class SAT {
 		byte[] wrapped = crypt.encrypt(StringTool.getBytes(xml), dest);
 		return wrapped;
 	}
-
+	
 	@Deprecated
 	public String sendRequest(String hostaddress, String request) {
-
+		
 		try {
 			// Connect to server
 			URLConnection conn = (new URL(hostaddress)).openConnection();
-
+			
 			// Get output stream
 			conn.setDoOutput(true);
 			OutputStreamWriter out = new OutputStreamWriter(conn
-					.getOutputStream());
-
+				.getOutputStream());
+			
 			String rx = new String(Base64Coder.encodeString(request));
 			out.write("request=" + rx);
 			out.close();
@@ -193,24 +193,24 @@ public class SAT {
 				sb.append((byte) in);
 			}
 			return Base64Coder.decodeString(sb.toString()); // .decodeBuffer(conn.getInputStream());
-
+			
 		} catch (Exception ex) {
 			ExHandler.handle(ex);
 			return "";
 		}
-
+		
 	}
-
+	
 	public byte[] sendRequest(String hostaddress, byte[] request) {
 		try {
 			// Connect to server
 			URLConnection conn = (new URL(hostaddress)).openConnection();
-
+			
 			// Get output stream
 			conn.setDoOutput(true);
 			OutputStreamWriter out = new OutputStreamWriter(conn
-					.getOutputStream());
-
+				.getOutputStream());
+			
 			// String rx = new String(Base64Coder.encode(request));
 			String rx = StringTool.enPrintableStrict(request);
 			out.write("request=" + rx);
@@ -226,13 +226,13 @@ public class SAT {
 			// return Base64Coder.decode(sb.toString()); //
 			// .decodeBuffer(conn.getInputStream());
 			return baos.toByteArray();
-
+			
 		} catch (Exception ex) {
 			ExHandler.handle(ex);
 			return null;
 		}
 	}
-
+	
 	private byte[] calcDigest(SoapConverter sc) {
 		try {
 			MessageDigest digest = MessageDigest.getInstance("SHA-1");
@@ -246,7 +246,7 @@ public class SAT {
 		}
 		return null;
 	}
-
+	
 	private void addParameters(Element e, MessageDigest digest) {
 		List<Element> params = e.getChildren("parameter", SoapConverter.ns);
 		for (Element el : params) {
