1961637b816ae4f36ced3ff4f156cb16771cf670
rgw_ch <devnull@localhost>
2009-12-26 23:30:59 +0100

    [svn]

---
+++ elexis/src/ch/elexis/exchange/MetaXid.java
@@ -0,0 +1,16 @@
+package ch.elexis.exchange;
+
+import org.jdom.Element;
+
+import ch.elexis.data.Xid;
+
+public class MetaXid {
+	
+	public MetaXid(Element xidElement){
+		
+	}
+	
+	public MetaXid(Xid xidObject){
+		
+	}
+}
