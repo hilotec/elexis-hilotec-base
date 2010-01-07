2d348f117fbf73b377dcb0a87d6d028c6600f326
rgw_ch <devnull@localhost>
2010-01-07 11:54:01 +0100

    [svn] Fehlendes feld lastupdate eingebaut

--- elexis-agenda/src/ch/elexis/agenda/data/TagesNachricht.java
+++ elexis-agenda/src/ch/elexis/agenda/data/TagesNachricht.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007-2009, G. Weirich and Elexis
+ * Copyright (c) 2007-2010, G. Weirich and Elexis
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,8 +7,8 @@
  *
  * Contributors:
  *    G. Weirich - initial implementation
- *    
- *  $Id: TagesNachricht.java 5189 2009-02-24 15:48:00Z rgw_ch $
+ * 
+ *  $Id: TagesNachricht.java 5922 2010-01-07 10:54:01Z rgw_ch $
  *******************************************************************************/
 package ch.elexis.agenda.data;
 
@@ -21,15 +21,18 @@ import ch.rgw.tools.VersionInfo;
 
 public class TagesNachricht extends PersistentObject {
 	private static final String TABLENAME = "CH_ELEXIS_AGENDA_DAYMSG";
-	private static final String VERSION = "0.2.0";
+	private static final String VERSION = "0.3.0";
 	private static final String createDB =
 		"CREATE TABLE " + TABLENAME + "(" + "ID		CHAR(8) primary key,"
-			+ "deleted	CHAR(1) default '0'," + "Kurz		VARCHAR(80)," + "Msg		TEXT" + ");"
-			+ "INSERT INTO " + TABLENAME + "(ID,Kurz) VALUES (1,'" + VERSION + "')";
+		+ "deleted	CHAR(1) default '0'," + "Kurz		VARCHAR(80)," + "Msg		TEXT" + ");"
+		+ "INSERT INTO " + TABLENAME + "(ID,Kurz) VALUES (1,'" + VERSION + "')";
 	
 	private static final String update020 =
 		"ALTER TABLE " + TABLENAME + " ADD deleted CHAR(1) default '0';" + "UPDATE " + TABLENAME
-			+ " SET KURZ='0.2.0' WHERE ID='1';";
+		+ " SET Kurz='0.2.0' WHERE ID='1';";
+	private static final String update030=
+		"ALTER TABLE "+TABLENAME+ " ADD lastupdate BIGINT default 0;"
+		+ "UPDATE "+TABLENAME+" SET Kurz='0.3.0' WHERE ID='1';";
 	static {
 		addMapping(TABLENAME, "Zeile=Kurz", "Text=Msg");
 		TagesNachricht start = load("1");
@@ -45,6 +48,8 @@ public class TagesNachricht extends PersistentObject {
 			if (vi.isOlder(VERSION)) {
 				if (vi.isOlder("0.2.0")) {
 					createOrModifyTable(update020);
+				}else if(vi.isOlder("0.3.0")){
+					createOrModifyTable(update030);
 				}
 			}
 			
