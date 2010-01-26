5d9bda39dc15aea8c20a229bb3a116bf76e3c962
rgw_ch <devnull@localhost>
2010-01-26 18:31:21 +0100

    [svn] Block importer

--- elexis/src/ch/elexis/exchange/elements/ServiceBlockElement.java
+++ elexis/src/ch/elexis/exchange/elements/ServiceBlockElement.java
@@ -8,12 +8,17 @@
  * Contributors:
  *    G. Weirich - initial implementation
  * 
- *  $Id: ServiceBlockElement.java 5880 2009-12-19 19:25:22Z rgw_ch $
+ *  $Id: ServiceBlockElement.java 5969 2010-01-26 17:31:21Z rgw_ch $
  *******************************************************************************/
 
 package ch.elexis.exchange.elements;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
 
 import ch.elexis.Hub;
 import ch.elexis.data.Eigenleistung;
@@ -21,13 +26,49 @@ import ch.elexis.data.ICodeElement;
 import ch.elexis.data.IVerrechenbar;
 import ch.elexis.data.Leistungsblock;
 import ch.elexis.data.PersistentObject;
+import ch.elexis.data.PersistentObjectFactory;
 import ch.elexis.exchange.XChangeExporter;
+import ch.elexis.util.Extensions;
+import ch.elexis.util.SWTHelper;
+import ch.elexis.views.codesystems.CodeSelectorFactory;
+import ch.rgw.tools.ExHandler;
 import ch.rgw.tools.StringTool;
 
 public class ServiceBlockElement extends XChangeElement {
 	public static final String XMLNAME = "serviceblock";
 	public static final String ENCLOSING = "serviceblocks";
 	public static final String ATTR_NAME = "name";
+	static java.util.List<IConfigurationElement> codesystems;
+	
+	static List<ICodeElement> codeElements;
+	static HashMap<ICodeElement, CodeSelectorFactory> factories;
+	
+	static {
+		codesystems = Extensions.getExtensions("ch.elexis.Verrechnungscode");
+		codeElements = new ArrayList<ICodeElement>(codesystems.size());
+		factories = new HashMap<ICodeElement, CodeSelectorFactory>(codesystems.size());
+		for (IConfigurationElement ic : codesystems) {
+			try {
+				PersistentObjectFactory po =
+					(PersistentObjectFactory) ic.createExecutableExtension("ElementFactory");
+				CodeSelectorFactory cs =
+					(CodeSelectorFactory) ic.createExecutableExtension("CodeSelectorFactory");
+				if (cs == null) {
+					SWTHelper.alert("Fehler", "CodeSelectorFactory is null");
+				}
+				ICodeElement ics = (ICodeElement) po.createTemplate(cs.getElementClass());
+				if (ics == null) {
+					SWTHelper.alert("Fehler", "CodeElement is null");
+				}
+				codeElements.add(ics);
+				factories.put(ics, cs);
+				
+			} catch (CoreException ex) {
+				ExHandler.handle(ex);
+			}
+		}
+		
+	}
 	
 	public ServiceBlockElement asExporter(XChangeExporter p, Leistungsblock lb){
 		asExporter(p);
@@ -61,9 +102,45 @@ public class ServiceBlockElement extends XChangeElement {
 					}
 				}
 				if (!bFound) {
-					ret.addElement(new Eigenleistung(se.getAttr(ServiceElement.ATTR_CONTRACT_CODE),
-						se.getAttr(ServiceElement.ATTR_NAME), se.getAttr(ServiceElement.ATTR_COST),
-						se.getAttr(ServiceElement.ATTR_PRICE)));
+					if (!bFound) { // we di not have a object with matching XID
+						String contract = se.getAttr("contractName");
+						String code = se.getAttr("contractCode");
+						String lname = se.getAttr("name");
+						boolean bMatched = false;
+						for (ICodeElement ice : codeElements) {
+							if (ice.getCodeSystemName().equals(contract)) {
+								CodeSelectorFactory cof = factories.get(ice);
+								if (cof != null) {
+									PersistentObject po = cof.findElement(code);
+									if (po != null && po.exists()) {
+										bMatched = true;
+										ret.addElement((ICodeElement) po);
+										break;
+									} else {
+										Eigenleistung custom = Eigenleistung.load(code);
+										if (custom.exists()) {
+											ret.addElement(custom);
+											bMatched = true;
+											break;
+										}
+									}
+								}
+							}
+						}
+						if (!bMatched) {
+							Eigenleistung custom =
+								new Eigenleistung(code, lname, se.getAttr("cost"), se
+									.getAttr("price"));
+							ret.addElement(custom);
+						}
+					}
+					
+					/*
+					 * ret.addElement(new
+					 * Eigenleistung(se.getAttr(ServiceElement.ATTR_CONTRACT_CODE),
+					 * se.getAttr(ServiceElement.ATTR_NAME), se.getAttr(ServiceElement.ATTR_COST),
+					 * se.getAttr(ServiceElement.ATTR_PRICE)));
+					 */
 				}
 			}
 		}
