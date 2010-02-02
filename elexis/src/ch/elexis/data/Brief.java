0934ad12c44827295124d121923dc14d57ef1595
rgw_ch <devnull@localhost>
2010-02-02 18:33:46 +0100

    [svn] output logging vorbereitet

--- elexis/src/ch/elexis/data/Brief.java
+++ elexis/src/ch/elexis/data/Brief.java
@@ -8,7 +8,7 @@
  * Contributors:
  *    G. Weirich - initial implementation
  * 
- *  $Id: Brief.java 6051 2010-02-02 17:25:48Z rgw_ch $
+ *  $Id: Brief.java 6052 2010-02-02 17:33:46Z rgw_ch $
  *******************************************************************************/
 package ch.elexis.data;
 
@@ -189,8 +189,8 @@ public class Brief extends PersistentObject {
 		return super.delete();
 	}
 	
-	public BriefAusgabe logOutput(IOutputter outputter){
-		return new BriefAusgabe(this,outputter);
+	public OutputLog logOutput(IOutputter outputter){
+		return new OutputLog(this,outputter);
 	}
 	/** Einen Brief unwiederruflich löschen */
 	public boolean remove(){
--- elexis/src/ch/elexis/data/OutputLog.java
+++ elexis/src/ch/elexis/data/OutputLog.java
@@ -8,24 +8,26 @@
  * Contributors:
  *    G. Weirich - initial implementation
  * 
- *  $Id: BriefAusgabe.java 6051 2010-02-02 17:25:48Z rgw_ch $
+ *  $Id: OutputLog.java 6052 2010-02-02 17:33:46Z rgw_ch $
  *******************************************************************************/
 package ch.elexis.data;
 
 import ch.rgw.tools.TimeTool;
 
-public class BriefAusgabe extends PersistentObject {
-	public static final String FLD_BRIEF_ID = "BriefID";
+
+
+public class OutputLog extends PersistentObject{
+	public static final String FLD_OBJECT_ID = "BriefID";
 	public static final String FLD_OUTPUTTER = "Outputter";
-	static final String TABLENAME="BRIEFE_AUSGABE_LOG";
+	static final String TABLENAME="OUTPUT_LOG";
 	
 	static{
-		addMapping(TABLENAME, FLD_BRIEF_ID, FLD_OUTPUTTER, DATE_FIELD, FLD_EXTINFO);
+		addMapping(TABLENAME, FLD_OBJECT_ID, FLD_OUTPUTTER, DATE_FIELD, FLD_EXTINFO);
 	}
 	
-	public BriefAusgabe(Brief brief,IOutputter op){
+	public OutputLog(PersistentObject po,IOutputter io){
 		create(null);
-		set(new String[]{FLD_BRIEF_ID,DATE_FIELD,FLD_OUTPUTTER},brief.getId(),new TimeTool().toString(TimeTool.DATE_GER),op.getOutputterID());
+		set(new String[]{FLD_OBJECT_ID,DATE_FIELD,FLD_OUTPUTTER},po.getId(),new TimeTool().toString(TimeTool.DATE_GER),io.getOutputterID());
 	}
 	@Override
 	public String getLabel(){
@@ -37,10 +39,10 @@ public class BriefAusgabe extends PersistentObject {
 		return TABLENAME;
 	}
 	
-	public static BriefAusgabe load(String id){
-		return new BriefAusgabe(id);
+	public static OutputLog load(String id){
+		return new OutputLog(id);
 	}
-	protected BriefAusgabe(String id){
+	protected OutputLog(String id){
 		super(id);
 	}
 }
