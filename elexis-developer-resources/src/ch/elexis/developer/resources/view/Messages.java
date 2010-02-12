c5244113702c8fad72eb4313f010ce14093ff493
rgw_ch <devnull@localhost>
2010-02-12 10:10:41 +0100

    [svn] il18n

--- elexis-developer-resources/src/ch/elexis/developer/resources/view/Messages.java
+++ elexis-developer-resources/src/ch/elexis/developer/resources/view/Messages.java
@@ -6,6 +6,8 @@ public class Messages extends NLS {
 	private static final String BUNDLE_NAME = "ch.elexis.developer.resources.view.messages"; //$NON-NLS-1$
 	public static String SampleView_deleteItem;
 	public static String SampleView_newSampleDataType;
+	public static String SampleView_OnlyCreateObjectsIfPatIsSelected;
+	public static String SampleView_PleaseSelectPatient;
 	public static String SampleView_SDTCreated;
 	static {
 		// initialize resource bundle
