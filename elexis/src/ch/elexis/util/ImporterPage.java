6b22bba64085a6378bf6edbaf4751ccd45019c6a
rgw_ch <devnull@localhost>
2010-03-01 19:30:24 +0100

    [svn]

--- elexis/src/ch/elexis/util/ImporterPage.java
+++ elexis/src/ch/elexis/util/ImporterPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006-2009, G. Weirich and Elexis
+ * Copyright (c) 2006-2010, G. Weirich and Elexis
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -9,7 +9,7 @@
  *    G. Weirich - initial implementation
  *    D. Lutz	 - DBBased Importer
  *    
- * $Id: ImporterPage.java 5171 2009-02-21 19:51:16Z rgw_ch $
+ * $Id: ImporterPage.java 6182 2010-03-01 18:30:24Z rgw_ch $
  *******************************************************************************/
 
 package ch.elexis.util;
