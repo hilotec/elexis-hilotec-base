8e3dd7858fe5e39232f5a52b42e9eb9b9d5883b6
rgw_ch <devnull@localhost>
2010-03-16 20:46:31 +0100

    [svn]

--- elexis/src/ch/elexis/util/IKonsExtension.java
+++ elexis/src/ch/elexis/util/IKonsExtension.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005-2009, G. Weirich and Elexis
+ * Copyright (c) 2005-2010, G. Weirich and Elexis
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,7 +8,7 @@
  * Contributors:
  *    G. Weirich - initial implementation
  *    
- * $Id: IKonsExtension.java 6194 2010-03-14 12:13:27Z rgw_ch $
+ * $Id: IKonsExtension.java 6211 2010-03-16 19:46:31Z rgw_ch $
  *******************************************************************************/
 
 package ch.elexis.util;
@@ -17,28 +17,31 @@ import org.eclipse.core.runtime.IExecutableExtension;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.swt.custom.StyleRange;
 
-import ch.elexis.text.EnhancedTextField;
+import ch.elexis.exchange.text.IRange;
 import ch.elexis.text.IRichTextDisplay;
 
 /**
- * Erweiterung für Konsultationseinträge. Wird vom Extensionpoint KonsExtensions benötigt Eine
- * KonsExtension kann Textstellen umformatieren, kann Hyperlinks einfügen und kann Einträge für das
- * Popup-Menu der Konsultationsanzeige definieren. Die IKonsExtension wird zunächst beim Analysieren
- * der KonsExtension mit connect() initialisiert. Dann wird sie beim Rendern des Texts im
- * KonsDetailView für jedes von ihr deklarierte xref-tag einmal aufgerufen (doLayout). Sie kann da
- * "true" zurückgeben um anzuzeigen, dass sie auf Mausklicks reagieren will, oder false, wenn es nur
- * um Layout ohne Klickaktivität geht. Falls sie auf doLayout "true" zurückgegeben hat, wird sie
- * immer dann via doXref aufgerufe, wenn der Benutzer den von ihr gesetzten Link anklickt.
- * Schliesslich wird die IKonsExtension immer dann aufgerufen, wenn der Anwender das Kontext- menu
- * des Textfelds anzeigen will (rechte Maustaste). Wenn getAction eine IAction zurückliefert, dann
- * wird diese ins Kontextmenu eingebunden. Wenn getAction null zurückliefert, erfolgt keine
+ * Erweiterung für Konsultationseinträge. Wird vom Extensionpoint KonsExtensions
+ * benötigt Eine KonsExtension kann Textstellen umformatieren, kann Hyperlinks
+ * einfügen und kann Einträge für das Popup-Menu der Konsultationsanzeige
+ * definieren. Die IKonsExtension wird zunächst beim Analysieren der
+ * KonsExtension mit connect() initialisiert. Dann wird sie beim Rendern des
+ * Texts im KonsDetailView für jedes von ihr deklarierte xref-tag einmal
+ * aufgerufen (doLayout). Sie kann da "true" zurückgeben um anzuzeigen, dass sie
+ * auf Mausklicks reagieren will, oder false, wenn es nur um Layout ohne
+ * Klickaktivität geht. Falls sie auf doLayout "true" zurückgegeben hat, wird
+ * sie immer dann via doXref aufgerufe, wenn der Benutzer den von ihr gesetzten
+ * Link anklickt. Schliesslich wird die IKonsExtension immer dann aufgerufen,
+ * wenn der Anwender das Kontext- menu des Textfelds anzeigen will (rechte
+ * Maustaste). Wenn getAction eine IAction zurückliefert, dann wird diese ins
+ * Kontextmenu eingebunden. Wenn getAction null zurückliefert, erfolgt keine
  * Veränderung des Kontxtmenüs. Referenzimplementation: ch.elexis.privatnotizen
  * 
  * @author gerry
  * 
  */
 public interface IKonsExtension extends IExecutableExtension {
-	
+
 	/**
 	 * diese KonsExtension mit einem EnhancedTextField verknüpfen
 	 * 
@@ -46,21 +49,25 @@ public interface IKonsExtension extends IExecutableExtension {
 	 *            das TextField, an das diese Extension gebunden wird
 	 * @return einen Namen, der diese Extension eindeutig identifiziert
 	 */
-	public String connect(IRichTextDisplay tf);
-	
+	public String connect(final IRichTextDisplay tf);
+
 	/**
 	 * Einen Querverweis für die Darstellung layouten
 	 * 
-	 * @param n
-	 *            eine StyleRange zum beliebig bearbeiten
+	 * @param styleRange
+	 *            eine StyleRange zum beliebig bearbeiten. Kann NULL sein.
 	 * @param provider
-	 *            den Provider-String, den diese IKonsExtension dem Extension-Point angegeben hat
+	 *            den Provider-String, den diese IKonsExtension dem
+	 *            Extension-Point angegeben hat
 	 * @param id
-	 *            die ID, die die IKonsExtension dieser Textstelle zugewiesen hat
-	 * @return true wenn der Text auch als Hyperlink funktionieren soll
+	 *            die ID, die die IKonsExtension dieser Textstelle zugewiesen
+	 *            hat
+	 * @return true wenn der Text auch als Hyperlink funktionieren soll. styleRange.length muss
+	 * dann >0 sein. 
 	 */
-	public boolean doLayout(StyleRange n, String provider, String id);
-	
+	public boolean doLayout(StyleRange styleRange,  final String provider,
+			final String id);
+
 	/**
 	 * Aktion für einen Querverweis auslösen (wurde angeklickt)
 	 * 
@@ -71,7 +78,7 @@ public interface IKonsExtension extends IExecutableExtension {
 	 * @return false wenn bei der Aktion ein Fehler auftrat
 	 */
 	public boolean doXRef(String refProvider, String refID);
-	
+
 	/**
 	 * Transportable Repräsentation des eingebetteten Inhalts liefern
 	 * 
@@ -79,16 +86,17 @@ public interface IKonsExtension extends IExecutableExtension {
 	 *            Provider-String
 	 * @param refID
 	 *            ID für das betreffende Object
-	 * @return ein MimePart mit dem Objekt, oder null, wenn die Extension keine Tranpsortform hat
+	 * @return ein MimePart mit dem Objekt, oder null, wenn die Extension keine
+	 *         Tranpsortform hat
 	 */
 	// public MimePart doRender(String refProvider, String refID);
-	
+
 	// public boolean doImport(MimePart object, int pos, String title);
 	/**
 	 * Actions für diese Extension holen. z.B. für Kontextmenu
 	 */
 	public IAction[] getActions();
-	
+
 	/**
 	 * Ein Object wurde eingefügt, z.B. mit drag&drop
 	 * 
@@ -96,9 +104,10 @@ public interface IKonsExtension extends IExecutableExtension {
 	 *            eingefügtes Object
 	 */
 	public void insert(Object o, int pos);
-	
+
 	/**
-	 * Anwender hat eine XRef gelöscht -> ggf. damit verbundene Daten müssen jetzt entfernt werden
+	 * Anwender hat eine XRef gelöscht -> ggf. damit verbundene Daten müssen
+	 * jetzt entfernt werden
 	 */
 	public void removeXRef(String refProvider, String refID);
 }
