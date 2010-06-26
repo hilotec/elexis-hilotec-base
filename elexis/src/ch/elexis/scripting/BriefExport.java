97095afab5986c1e99519640978ab1bf9405a989
Gerry (pxg) <weririch@elexis.ch>
2010-06-26 16:53:13 +0200

    brief export

---
+++ elexis/src/ch/elexis/scripting/BriefExport.java
@@ -0,0 +1,76 @@
+package ch.elexis.scripting;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.util.List;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.FileDialog;
+
+import ch.elexis.Desk;
+import ch.elexis.data.Brief;
+import ch.elexis.data.Patient;
+import ch.elexis.data.Person;
+import ch.elexis.data.Query;
+import ch.elexis.util.SWTHelper;
+import ch.rgw.io.FileTool;
+import ch.rgw.tools.ExHandler;
+
+public class BriefExport {
+
+	public String doExport() {
+		FileDialog fd = new FileDialog(Desk.getTopShell(), SWT.SAVE);
+		fd.setFilterExtensions(new String[] { "*.csv" });
+		fd.setFilterNames(new String[] { "Comma Separated Values (CVS)" });
+		fd.setOverwrite(true);
+		String filename = fd.open();
+		if (filename != null) {
+			List<Brief> briefe = new Query<Brief>(Brief.class).execute();
+			File csv = new File(filename);
+			File parent = csv.getParentFile();
+			File dir = new File(parent, FileTool.getNakedFilename(filename));
+			dir.mkdirs();
+			try {
+				CSVWriter writer = new CSVWriter(new FileWriter(csv));
+				String[] header = new String[] { "Betreff", "Datum",
+						"Adressat", "Mimetype", "Typ", "Patient", "Pfad" };
+				String[] fields = new String[] { Brief.SUBJECT, Brief.DATE,
+						Brief.DESTINATION_ID, Brief.MIME_TYPE, Brief.TYPE,
+						Brief.PATIENT_ID, Brief.PATIENT_ID };
+				writer.writeNext(header);
+				for (Brief brief : briefe) {
+					Person pat = brief.getPatient();
+					if (pat != null) {
+						String subdirname = pat.get(Patient.FLD_PATID);
+						if (subdirname != null) {
+							File subdir = new File(dir, subdirname);
+							subdir.mkdirs();
+							String[] line = new String[fields.length];
+							brief.get(fields, line);
+							byte[] bin = brief.loadBinary();
+							if (bin != null) {
+								File f = new File(subdir, brief.getId()
+										+ ".odt");
+								FileOutputStream fos = new FileOutputStream(f);
+								fos.write(bin);
+								fos.close();
+								line[line.length - 1] = dir.getName()
+										+ File.separator + subdir.getName()
+										+ File.separator + f.getName();
+								writer.writeNext(line);
+							}
+						}
+					}
+				}
+				writer.close();
+				return "Export ok";
+			} catch (Exception ex) {
+				ExHandler.handle(ex);
+				SWTHelper.showError("Fehler beim Export", ex.getMessage());
+				return "Fehler beim Export";
+			}
+		}
+		return "Abgebrochen";
+	}
+}
--- elexis/src/ch/elexis/scripting/Stammdatenexport.java
+++ elexis/src/ch/elexis/scripting/Stammdatenexport.java
@@ -5,7 +5,6 @@ import java.io.FileWriter;
 import java.util.HashMap;
 import java.util.List;
 
-import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.FileDialog;
 
@@ -27,32 +26,45 @@ public class Stammdatenexport {
 		if (file != null) {
 			try {
 				FileWriter writer = new FileWriter(new File(file));
-				CSVWriter csv=new CSVWriter(writer);
-				String[] header=new String[]{"UUID","Nr","Titel","Name","Vorname","Geschlecht","Geburtsdatum",
-						"Strasse","Plz","Ort","Postanschrift","Telefon 1", "Telefon 2", "Telefon Mobil",
-						"Bemerkung"};
-				String[] fields=new String[]{"ID", Patient.FLD_PATID, "Titel",Patient.NAME,Patient.FIRSTNAME,Patient.SEX,Patient.FLD_DOB,
-						Patient.FLD_STREET,Patient.FLD_ZIP,Patient.FLD_PLACE,"Anschrift",Patient.FLD_PHONE1,"Telefon2","Natel",
-						"Bemerkung"
-				};
+				CSVWriter csv = new CSVWriter(writer);
+				String[] header = new String[] { "UUID", "Nr", "Titel", "Name",
+						"Vorname", "Geschlecht", "Geburtsdatum", "Strasse",
+						"Plz", "Ort", "Postanschrift", "Telefon 1",
+						"Telefon 2", "Telefon Mobil", "Bemerkung" };
+				String[] fields = new String[] { "ID", Patient.FLD_PATID,
+						"Titel", Patient.NAME, Patient.FIRSTNAME, Patient.SEX,
+						Patient.FLD_DOB, Patient.FLD_STREET, Patient.FLD_ZIP,
+						Patient.FLD_PLACE, "Anschrift", Patient.FLD_PHONE1,
+						"Telefon2", "Natel", "Bemerkung" };
 				csv.writeNext(header);
-				HashMap<Patient, String> patienten = new HashMap<Patient, String>();
-				Query<Konsultation> qbe = new Query<Konsultation>(
-						Konsultation.class);
-				qbe.add("Datum", ">", startDate);
-				List<Konsultation> lKons = qbe.execute();
-
-				for (Konsultation k : lKons) {
-					Fall fall = k.getFall();
-					Patient p = fall.getPatient();
-					patienten.put(p, "1");
-				}
-				for (Patient p : patienten.keySet()) {
-					String[] line=new String[header.length];
-					for(int i=0;i<header.length;i++){
-						line[i]=p.get(fields[i]);
+				if (startDate == null || startDate.length()==0) {
+					for (Patient pat : new Query<Patient>(Patient.class)
+							.execute()) {
+						String[] line = new String[header.length];
+						for (int i = 0; i < header.length; i++) {
+							line[i] = pat.get(fields[i]);
+						}
+						csv.writeNext(line);
 					}
-					csv.writeNext(line);
+				} else {
+					HashMap<Patient, String> patienten = new HashMap<Patient, String>();
+					Query<Konsultation> qbe = new Query<Konsultation>(
+							Konsultation.class);
+					qbe.add("Datum", ">", startDate);
+					List<Konsultation> lKons = qbe.execute();
+
+					for (Konsultation k : lKons) {
+						Fall fall = k.getFall();
+						Patient p = fall.getPatient();
+						patienten.put(p, "1");
+					}
+					for (Patient p : patienten.keySet()) {
+						String[] line = new String[header.length];
+						for (int i = 0; i < header.length; i++) {
+							line[i] = p.get(fields[i]);
+						}
+						csv.writeNext(line);
+					}
 				}
 				csv.close();
 				return "Der Export wurde efrolgreich abgeschlossen";
