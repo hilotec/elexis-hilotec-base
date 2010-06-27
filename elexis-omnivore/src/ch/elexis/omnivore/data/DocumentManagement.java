0c8f5fee6c549370d67875663716d743e2612377
gerry (mac) <weirich@elexis.ch>
2010-06-27 08:49:05 +0200

    added Retrieving to DocumentManager

--- elexis-omnivore/src/ch/elexis/omnivore/data/DocumentManagement.java
+++ elexis-omnivore/src/ch/elexis/omnivore/data/DocumentManagement.java
@@ -5,13 +5,19 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import ch.elexis.ElexisException;
 import ch.elexis.data.Patient;
+import ch.elexis.data.Query;
 import ch.elexis.services.IDocumentManager;
 import ch.elexis.text.IDocument;
 import ch.rgw.io.FileTool;
 import ch.rgw.tools.ExHandler;
+import ch.rgw.tools.RegexpFilter;
+import ch.rgw.tools.TimeSpan;
+import ch.rgw.tools.TimeTool;
 
 public class DocumentManagement implements IDocumentManager {
 
@@ -68,4 +74,51 @@ public class DocumentManagement implements IDocumentManager {
 		return dh.getContentsAsStream();
 	}
 
+	@Override
+	public List<IDocument> listDocuments(final Patient pat, final String categoryMatch,
+			final String titleMatch, final String keywordMatch, final TimeSpan dateMatch,
+			final String contentsMatch) throws ElexisException {
+		Query<DocHandle> qbe=new Query<DocHandle>(DocHandle.class);
+		if(pat!=null){
+			qbe.add("PatID", Query.EQUALS, pat.getId());
+		}
+		if(dateMatch!=null){
+			String from=dateMatch.from.toString(TimeTool.DATE_COMPACT);
+			String until=dateMatch.until.toString(TimeTool.DATE_COMPACT);
+			qbe.add("Datum", Query.GREATER_OR_EQUAL, from);
+			qbe.add("Datum", Query.LESS_OR_EQUAL, until);
+		}
+		if(titleMatch!=null){
+			if(titleMatch.matches("/.+/")){
+				qbe.addPostQueryFilter(new RegexpFilter(titleMatch.substring(1, titleMatch.length()-1)));
+			}else{
+				qbe.add("Titel", Query.EQUALS, titleMatch);
+			}
+		}
+		if(keywordMatch!=null){
+			if(keywordMatch.matches("/.+/")){
+				qbe.addPostQueryFilter(new RegexpFilter(keywordMatch.substring(1, keywordMatch.length()-1)));
+			}else{
+				qbe.add("Keywords", Query.LIKE, "%"+keywordMatch+"%");
+			}
+		}
+		/*
+		if(categoryMatch!=null){
+			if(categoryMatch.matches("/.+/")){
+				qbe.addPostQueryFilter(new RegexpFilter(categoryMatch.substring(1, categoryMatch.length()-1)));
+			}else{
+				qbe.add("Category", Query.EQUALS, titleMatch);
+			}
+		}
+		*/
+		if(contentsMatch!=null){
+			throw new ElexisException(getClass(), "ContentsMatch not supported", ElexisException.EE_NOT_SUPPORTED);
+		}
+		List<DocHandle> dox=qbe.execute();
+		ArrayList<IDocument> ret=new ArrayList<IDocument>(dox.size());
+		for(DocHandle doc:dox){
+			ret.add(doc);
+		}
+		return ret;
+	}
 }
