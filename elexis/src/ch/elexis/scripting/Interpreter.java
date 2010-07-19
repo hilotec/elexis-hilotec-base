9b179c607a86a8c299fc7ae6ec98c858c3240ed1
gerry (mac) <weirich@elexis.ch>
2010-07-19 21:10:53 +0200

    scripter ausgelagert

---
+++ elexis/src/ch/elexis/scripting/Interpreter.java
@@ -0,0 +1,9 @@
+package ch.elexis.scripting;
+
+import ch.elexis.ElexisException;
+
+public interface Interpreter {
+
+	public void setValue(String name, Object value) throws ElexisException;
+	public Object run(String script) throws ElexisException;
+}
