35de1e7ebc432822fd9e267cde875451124f8915
gerry (mac) <weirich@elexis.ch>
2010-07-20 07:11:26 +0200

    labitem converted to new scripting

--- elexis/src/ch/elexis/data/LabItem.java
+++ elexis/src/ch/elexis/data/LabItem.java
@@ -13,28 +13,32 @@
 
 package ch.elexis.data;
 
+import java.text.MessageFormat;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import bsh.EvalError;
-import bsh.Interpreter;
+import ch.elexis.ElexisException;
 import ch.elexis.StringConstants;
+import ch.elexis.scripting.Interpreter;
 import ch.elexis.text.TextContainer;
+import ch.elexis.util.SWTHelper;
 import ch.rgw.tools.StringTool;
 import ch.rgw.tools.TimeTool;
 
 /**
  * Ein Laboritem, also ein anzeigbarer Laborwert. Jedes Laboritem hat einen
- * Titel, ein Kürzel, ein Labor, aus dem es stammt, einen Normbereich.
- * Ausserdem gehört jedes Laboritem zu einer Itemgruppe (Beispielsweise
- * Hämatologie oder Vitamine) und hat eine Priorität innerhalb dieser Gruppe.
- * Gruppe und Priorität beeinflussen die Darstellungsreihenfolge und Gruppierung
- * auf dem Laborblatt.
+ * Titel, ein Kürzel, ein Labor, aus dem es stammt, einen Normbereich. Ausserdem
+ * gehört jedes Laboritem zu einer Itemgruppe (Beispielsweise Hämatologie oder
+ * Vitamine) und hat eine Priorität innerhalb dieser Gruppe. Gruppe und
+ * Priorität beeinflussen die Darstellungsreihenfolge und Gruppierung auf dem
+ * Laborblatt.
+ * 
  * @author Gerry
- *
+ * 
  */
-public class LabItem extends PersistentObject implements Comparable<LabItem>{
+public class LabItem extends PersistentObject implements Comparable<LabItem> {
+	public static final String SCRIPT_MARKER = "SCRIPT:";
 	public static final String REF_MALE = "RefMann";
 	public static final String REF_FEMALE_OR_TEXT = "RefFrauOrTx";
 	public static final String PRIO = "prio";
@@ -45,185 +49,250 @@ public class LabItem extends PersistentObject implements Comparable<LabItem>{
 	public static final String TITLE = "titel";
 	public static final String SHORTNAME = "kuerzel";
 	private static final String LABITEMS = "LABORITEMS";
-	private static final Pattern varPattern=Pattern.compile(TextContainer.MATCH_TEMPLATE);
+	private static final Pattern varPattern = Pattern
+			.compile(TextContainer.MATCH_TEMPLATE);
+
 	@Override
 	protected String getTableName() {
 		return LABITEMS;
 	}
-	static{
-		addMapping(LABITEMS,SHORTNAME,TITLE,LAB_ID,
-				REF_MALE,REF_FEMALE_OR_TEXT,UNIT,TYPE,GROUP,PRIO);
-	}
-	public enum typ{NUMERIC,TEXT,ABSOLUTE,FORMULA};
 
-	public LabItem(String k,String t, Kontakt labor, String RefMann, String RefFrau,
-			String Unit, typ type,String grp,String seq){
-		String tp="1";
-		if(type==typ.NUMERIC){
-			tp="0";
-		}else if(type==typ.ABSOLUTE){
-			tp="2";
+	static {
+		addMapping(LABITEMS, SHORTNAME, TITLE, LAB_ID, REF_MALE,
+				REF_FEMALE_OR_TEXT, UNIT, TYPE, GROUP, PRIO);
+	}
+
+	public enum typ {
+		NUMERIC, TEXT, ABSOLUTE, FORMULA
+	};
+
+	public LabItem(String k, String t, Kontakt labor, String RefMann,
+			String RefFrau, String Unit, typ type, String grp, String seq) {
+		String tp = "1";
+		if (type == typ.NUMERIC) {
+			tp = "0";
+		} else if (type == typ.ABSOLUTE) {
+			tp = "2";
 		}
 		create(null);
-		if(StringTool.isNothing(seq)){
-			seq=t.substring(0,1);
+		if (StringTool.isNothing(seq)) {
+			seq = t.substring(0, 1);
 		}
-		if(StringTool.isNothing(grp)){
-			grp="z Verschiedenes";
+		if (StringTool.isNothing(grp)) {
+			grp = "z Verschiedenes";
 		}
-		if(labor==null){
-			Query<Kontakt> qbe=new Query<Kontakt>(Kontakt.class);
-			String labid=qbe.findSingle(Kontakt.FLD_IS_LAB,Query.EQUALS,StringConstants.ONE);
-			if(labid==null){
-				labor=new Labor("Eigen","Eigenlabor");
-			}else{
-				labor=Labor.load(labid);
+		if (labor == null) {
+			Query<Kontakt> qbe = new Query<Kontakt>(Kontakt.class);
+			String labid = qbe.findSingle(Kontakt.FLD_IS_LAB, Query.EQUALS,
+					StringConstants.ONE);
+			if (labid == null) {
+				labor = new Labor("Eigen", "Eigenlabor");
+			} else {
+				labor = Labor.load(labid);
 			}
 		}
-		set(new String[]{SHORTNAME,TITLE,LAB_ID,REF_MALE,
-				REF_FEMALE_OR_TEXT,UNIT,TYPE,GROUP,PRIO},
-				k,t,labor.getId(),RefMann,RefFrau,Unit,tp,grp,seq);
+		set(new String[] { SHORTNAME, TITLE, LAB_ID, REF_MALE,
+				REF_FEMALE_OR_TEXT, UNIT, TYPE, GROUP, PRIO }, k, t,
+				labor.getId(), RefMann, RefFrau, Unit, tp, grp, seq);
 	}
 
-	public static LabItem load(String id){
+	public static LabItem load(String id) {
 		return new LabItem(id);
 	}
-	public String getEinheit(){
+
+	public String getEinheit() {
 		return checkNull(get(UNIT));
 	}
-	public String getGroup(){
+
+	public String getGroup() {
 		return checkNull(get(GROUP));
 	}
-	public String getPrio(){
+
+	public String getPrio() {
 		return checkNull(get(PRIO));
 	}
-	public String getKuerzel(){
+
+	public String getKuerzel() {
 		return checkNull(get(SHORTNAME));
 	}
-	public String getName(){
+
+	public String getName() {
 		return checkNull(get(TITLE));
 	}
-	public Labor getLabor(){
+
+	public Labor getLabor() {
 		return Labor.load(get(LAB_ID));
 	}
-	public typ getTyp(){
-		String t=get(TYPE);
-		if(t.equals(StringConstants.ZERO)){
+
+	public typ getTyp() {
+		String t = get(TYPE);
+		if (t.equals(StringConstants.ZERO)) {
 			return typ.NUMERIC;
-		}else if(t.equals(StringConstants.ONE)){
+		} else if (t.equals(StringConstants.ONE)) {
 			return typ.TEXT;
-		}else if(t.equals("2")){
+		} else if (t.equals("2")) {
 			return typ.ABSOLUTE;
-		}else{
+		} else {
 			return typ.FORMULA;
 		}
 
 	}
+
 	/**
-	 * Evaluate a formula-based LabItem for a given Patient at a given date.
-	 * It will try to retrieve all LabValues it depends on of that Patient and date
-	 * and then calculate the result. If there are not all necessare values given,
-	 * it will return "?formula?".
-	 * @param date The date to consider for calculating
+	 * Evaluate a formula-based LabItem for a given Patient at a given date. It
+	 * will try to retrieve all LabValues it depends on of that Patient and date
+	 * and then calculate the result. If there are not all necessare values
+	 * given, it will return "?formula?". The formula can be a beanshell-script
+	 * by itself (for compatibility with previous versions), or the name of a
+	 * script prefixed with SCRIPT:, e.g. SCRIPT:mdrd($krea=c_10). Variable
+	 * names are the group and priority values of a lab item separated with an
+	 * underscore.
+	 * 
+	 * @param date
+	 *            The date to consider for calculating
 	 * @return the result or "?formel?" if no result could be calculated.
 	 */
-	public String evaluate(Patient pat,TimeTool date){
-		if(!getTyp().equals(typ.FORMULA)){
-			return  null;
+	public String evaluate(Patient pat, TimeTool date) {
+		if (!getTyp().equals(typ.FORMULA)) {
+			return null;
 		}
-		Query<LabResult> qbe=new Query<LabResult>(LabResult.class);
+		Query<LabResult> qbe = new Query<LabResult>(LabResult.class);
 		qbe.add(LabResult.PATIENT_ID, Query.EQUALS, pat.getId());
-		qbe.add(LabResult.DATE, Query.EQUALS, date.toString(TimeTool.DATE_COMPACT));
-		List<LabResult> results=qbe.execute();
-		String formel=getFormula();
-		boolean bMatched=false;
-		for(LabResult result:results){
-			String var=result.getItem().makeVarName();
-			if(formel.indexOf(var)!=-1){
-				formel=formel.replaceAll(var, result.getResult());
-				bMatched=true;
+		qbe.add(LabResult.DATE, Query.EQUALS,
+				date.toString(TimeTool.DATE_COMPACT));
+		List<LabResult> results = qbe.execute();
+		String formel = getFormula();
+		if (!formel.startsWith(SCRIPT_MARKER)) {		// convert to new system
+			String scriptname="auto_"+getName();
+			Pattern varFinder = Pattern.compile("[a-z]+_[0-9]+",
+					Pattern.CASE_INSENSITIVE);
+			Matcher m = varFinder.matcher(formel);
+			StringBuffer sb = new StringBuffer();
+			StringBuilder params = new StringBuilder();
+			int i = 0;
+			while (m.find()) {
+				String vn = m.group();
+				String repl = "var" + i;
+				m.appendReplacement(sb, repl);
+				params.append("$").append(repl).append(vn).append(",");
+			}
+			if (params.length() > 0) {
+				params.deleteCharAt(params.length() - 1);
+				params.insert(0, "(");
+				params.append(")");
+			}
+			m.appendTail(sb);
+			String nscript = SCRIPT_MARKER + sb.toString();
+			try{
+			Script newscript=Script.create(scriptname, nscript);
+			}catch(ElexisException ex){
+				SWTHelper.showError("Fehler beim Konvertieren", MessageFormat.format("Die Formel {0} konnte nicht in ein Script konvertiert werden.",sb.toString()));
+			}
+			setFormula(nscript);
+			formel=nscript;
+		}else{
+			formel=formel.substring(SCRIPT_MARKER.length()+1);
+		}
+		boolean bMatched = false;
+		for (LabResult result : results) {
+			String var = result.getItem().makeVarName();
+			if (formel.indexOf(var) != -1) {
+				formel = formel.replaceAll(var, result.getResult());
+				bMatched = true;
 			}
 		}
-		Matcher matcher=varPattern.matcher(formel);
+		/*
+		Matcher matcher = varPattern.matcher(formel);
 		// Suche Variablen der Form [Patient.Alter]
 		StringBuffer sb = new StringBuffer();
 
-		while(matcher.find()){
-			String var=matcher.group();
-			String[] fields=var.split("\\.");
-			if(fields.length>1){
-				String repl="\""+pat.get(fields[1].replaceFirst("\\]", StringTool.leer))+"\"";
-				//formel=matcher.replaceFirst(repl);
+		while (matcher.find()) {
+			String var = matcher.group();
+			String[] fields = var.split("\\.");
+			if (fields.length > 1) {
+				String repl = "\""
+						+ pat.get(fields[1]
+								.replaceFirst("\\]", StringTool.leer)) + "\"";
+				// formel=matcher.replaceFirst(repl);
 				matcher.appendReplacement(sb, repl);
-				bMatched=true;
+				bMatched = true;
 			}
 		}
 		matcher.appendTail(sb);
-		if(!bMatched){
+		if (!bMatched) {
 			return null;
 		}
-		Interpreter scripter=new Interpreter();
-
+		*/	
+		
 		try {
-			return scripter.eval(sb.toString()).toString();
-		} catch (EvalError e) {
+			
+			return (String)Script.executeScript(formel, pat);
+		} catch (ElexisException e) {
 			return "?formel?";
 		}
 
 	}
+
 	/**
 	 * Return the variable Name that identifies this item (in a script)
+	 * 
 	 * @return a name that is made of the group and the priority values.
 	 */
-	public String makeVarName(){
-		String[] group=getGroup().split(StringTool.space,2);
-		String num=getPrio().trim();
-		return group[0]+"_"+num;
+	public String makeVarName() {
+		String[] group = getGroup().split(StringTool.space, 2);
+		String num = getPrio().trim();
+		return group[0] + "_" + num;
 	}
-	public String getRefW(){
-		String ret= checkNull(get(REF_FEMALE_OR_TEXT)).split("##")[0];
+
+	public String getRefW() {
+		String ret = checkNull(get(REF_FEMALE_OR_TEXT)).split("##")[0];
 		return ret;
 	}
-	public String getRefM(){
+
+	public String getRefM() {
 		return checkNull(get(REF_MALE));
 	}
-	public void setRefW(String r){
-		set(REF_FEMALE_OR_TEXT,r);
-	}
-	public void setRefM(String r){
-		set(REF_MALE,r);
+
+	public void setRefW(String r) {
+		set(REF_FEMALE_OR_TEXT, r);
 	}
 
-	public void setFormula(String f){
-		String val=getRefW();
-		if(!StringTool.isNothing(f)){
-			val+="##"+f;
+	public void setRefM(String r) {
+		set(REF_MALE, r);
+	}
+
+	public void setFormula(String f) {
+		String val = getRefW();
+		if (!StringTool.isNothing(f)) {
+			val += "##" + f;
 		}
-		set(REF_FEMALE_OR_TEXT,val);
+		set(REF_FEMALE_OR_TEXT, val);
 	}
-	public String getFormula(){
-		String[] all=get(REF_FEMALE_OR_TEXT).split("##");
-		return all.length>1 ? all[1] : "";
-	}
-	protected LabItem() {/* leer */}
 
+	public String getFormula() {
+		String[] all = get(REF_FEMALE_OR_TEXT).split("##");
+		return all.length > 1 ? all[1] : "";
+	}
+
+	protected LabItem() {/* leer */
+	}
 
 	protected LabItem(String id) {
 		super(id);
 	}
 
-
 	@Override
 	public String getLabel() {
-		StringBuilder sb=new StringBuilder();
-		String [] fields={SHORTNAME,TITLE,REF_MALE,REF_FEMALE_OR_TEXT,UNIT,
-				TYPE,GROUP,PRIO};
-		String[] vals=new String[fields.length];
-		get(fields,vals);
+		StringBuilder sb = new StringBuilder();
+		String[] fields = { SHORTNAME, TITLE, REF_MALE, REF_FEMALE_OR_TEXT,
+				UNIT, TYPE, GROUP, PRIO };
+		String[] vals = new String[fields.length];
+		get(fields, vals);
 		sb.append(vals[0]).append(", ").append(vals[1]);
-		if(vals[5].equals(StringConstants.ZERO)){
-			sb.append(" (").append(vals[2]).append("-").append(vals[3]).append(StringTool.space).append(vals[4]).append(")");
-		}else{
+		if (vals[5].equals(StringConstants.ZERO)) {
+			sb.append(" (").append(vals[2]).append("-").append(vals[3])
+					.append(StringTool.space).append(vals[4]).append(")");
+		} else {
 			sb.append(" (").append(vals[3]).append(")");
 		}
 		sb.append("[").append(vals[6]).append(", ").append(vals[7]).append("]");
@@ -231,18 +300,18 @@ public class LabItem extends PersistentObject implements Comparable<LabItem>{
 
 	}
 
-	public String getShortLabel(){
-		StringBuilder sb=new StringBuilder();
-		String[] fields={TITLE,UNIT,LAB_ID};
-		String[] vals=new String[fields.length];
-		get(fields,vals);
-		Labor lab=Labor.load(vals[2]);
-		String labName="Labor?";
-		if(lab!=null){
-			labName=lab.get("Bezeichnung1");
+	public String getShortLabel() {
+		StringBuilder sb = new StringBuilder();
+		String[] fields = { TITLE, UNIT, LAB_ID };
+		String[] vals = new String[fields.length];
+		get(fields, vals);
+		Labor lab = Labor.load(vals[2]);
+		String labName = "Labor?";
+		if (lab != null) {
+			labName = lab.get("Bezeichnung1");
 		}
 		sb.append(vals[0]).append(" (").append(vals[1]).append("; ")
-		.append(labName).append(")");
+				.append(labName).append(")");
 		return sb.toString();
 	}
 
@@ -261,11 +330,11 @@ public class LabItem extends PersistentObject implements Comparable<LabItem>{
 		}
 
 		// compare item priorities
-		String mine=getPrio();
-		String others=other.getPrio();
-		if((mine.matches("[0-9]+")) && (others.matches("[0-9]+"))){
-			Integer iMine=Integer.parseInt(mine);
-			Integer iOthers=Integer.parseInt(others);
+		String mine = getPrio();
+		String others = other.getPrio();
+		if ((mine.matches("[0-9]+")) && (others.matches("[0-9]+"))) {
+			Integer iMine = Integer.parseInt(mine);
+			Integer iOthers = Integer.parseInt(others);
 			return iMine.compareTo(iOthers);
 		}
 		return mine.compareTo(others);
