4cd325e02fbc5e72c31c5e7ed833b222a2996c50
gerry (mac) <weirich@elexis.ch>
2010-08-14 21:46:32 +0200

    viewport loading ok

--- elexis/src/ch/elexis/text/model/Range.java
+++ elexis/src/ch/elexis/text/model/Range.java
@@ -5,28 +5,31 @@ import org.jdom.Element;
 
 import ch.rgw.tools.StringTool;
 
-
 /**
- * A Range is some part of a document. It has a position and a length within the text
- * Optionally, it can be places outside the text flow. In that case, it must provide a
- * viewport position relative to the character indicated by position. The contents of the Range is
- * totally implementation specific. It might be some text or some graphics or both.  
+ * A Range is some part of a document. It has a position and a length within the
+ * text Optionally, it can be places outside the text flow. In that case, it
+ * must provide a viewport position relative to the character indicated by
+ * position. The contents of the Range is totally implementation specific. It
+ * might be some text or some graphics or both.
+ * 
  * @author gerry
- *
+ * 
  */
-public class Range{
-	public static final String TYPE_MARKUP="markup";
-	
-	public static final String ELEM_NAME="range";
+public class Range {
+	public static final String TYPE_MARKUP = "markup";
+
+	public static final String ELEM_NAME = "range";
 	/** It is not possible to set the cursor within this range */
-	private static final String ATTR_LOCKED="locked";
+	private static final String ATTR_LOCKED = "locked";
 	/** Identifier of this range. Used to find a matching renderer */
-	private static final String ATTR_TYPENAME="typename";
+	private static final String ATTR_TYPENAME = "typename";
 	/** ID of this range. Hint for the renderer */
-	private static final String ATTR_ID="ID";
-	/** Some range types are displayed off the text flow. This is a hint in 
-	 * wich region of the screen to display */
-	public static final String ATTR_VIEWPORT="viewport";
+	private static final String ATTR_ID = "ID";
+	/**
+	 * Some range types are displayed off the text flow. This is a hint in wich
+	 * region of the screen to display
+	 */
+	public static final String ATTR_VIEWPORT = "viewport";
 	/**
 	 * Length of the range within the text flow in characters
 	 */
@@ -36,16 +39,16 @@ public class Range{
 	 */
 	private static final String ATTR_START_OFFSET = "startOffset";
 	/**
-	 * Hint what to to if an application does not know this type of Range
-	 * one of: prefix:(prefix), append:(append), replace:replace 
+	 * Hint what to to if an application does not know this type of Range one
+	 * of: prefix:(prefix), append:(append), replace:replace
 	 */
-	private static final String ATTR_HINT="hint";
-	
-	public static final String STYLE_BOLD="bold";
-	public static final String STYLE_ITALIC="italic";
-	public static final String STYLE_UNDERLINE="underline";
-	public static final String STYLE_FOREGROUND="foreground:";
-	public static final String STYLE_BACKGROUND="backgreound:";
+	private static final String ATTR_HINT = "hint";
+
+	public static final String STYLE_BOLD = "bold";
+	public static final String STYLE_ITALIC = "italic";
+	public static final String STYLE_UNDERLINE = "underline";
+	public static final String STYLE_FOREGROUND = "foreground:";
+	public static final String STYLE_BACKGROUND = "backgreound:";
 	String id;
 	String typename;
 	int length;
@@ -54,26 +57,35 @@ public class Range{
 	boolean bLocked;
 	String hint;
 	String contents;
-	
-	public Range(Element el){
-		id=el.getAttributeValue(ATTR_ID);
-		typename=el.getAttributeValue(ATTR_TYPENAME);
+
+	public Range(Element el) {
+		id = el.getAttributeValue(ATTR_ID);
+		typename = el.getAttributeValue(ATTR_TYPENAME);
 		position = Integer.parseInt(el.getAttributeValue(ATTR_START_OFFSET));
 		length = Integer.parseInt(el.getAttributeValue(ATTR_LENGTH));
-		//length=Integer.parseInt(el.getAttributeValue(""));
-		hint=el.getAttributeValue(ATTR_HINT);
-		contents=el.getText();
+		// length=Integer.parseInt(el.getAttributeValue(""));
+		hint = el.getAttributeValue(ATTR_HINT);
+		String v = el.getAttributeValue(ATTR_VIEWPORT);
+		if (v!=null && v.matches("\\d+,\\d+,\\d+,\\d+")) {
+			String[] koor = v.split(",");
+			viewport = new Rectangle(Integer.parseInt(koor[0]),
+					Integer.parseInt(koor[1]), Integer.parseInt(koor[2]),
+					Integer.parseInt(koor[3]));
+		}
+		contents = el.getText();
 	}
-	public Range(final int start, final int len, String typename, String id){
-		length=len;
-		position=start;
-		this.typename=typename;
-		this.id=id;
+
+	public Range(final int start, final int len, String typename, String id) {
+		length = len;
+		position = start;
+		this.typename = typename;
+		this.id = id;
 	}
-	
-	public boolean isLocked(){
+
+	public boolean isLocked() {
 		return bLocked;
 	}
+
 	public int getLength() {
 		return length;
 	}
@@ -82,57 +94,64 @@ public class Range{
 		return position;
 	}
 
-	public String getType(){
+	public String getType() {
 		return typename;
 	}
-	
-	public String getID(){
+
+	public String getID() {
 		return id;
 	}
+
 	public void setLength(final int pos) {
-		length=pos;
+		length = pos;
 	}
 
 	public void setPosition(final int pos) {
-		position=pos;
+		position = pos;
 	}
+
 	public Rectangle getViewPort() {
 		return viewport;
 	}
 
-	public void setViewPort(Rectangle r){
-		this.viewport=r;
+	public void setViewPort(Rectangle r) {
+		this.viewport = r;
 	}
-	
-	public String getHint(){
+
+	public String getHint() {
 		return hint;
 	}
-	
-	public String getContents(){
+
+	public String getContents() {
 		return contents;
 	}
-	
-	public void setContents(String c){
-		contents=c;
+
+	public void setContents(String c) {
+		contents = c;
 	}
-	
-	public Element toElement(){
-		Element el=new Element(ELEM_NAME,SimpleStructuredDocument.ns);
-		el.setAttribute(ATTR_ID,id);
-		el.setAttribute(ATTR_LENGTH,Integer.toString(length));
-		el.setAttribute(ATTR_START_OFFSET,Integer.toString(position));
-		el.setAttribute(ATTR_TYPENAME,typename);
+
+	public Element toElement() {
+		Element el = new Element(ELEM_NAME, SimpleStructuredDocument.ns);
+		el.setAttribute(ATTR_ID, id);
+		el.setAttribute(ATTR_LENGTH, Integer.toString(length));
+		el.setAttribute(ATTR_START_OFFSET, Integer.toString(position));
+		el.setAttribute(ATTR_TYPENAME, typename);
 		setAttributeIfExists(el, ATTR_HINT, hint);
-		setAttributeIfExists(el,ATTR_VIEWPORT,viewport==null ? null : StringTool.RectangleToString(viewport.x, viewport.y, viewport.width, viewport.height));
-		if(contents!=null){
+		setAttributeIfExists(
+				el,
+				ATTR_VIEWPORT,
+				viewport == null ? null : StringTool
+						.RectangleToString(viewport.x, viewport.y,
+								viewport.width, viewport.height));
+		if (contents != null) {
 			el.setText(contents);
 		}
 		return el;
 	}
-	
-	private void setAttributeIfExists(Element e, String attr, String value){
-		if(attr!=null && value!=null){
-			e.setAttribute(attr,value);
+
+	private void setAttributeIfExists(Element e, String attr, String value) {
+		if (attr != null && value != null) {
+			e.setAttribute(attr, value);
 		}
 	}
 }
