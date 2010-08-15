916cb289adbe9df999e3abf7bebbec24529e821e
Niklaus Giger <niklaus.giger@member.fsf.org>
2010-08-15 22:07:39 +0200

    Added support for Linux X86_64 bit

--- BuildElexis/prepare4hudson.rb
+++ BuildElexis/prepare4hudson.rb
@@ -1,11 +1,14 @@
 #!/usr/bin/env ruby
 # Niklaus Giger (c) 2010 niklaus.giger@member.fsf.org
 #
-# Beware, that at the moment we only support 32-bit and no 64 bit build
-# Also we don't attempt to support cross-building. 
+# Supported targets:
+#	linux
+#	linux.x86_64
+#	windows (32 bit)
 #
 # 2010.04.11	Simple setup for a Eclipse-RCP environment for elexis
 # 2010.07.10    Update for new build.xml & Helios
+# 2010.08.14    Added support linux.x86_64
 #
 require 'ftools'
 require 'fileutils'
@@ -49,8 +52,15 @@ Dir.chdir(platformRuntime)
 from=nil
 platformRuntimeTar = nil
 cmd = nil
-if /linux/i.match RUBY_PLATFORM
-  platformRuntimeTar = "#{HudsonRoot}/downloads/eclipse-rcp-#{EclipseVers}-linux-gtk.tar.gz"
+if /x86_64-linux/i.match RUBY_PLATFORM
+  platformRuntimeTar =
+  "#{HudsonRoot}/downloads/eclipse-rcp-#{EclipseVers}-linux-gtk-x86_64.tar.gz"
+  if !File.exists?(eclipse)
+    cmd = "tar -zxf #{platformRuntimeTar}"
+  end
+elsif /linux/i.match RUBY_PLATFORM
+  platformRuntimeTar =
+  "#{HudsonRoot}/downloads/eclipse-rcp-#{EclipseVers}-linux-gtk.tar.gz"
   if !File.exists?(eclipse)
     cmd = "tar -zxf #{platformRuntimeTar}"
   end
@@ -59,11 +69,6 @@ elsif /mingw|msys|mswin/i.match RUBY_PLATFORM
   if !File.exists?(eclipse+".exe")
     cmd = "unzip #{platformRuntimeTar}".gsub('/','\\\\')
   end
-elsif /universal-darwin10.0/i.match RUBY_PLATFORM
-	platformRuntimeTar="#{HudsonRoot}/downloads/eclipse-rcp-helios-macosx-cocoa-x86_64.tar"
-	if !File.exists?(eclipse)
-		cmd="tar -zxf #{platformRuntimeTar}"
-	end
 else
   puts "Unsupported "+RUBY_PLATFORM
   exit 2
@@ -79,19 +84,20 @@ else
 	system(cmd)
 end
 
-from = "#{savedDir}/local.properties.hudson"
-to   = "#{savedDir}/local.properties"
-File.copy(from, to, :verbose => true)
-inhalt = IO.readlines(from)
-neu    = File.open(to, "w+")
-neu.puts(inhalt)
 origin=File.dirname(File.dirname(savedDir))
-neu.puts("base=#{origin}")
-neu.puts("repositories=#{origin}")
-neu.puts("rsc=#{savedDir}/rsc")
-neu.puts("platform-runtime=#{platformRuntime}/eclipse")
-neu.puts("output=#{File.expand_path(File.dirname(savedDir))}/deploy")
+to   = "#{savedDir}/local.properties"
+properties    = File.open(to, "w+")
+properties.puts("skipPlugins=archie-patientstatistik,Statistics,StatisticsFragmentExample,"+
+         "hilotec-pluginstatistiken,elexis-buchhaltung-basis,hilotec-messwerte,"+
+	 "marlovits-addressSearch,marlovits-vornamen,marlovits-plz")
+properties.puts("base=#{origin}")
+properties.puts("hg=hg")
+properties.puts("repositories=#{origin}")
+properties.puts("rsc=#{savedDir}/rsc")
+properties.puts("platform-runtime=#{platformRuntime}/eclipse")
+properties.puts("output=#{File.expand_path(File.dirname(savedDir))}/deploy")
+# properties.puts("unplugged=true")
 Dir.chdir("#{savedDir}/..")
 if !File.directory?("archie")
-	system("svn checkout http://archie.googlecode.com/svn/archie/ch.unibe.iam.scg.archie/trunk archie");
+	system("svn --quiet checkout http://archie.googlecode.com/svn/archie/ch.unibe.iam.scg.archie/trunk archie");
 end
