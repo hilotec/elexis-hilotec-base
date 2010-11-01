cb83384090d1dc506fe436619be8958432b9f804
Gerry (pxg) <weririch@elexis.ch>
2010-11-01 18:25:13 +0100

    tarmedprefs

--- elexis-arzttarife-schweiz/src/ch/elexis/tarmedprefs/PreferenceConstants.java
+++ elexis-arzttarife-schweiz/src/ch/elexis/tarmedprefs/PreferenceConstants.java
@@ -6,6 +6,7 @@ public class PreferenceConstants {
 	public static final String USEEDA="TarmedUseEDa"; //$NON-NLS-1$
 	public static final String TCWITHIMAGE="TarmedTCWithImage";  //$NON-NLS-1$
 	public static final String TARMEDBIL_INTERMEDIATE_EAN="TarmedIntermediateEAN"; //$NON-NLS-1$
+	public static final String BILL_ELECTRONICALLY="TarmedBillElectronic";  //$NON-NLS-1$
 
 	public static final String RNN_EXPORTDIR="TarmedExport_Directory"; //$NON-NLS-1$
 	public static final String RNN_RELAXED="TarmedRnn_relaxed"; //$NON-NLS-1$
