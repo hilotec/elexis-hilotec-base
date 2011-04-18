07a7372e7caa88e46b4b3e7938a594373bc83ada
thomashu <devnull@localhost>
2011-04-18 16:58:03 +0200

    Ticket: #102 - added possibility to create Status with severity ERROR which will be ignored by the QF handler

--- elexis/src/ch/elexis/wizards/DBConnectWizard.java
+++ elexis/src/ch/elexis/wizards/DBConnectWizard.java
@@ -87,7 +87,7 @@ public class DBConnectWizard extends Wizard {
 			j.connect(user, pwd);
 		} catch (JdbcLinkException je) {
 			ElexisStatus status =
-				new ElexisStatus(IStatus.ERROR, Hub.PLUGIN_ID, IStatus.ERROR, Messages
+				new ElexisStatus(IStatus.ERROR, Hub.PLUGIN_ID, ElexisStatus.CODE_NOFEEDBACK, Messages
 					.getString("DBConnectWizard.couldntConnect"), je);
 			StatusManager.getManager().handle(status, StatusManager.BLOCK);
 			return false;
