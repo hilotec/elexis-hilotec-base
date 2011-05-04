5a4b4666745cdeb242e071ea2a7bc4ee82d6631a
marco <devnull@localhost>
2011-05-04 16:44:54 +0200

    Ticket #389: Eigenartikel aus ch.elexis in ch.elexis.eigenartikel ausgelagert - Superfluous Strings

--- ch.elexis.eigenartikel/src/ch/elexis/eigenartikel/Messages.java
+++ ch.elexis.eigenartikel/src/ch/elexis/eigenartikel/Messages.java
@@ -4,33 +4,6 @@ import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
 	private static final String BUNDLE_NAME = "ch.elexis.eigenartikel.messages"; //$NON-NLS-1$
-	public static String ArtikelContextMenu_create;
-	public static String ArtikelContextMenu_deleteAction;
-	public static String ArtikelContextMenu_deleteActionConfirmCaption;
-	public static String ArtikelContextMenu_deleteActionToolTip;
-	public static String ArtikelContextMenu_deleteConfirmBody;
-	public static String ArtikelContextMenu_newAction;
-	public static String ArtikelContextMenu_newActionTooltip;
-	public static String ArtikelContextMenu_pleaseEnterNameForArticle;
-	public static String ArtikelContextMenu_propertiesAction;
-	public static String ArtikelContextMenu_propertiesTooltip;
-	public static String Artikeldetail_EAN;
-	public static String Artikeldetail_Einkaufspreis;
-	public static String Artikeldetail_istbestand;
-	public static String Artikeldetail_istbestandAnbruch;
-	public static String Artikeldetail_lieferant;
-	public static String Artikeldetail_LieferantWaehlen;
-	public static String Artikeldetail_maxbestand;
-	public static String Artikeldetail_minbestand;
-	public static String Artikeldetail_Pharmacode;
-	public static String Artikeldetail_stueckProAbgabe;
-	public static String Artikeldetail_typ;
-	public static String Artikeldetail_Verkaufspreis;
-	public static String Artikeldetail_verpackungseinheit;
-	public static String ArtikelView_errorCaption;
-	public static String ArtikelView_errorText;
-	public static String ArtikelView_importAction;
-	public static String ArtikelView_importCaption;
 	public static String EigenartikelDisplay_actualOnStockPacks;
 	public static String EigenartikelDisplay_actualOnStockPieces;
 	public static String EigenartikelDisplay_buyPrice;
