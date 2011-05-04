74fdb1ac7811432dfdc155c8d87b03f15641623b
marco <devnull@localhost>
2011-05-04 15:08:33 +0200

    Ticket #389: Eigenartikel aus ch.elexis in ch.elexis.eigenartikel ausgelagert

---
+++ ch.elexis.eigenartikel/src/ch/elexis/eigenartikel/EigenartikelPersistentObjectFactory.java
@@ -0,0 +1,32 @@
+package ch.elexis.eigenartikel;
+
+import java.lang.reflect.Method;
+
+import ch.elexis.data.PersistentObject;
+import ch.elexis.data.PersistentObjectFactory;
+
+public class EigenartikelPersistentObjectFactory extends PersistentObjectFactory {
+	
+	@Override
+	public PersistentObject createFromString(String code){
+		try {
+			String[] ci = code.split("::"); //$NON-NLS-1$
+			Class<?> clazz = Class.forName(ci[0]);
+			Method load = clazz.getMethod("load", new Class[] { String.class}); //$NON-NLS-1$
+			return (PersistentObject) (load.invoke(null, new Object[] {
+				ci[1]
+			}));
+		} catch (Exception ex) {
+			return null;
+		}
+	}
+
+	@Override
+	public PersistentObject doCreateTemplate(Class<? extends PersistentObject> typ){
+		try {
+			return (PersistentObject) typ.newInstance();
+		} catch (Exception ex) {
+			return null;
+		}
+	}
+}
---
+++ ch.elexis.eigenartikel/src/ch/elexis/eigenartikel/Messages.java
@@ -0,0 +1,53 @@
+package ch.elexis.eigenartikel;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "ch.elexis.views.artikel.messages"; //$NON-NLS-1$
+	public static String ArtikelContextMenu_create;
+	public static String ArtikelContextMenu_deleteAction;
+	public static String ArtikelContextMenu_deleteActionConfirmCaption;
+	public static String ArtikelContextMenu_deleteActionToolTip;
+	public static String ArtikelContextMenu_deleteConfirmBody;
+	public static String ArtikelContextMenu_newAction;
+	public static String ArtikelContextMenu_newActionTooltip;
+	public static String ArtikelContextMenu_pleaseEnterNameForArticle;
+	public static String ArtikelContextMenu_propertiesAction;
+	public static String ArtikelContextMenu_propertiesTooltip;
+	public static String Artikeldetail_EAN;
+	public static String Artikeldetail_Einkaufspreis;
+	public static String Artikeldetail_istbestand;
+	public static String Artikeldetail_istbestandAnbruch;
+	public static String Artikeldetail_lieferant;
+	public static String Artikeldetail_LieferantWaehlen;
+	public static String Artikeldetail_maxbestand;
+	public static String Artikeldetail_minbestand;
+	public static String Artikeldetail_Pharmacode;
+	public static String Artikeldetail_stueckProAbgabe;
+	public static String Artikeldetail_typ;
+	public static String Artikeldetail_Verkaufspreis;
+	public static String Artikeldetail_verpackungseinheit;
+	public static String ArtikelView_errorCaption;
+	public static String ArtikelView_errorText;
+	public static String ArtikelView_importAction;
+	public static String ArtikelView_importCaption;
+	public static String EigenartikelDisplay_actualOnStockPacks;
+	public static String EigenartikelDisplay_actualOnStockPieces;
+	public static String EigenartikelDisplay_buyPrice;
+	public static String EigenartikelDisplay_dealer;
+	public static String EigenartikelDisplay_displayTitle;
+	public static String EigenartikelDisplay_group;
+	public static String EigenartikelDisplay_maxOnStock;
+	public static String EigenartikelDisplay_minOnStock;
+	public static String EigenartikelDisplay_PiecesPerDose;
+	public static String EigenartikelDisplay_PiecesPerPack;
+	public static String EigenartikelDisplay_pleaseChooseDealer;
+	public static String EigenartikelDisplay_sellPrice;
+	public static String EigenartikelDisplay_typ;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+	
+	private Messages(){}
+}
