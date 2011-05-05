4a99174c9dea1a95fcaddee7f0f8856183d8d59b
marco <devnull@localhost>
2011-05-05 17:07:40 +0200

    Ticket #72: Need a method to get instances of data classes in a plugin, method has to be overriden by the local implementing POFactory

--- elexis-artikel-schweiz/src/ch/elexis/artikel_ch/data/ArtikelFactory.java
+++ elexis-artikel-schweiz/src/ch/elexis/artikel_ch/data/ArtikelFactory.java
@@ -56,4 +56,15 @@ public class ArtikelFactory extends PersistentObjectFactory {
 		
 	}
 	
+	@Override
+	public Class getClassforName(String fullyQualifiedClassName) {
+		Class ret = null;
+		try {
+			ret = Class.forName(fullyQualifiedClassName);
+			return ret;
+		} catch ( ClassNotFoundException e ) {
+			e.printStackTrace();
+			return ret;
+		}
+	}
 }
