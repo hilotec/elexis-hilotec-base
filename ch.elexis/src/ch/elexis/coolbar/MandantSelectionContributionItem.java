aa5a1718cb51546f0c78b308e18569549c1d196a
marcode79 <devnull@localhost>
2012-04-23 10:57:13 +0200

    Ticket #836: Wrong handling of ACE for change mandant

--- ch.elexis/src/ch/elexis/coolbar/MandantSelectionContributionItem.java
+++ ch.elexis/src/ch/elexis/coolbar/MandantSelectionContributionItem.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006-2011, G. Weirich and Elexis
+ * Copyright (c) 2006-2012, G. Weirich and Elexis
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -42,30 +42,31 @@ import ch.elexis.data.Mandant;
 import ch.elexis.preferences.PreferenceConstants;
 
 /**
- * This class implements the {@link Mandant} selection button bar within the application toolbar
- * (coolbar). The list of colors represents the available colors to distinguish the currently
- * selected mandant.
+ * This class implements the {@link Mandant} selection button bar within the
+ * application toolbar (coolbar). The list of colors represents the available
+ * colors to distinguish the currently selected mandant.
  */
 public class MandantSelectionContributionItem extends ContributionItem {
-	
-	
+
 	private ToolItem item;
 	private Menu menu;
 	private Mandant[] mandants;
 	private MenuItem[] menuItems;
 	private ToolBar fParent;
-		
-	private ElexisEventListenerImpl eeli_mandant = new ElexisEventListenerImpl(Mandant.class,
-		ElexisEvent.EVENT_MANDATOR_CHANGED) {
-		public void runInUi(ElexisEvent ev){
-			ICoolBarManager icb =
-				((ApplicationWindow) PlatformUI.getWorkbench().getActiveWorkbenchWindow())
+
+	private boolean rightToChange = false;
+
+	private ElexisEventListenerImpl eeli_mandant = new ElexisEventListenerImpl(
+			Mandant.class, ElexisEvent.EVENT_MANDATOR_CHANGED) {
+		public void runInUi(ElexisEvent ev) {
+			ICoolBarManager icb = ((ApplicationWindow) PlatformUI
+					.getWorkbench().getActiveWorkbenchWindow())
 					.getCoolBarManager2();
 			Mandant m = (Mandant) ev.getObject();
 			if (m != null && item != null) {
 				item.setText(m.getMandantLabel());
 				fParent.setBackground(getColorForMandator(m));
-				if(menuItems == null) {
+				if (menuItems == null) {
 					// We have a read-only coolbar item entry
 					fParent.pack();
 					icb.update(true);
@@ -74,8 +75,8 @@ public class MandantSelectionContributionItem extends ContributionItem {
 				for (int i = 0; i < menuItems.length; i++) {
 					String id = (String) menuItems[i].getData();
 					if (m.getId().equalsIgnoreCase(id)) {
-						// fParent.setBackground(Display.getCurrent().getSystemColor(colors[i %
-// cl]));
+						// fParent.setBackground(Display.getCurrent().getSystemColor(colors[i
+						// % cl]));
 						fParent.pack();
 						// TODO: Anordnung Elemente in Coolbar speicherbar?
 						// TODO: Programmatische Anordnung Elemente coolbar
@@ -88,61 +89,75 @@ public class MandantSelectionContributionItem extends ContributionItem {
 			}
 		}
 	};
-	
-	public MandantSelectionContributionItem(){
+
+	public MandantSelectionContributionItem() {
 		ElexisEventDispatcher.getInstance().addListeners(eeli_mandant);
 	}
-	
+
 	@Override
-	public void fill(ToolBar parent, int index){
+	public void fill(ToolBar parent, int index) {
 		fParent = parent;
 		menu = new Menu(fParent);
-		
-		boolean rightToChange = Hub.acl.request(AccessControlDefaults.AC_CHANGEMANDANT);
+		rightToChange = Hub.acl.request(AccessControlDefaults.AC_CHANGEMANDANT);
 
-		if(rightToChange) {
-			List<Mandant> qre = Hub.getMandantenList();
-			mandants = qre.toArray(new Mandant[] {});
-			if (mandants.length < 2)
-				return;		
-			
-			item = new ToolItem(parent, SWT.DROP_DOWN);
-			item.setToolTipText("Aktuell ausgewählter Mandant bzw. Mandantenauswahl");
-			
-			menuItems = new MenuItem[mandants.length];
-			
-			for (int i = 0; i < mandants.length; i++) {
-				final Mandant m = mandants[i];
-				menuItems[i] = new MenuItem(menu, SWT.RADIO);
-				menuItems[i].setText(m.getMandantLabel());
-				menuItems[i].setImage(getBoxSWTColorImage(getColorForMandator(m)));
-				menuItems[i].setData(m.getId());
-				menuItems[i].addSelectionListener(new SelectionAdapter() {
-					@Override
-					public void widgetSelected(SelectionEvent e){
-						Hub.setMandant(m);
-					}
-				});
-			}
-			
-			item.addListener(SWT.Selection, new Listener() {
-				public void handleEvent(Event event){
-					if (event.detail == SWT.ARROW || event.type == SWT.Selection) {
-						Rectangle rect = item.getBounds();
-						Point pt = new Point(rect.x, rect.y + rect.height);
-						pt = fParent.toDisplay(pt);
-						menu.setLocation(pt.x, pt.y);
-						menu.setVisible(true);
-					}
+		List<Mandant> qre = Hub.getMandantenList();
+		mandants = qre.toArray(new Mandant[] {});
+		if (mandants.length < 2)
+			return;
+
+		item = new ToolItem(parent, SWT.DROP_DOWN);
+		item.setToolTipText("Aktuell ausgewählter Mandant bzw. Mandantenauswahl");
+
+		menuItems = new MenuItem[mandants.length];
+
+		for (int i = 0; i < mandants.length; i++) {
+			final Mandant m = mandants[i];
+			menuItems[i] = new MenuItem(menu, SWT.RADIO);
+			menuItems[i].setText(m.getMandantLabel());
+			menuItems[i].setImage(getBoxSWTColorImage(getColorForMandator(m)));
+			menuItems[i].setData(m.getId());
+			menuItems[i].addSelectionListener(new SelectionAdapter() {
+				@Override
+				public void widgetSelected(SelectionEvent e) {
+					Hub.setMandant(m);
 				}
 			});
-		} else {
-			item = new ToolItem(parent, SWT.None);
-			item.setToolTipText("Aktuell ausgewählter Mandant bzw. Mandantenauswahl");
+			if (Hub.actMandant != null && Hub.actMandant.equals(m)) {
+				menuItems[i].setSelection(true);
+			} else {
+				menuItems[i].setSelection(false);
+			}
 		}
+		
+		if(rightToChange) {
+			item.addListener(SWT.Selection, selectionListener);
+		} else {
+			item.removeListener(SWT.Selection, selectionListener);
+		}
+
+		if (Hub.actMandant != null && item != null) {
+			item.setText(Hub.actMandant.getMandantLabel());
+			fParent.setBackground(getColorForMandator(Hub.actMandant));
+		}
+
+		item.setEnabled(rightToChange);
 	}
+
+	private final Listener selectionListener = new Listener() {
 	
-	private Image getBoxSWTColorImage(Color color){
+		@Override
+		public void handleEvent(Event event) {
+			if (event.detail == SWT.ARROW || event.type == SWT.Selection) {
+				Rectangle rect = item.getBounds();
+				Point pt = new Point(rect.x, rect.y + rect.height);
+				pt = fParent.toDisplay(pt);
+				menu.setLocation(pt.x, pt.y);
+				menu.setVisible(true);
+			}
+		}
+	};
+
+	private Image getBoxSWTColorImage(Color color) {
 		Display display = Display.getCurrent();
 		Image image = new Image(display, 16, 16);
 		GC gc = new GC(image);
@@ -152,14 +167,21 @@ public class MandantSelectionContributionItem extends ContributionItem {
 		gc.dispose();
 		return image;
 	}
-	
-	private Color getColorForMandator(Mandant m){
+
+	private Color getColorForMandator(Mandant m) {
 		return Desk.getColorFromRGB(Hub.globalCfg.get(
-			PreferenceConstants.USR_MANDATOR_COLORS_PREFIX + m.getLabel(), Desk.COL_GREY60));
+				PreferenceConstants.USR_MANDATOR_COLORS_PREFIX + m.getLabel(),
+				Desk.COL_GREY60));
 	}
-	
+
 	@Override
-	public void dispose(){
+	public void dispose() {
 		ElexisEventDispatcher.getInstance().removeListeners(eeli_mandant);
 	}
+
+	@Override
+	public boolean isDynamic() {
+		return true;
+	}
+
 }
