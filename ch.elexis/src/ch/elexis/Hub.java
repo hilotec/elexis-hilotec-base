cc3eb8f0b4f32072fe46b517bafd761fc78d8166
Niklaus Giger <niklaus.giger@member.fsf.org>
2012-10-03 20:43:24 +0200

    Switch versions back to 2.1.7.dev-qualifier

--- ch.elexis/src/ch/elexis/Hub.java
+++ ch.elexis/src/ch/elexis/Hub.java
@@ -73,7 +73,7 @@ public class Hub extends AbstractUIPlugin {
 	public static final String PLUGIN_ID = "ch.elexis"; //$NON-NLS-1$
 	public static final String COMMAND_PREFIX = PLUGIN_ID + ".commands."; //$NON-NLS-1$
 	static final String neededJRE = "1.6.0"; //$NON-NLS-1$
-	public static final String Version = "2.1.7.rc2"; //$NON-NLS-1$
+	public static final String Version = "2.1.7.dev-qualifier"; //$NON-NLS-1$
 	public static final String DBVersion = "1.8.16"; //$NON-NLS-1$
 	public static final String SWTBOTTEST_KEY = "ch.elexis.swtbottest.key"; //$NON-NLS-1$
 	static final String[] mine = {
